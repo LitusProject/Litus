a5cc1989c29ce0b917cd27105364ba7f174edb60
Pieter Maene <maene.pieter@gmail.com>
2011-07-28 21:35:32 +0200

    Some changes that were made before my commit

--- application/models/Litus/Entity/Cudi/Article.php
+++ application/models/Litus/Entity/Cudi/Article.php
@@ -2,9 +2,9 @@
 
 namespace Litus\Entity\Cudi;
 
-use Litus\Entities\Cudi\Articles\MetaInfo;
+use \DateTime;
 
-use DateTime;
+use \Litus\Entity\Cudi\Articles\MetaInfo;
 
 /**
  * @Entity(repositoryClass="Litus\Repository\Cudi\Article")
@@ -17,78 +17,77 @@ use DateTime;
  *      "internal"="Litus\Entity\Cudi\Articles\StockArticles\Internal"}
  * )
  */
-
 abstract class Article
 {
-	/**
-	 * @Id
-	 * @GeneratedValue
-	 * @Column(type="bigint")
-	 */
-	private $id;
-	
-	/**
-	 * @Column(type="string")
-	 */
-	private $title;
-	
-	/**
-	 * @OneToOne(targetEntity="Litus\Entity\Cudi\Articles\MetaInfo")
+    /**
+     * @Id
+     * @GeneratedValue
+     * @Column(type="bigint")
+     */
+    private $id;
+
+    /**
+     * @Column(type="string")
+     */
+    private $title;
+
+    /**
+     * @OneToOne(targetEntity="Litus\Entity\Cudi\Articles\MetaInfo")
      * @JoinColumn(name="metainfo_id", referencedColumnName="id")
-	 */
-	private $metaInfo;
-	
-	/**
-	 * @Column(type="datetime")
-	 */
-	private $timestamp;
-	
-	/**
-	 * @param string $title The title of the article
-	 * @param Litus\Entities\Cudi\Articles\MetaInfo $metaInfo An unlinked metainfo object to link to this article.
-	 * 
-	 * @throws InvalidArgumentException If the given meta info object already has an article linked to it.
-	 */
-	public function __construct($title, $metaInfo)
-	{
-		if ($metaInfo->getArticle() != null)
-			throw new InvalidArgumentException('');
-		
-		$this->title = $title;
-		$this->metaInfo = $metaInfo;
-		$metaInfo->setArticle($this);
-		$this->timestamp = new DateTime();
-	}
-	
-	/**
-	 * @return bigint
-	 */
-	public function getId()
-	{
-		return $this->id;
-	}
-	
-	/**
-	 * @return string
-	 */
-	public function getTitle()
-	{
-		return $this->title;
-	}
-	
-	/**
-	 * @return MetaInfo
-	 */
-	public function getMetaInfo()
-	{
-		return $this->metaInfo;
-	}
-	
-	/**
-	 * @return datetime
-	 */
-	public function getTimestamp()
-	{
-		return $this->timestamp;
-	}
+     */
+    private $metaInfo;
+
+    /**
+     * @Column(type="datetime")
+     */
+    private $timestamp;
+
+    /**
+     * @throws \InvalidArgumentException If the given meta info object already has an article linked to it
+     *
+     * @param string $title The title of the article
+     * @param Litus\Entities\Cudi\Articles\MetaInfo $metaInfo An unlinked metainfo object to link to this article.
+     */
+    public function __construct($title, $metaInfo)
+    {
+        if ($metaInfo->getArticle() != null)
+            throw new \InvalidArgumentException('');
+
+        $this->title = $title;
+        $this->metaInfo = $metaInfo;
+        $metaInfo->setArticle($this);
+        $this->timestamp = new DateTime();
+    }
+
+    /**
+     * @return bigint
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return MetaInfo
+     */
+    public function getMetaInfo()
+    {
+        return $this->metaInfo;
+    }
+
+    /**
+     * @return datetime
+     */
+    public function getTimestamp()
+    {
+        return $this->timestamp;
+    }
 }
--- application/models/Litus/Entity/Cudi/Articles/Stock.php
+++ application/models/Litus/Entity/Cudi/Articles/Stock.php
@@ -7,38 +7,62 @@ namespace Litus\Entity\Cudi\Articles;
  */
 abstract class Stock extends \Litus\Entity\Cudi\Article
 {
-	/**
-	 * @Column(name="purchase_price", type="bigint")
-	 */
-	private $purchasePrice;
-	
-	/**
-	 * @Column(name="sell_price", type="bigint")
-	 */
-	private $sellPrice;
-	
-	/**
-	 * @Column(name="sell_price_members", type="bigint")
-	 */
-	private $sellPriceMembers;	
-	
-	/**
-	 * @Column(type="smallint")
-	 */
-	private $barcode;
-	
-	/**
-	 * @Column(type="boolean")
-	 */
-	private $bookable;
-	
-	/**
-	 * @Column(type="boolean")
-	 */
-	private $unbookable;
-	
-	/**
-	 * @TODO OneToOne(targetEntity="Litus\Entity\Cudi\Supplier")
-	 */
-	private $supplier;
+    /**
+     * @Column(name="purchase_price", type="bigint")
+     */
+    private $purchasePrice;
+
+    /**
+     * @Column(name="sell_price", type="bigint")
+     */
+    private $sellPrice;
+
+    /**
+     * @Column(name="sell_price_members", type="bigint")
+     */
+    private $sellPriceMembers;
+
+    /**
+     * @Column(type="smallint")
+     */
+    private $barcode;
+
+    /**
+     * @Column(type="boolean")
+     */
+    private $bookable;
+
+    /**
+     * @Column(type="boolean")
+     */
+    private $unbookable;
+
+    /**
+     * @TODO OneToOne(targetEntity="Litus\Entity\Cudi\Supplier")
+     */
+    private $supplier;
+
+    /**
+     * @param string $title The title of the article
+     * @param Litus\Entities\Cudi\Articles\MetaInfo $metaInfo An unlinked metainfo object to link to this article.
+     * @param bigint $purchase_price The purchase price of this article.
+     * @param bigint $sellPrice The sell price of this article for non-members.
+     * @param bigint $sellPriceMembers The sell price of this article for members.
+     * @param smallint $barcode This article's barcode.
+     * @param boolean $bookable Indicates whether the article can be booked.
+     * @param boolean $unbookable Indicates whether the article can be unbooked.
+     *
+     * @TODO supplier when db is ready
+     */
+    public function __construct($title, $metaInfo, $purchase_price, $sellPrice, $sellPriceMembers, $barcode, $bookable, $unbookable)
+    {
+        parent::__construct($title, $metaInfo);
+
+        $this->purchasePrice = $purchase_price;
+        $this->sellPrice = $sellPrice;
+        $this->sellPriceMembers = $sellPriceMembers;
+        $this->barcode = $barcode;
+        $this->bookable = $bookable;
+        $this->unbookable = $unbookable;
+    }
 }
