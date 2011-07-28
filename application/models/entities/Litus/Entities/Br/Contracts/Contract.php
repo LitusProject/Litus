df734c3d14118511c364c22382421b57c818871a
Bram Gotink <bram.gotink@litus.cc>
2011-07-28 13:37:59 +0200

    some more work on the BR contract model

--- application/models/entities/Litus/Entities/Br/Contracts/Contract.php
+++ application/models/entities/Litus/Entities/Br/Contracts/Contract.php
@@ -7,6 +7,9 @@ use Zend\Pdf\Page;
 
 use Doctrine\Common\Collections\ArrayCollection;
 
+use Litus\Entities\Users\Person;
+use Litus\Entities\Users\People\Company;
+
 /**
  *
  * @Entity(repositoryClass="Litus\Repositories\Br\Contracts\ContractRepository")
@@ -24,35 +27,38 @@ class Contract {
     private $id;
 
     /**
-     * @OneToMany(targetEntity="Litus\Entities\Br\Contracts\ContractPartMap", mappedBy="contract")
+     * @OneToMany(targetEntity="Litus\Entities\Br\Contracts\ContractComposition", mappedBy="contract")
      *
      * @var array
      */
-    private $parts;
+    private $sections;
 
+    /**
+     * @Column(type="datetime")
+     *
+     * @var \DateTime
+     */
     private $date;
 
-    private $contactPerson;
+    /**
+     * @ManyToOne(targetEntity="Litus\Entities\Users\Person", fetch="LAZY")
+     * @JoinColumn(name="author", referencedColumnName="id", nullable="false")
+     *
+     * @var Person
+     */
+    private $author;
 
+    /**
+     * @ManyToOne(targetEntity="Litus\Entities\Users\People\Company", fetch="LAZY")
+     * @JoinColumn(name="company", referencedColumnName="id", nullable="false")
+     *
+     * @var Company
+     */
     private $company;
-
-    // ...
 
     public function __construct()
     {
         $this->parts = new ArrayCollection();
-    }
-
-    /**
-     * Adds the given part at the given order to this Contract.
-     *
-     * @param Part $part
-     * @param int $order
-     * @return void
-     */
-    public function addPart(Part $part, $order)
-    {
-        $this->parts->add(new ContractPartMap($this, $part, $order));
     }
 
     /**
@@ -63,5 +69,62 @@ class Contract {
     public function getParts()
     {
         return $this->parts->toArray();
+    }
+
+    /**
+     * Adds the given part at the given order to this Contract.
+     *
+     * @param Section $section
+     * @param int $order
+     * @return void
+     */
+    public function addSection(Section $section, $order)
+    {
+        $this->parts->add(new ContractComposition($this, $section, $order));
+    }
+
+    /**
+     * @return Person
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @throws \Exception
+     * @param \Litus\Entities\Users\Person $author
+     * @return void
+     */
+    public function setAuthor(Person $author)
+    {
+        if($author === null)
+            throw new \Exception('Author cannot be null.');
+        $this->author = $author;
+    }
+
+    public function getCompany()
+    {
+        return $this->company;
+    }
+
+    public function setCompany(Company $company)
+    {
+        if($company === null)
+            throw new \Exception('Company cannot be null.');
+        $this->company = $company;
+    }
+
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTime $date = null)
+    {
+        if($date === null)
+            $this->date = new \DateTime();
+        else
+            $this->date = $date;
     }
 }
