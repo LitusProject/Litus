2ddf8375274adcc4bc743f074a237b74695bfacb
Niels <niels.avonds@litus.cc>
2011-07-30 23:14:59 +0200

    Changed tab to 4 spaces, no code changes.

--- application/modules/admin/controllers/TextbookController.php
+++ application/modules/admin/controllers/TextbookController.php
@@ -10,7 +10,7 @@ use \Litus\Entity\Cudi\Articles\MetaInfo;
 use \Litus\Entity\Cudi\Articles\StockArticles\External;
 
 /**
- * 
+ *
  * This class controlls management and adding of textbooks.
  * @author Niels Avonds
  *
@@ -24,90 +24,93 @@ class TextbookController extends \Litus\Controller\Action
 
     public function indexAction()
     {
-    	$this->_forward('add');
+        $this->_forward('add');
     }
-    
+
     public function addAction()
     {
-    	$form = new Add();
-    	$internal_form = $form->getInternalForm();
+        $form = new Add();
+        $internal_form = $form->getInternalForm();
 
-    	$this->view->form = $form;
-    	
-    	if($this->getRequest()->isPost()) {
-    		$formData = $this->getRequest()->getPost();
-    		
-    		/*
-    		 * If this isn't an internal article, remove all the validators and required flags from that subform.
-    		 * 
-    		 * Remember them in arrays to restore it later.
-    		 */
-    		$validators = array();
-    		$required = array();
-    		if (!$formData['internal']) {
-    		    foreach ($internal_form->getElements() as $formelement) {
-    				$validators[$formelement->getName()] = $formelement->getValidators();
-    				$required[$formelement->getName()] = $formelement->isRequired();
-    				$formelement->clearValidators();
-    				$formelement->setRequired(false);
-    			}
-    		}
-    		
-    		if($form->isValid($formData)) {
-    			
-    			// Add the new article to the database
-    			
-    			// Insert common information (between internal and external) in variables
-    			
-    			$authors = $formData['author'];
-				$publishers = $formData['publisher'];
-    			$yearPublished = $formData['year_published'];
-    			    				
-    			$metaInfo = new MetaInfo($authors, $publishers, $yearPublished);
-    			
-    			$title = $formData['title'];
-    			$purchase_price = $formData['purchaseprice'];
-    			$sellPrice = $formData['sellpricenomember'];
-    			$sellPriceMembers = $formData['sellpricemember'];
-    			$barcode = 0; // TODO barcode
-    			$bookable = $formData['bookable'];
-    			$unbookable = $formData['unbookable'];
-    			
-    			if (!$formData['internal']) {
-    				$article = new External(
+        $this->view->form = $form;
+         
+        if($this->getRequest()->isPost()) {
+            $formData = $this->getRequest()->getPost();
+
+            /*
+             * If this isn't an internal article, remove all the validators and required flags from that subform.
+            *
+            * Remember them in arrays to restore it later.
+            */
+            $validators = array();
+            $required = array();
+            if (!$formData['internal']) {
+                foreach ($internal_form->getElements() as $formelement) {
+                    $validators[$formelement->getName()] = $formelement->getValidators();
+                    $required[$formelement->getName()] = $formelement->isRequired();
+                    $formelement->clearValidators();
+                    $formelement->setRequired(false);
+                }
+            }
+
+            if($form->isValid($formData)) {
+                 
+                // Add the new article to the database
+                 
+                // Insert common information (between internal and external) in variables
+                 
+                $authors = $formData['author'];
+                $publishers = $formData['publisher'];
+                $yearPublished = $formData['year_published'];
+                 
+                $metaInfo = new MetaInfo($authors, $publishers, $yearPublished);
+                 
+                $title = $formData['title'];
+                $purchase_price = $formData['purchaseprice'];
+                $sellPrice = $formData['sellpricenomember'];
+                $sellPriceMembers = $formData['sellpricemember'];
+                $barcode = 0; // TODO barcode
+                $bookable = $formData['bookable'];
+                $unbookable = $formData['unbookable'];
+                 
+                if (!$formData['internal']) {
+                    $article = new External(
                         $title, $metaInfo, $purchase_price, $sellPrice,
-    					$sellPriceMembers, $barcode, $bookable, $unbookable
+                        $sellPriceMembers, $barcode, $bookable, $unbookable
                     );
-    				
-    			} else {
-    				
-    				// Insert additional information needed for internal textbooks in variables
-    				$nrbwpages = $formData['nrbwpages'];
-    				$nrcolorpages = $formData['nrcolorpages'];
-    				$official = $formData['official'];
-    				$rectoverso = $formData['rectoverso'];
 
-    				$article = new Internal($title, $metaInfo, $purchase_price, $sellPrice, $sellPriceMembers, $barcode, 
-    					$bookable, $unbookable, $nrbwpages, $nrcolorpages, $official, $rectoverso);
+                } else {
 
-    			}
-    			
-    			$this->getEntityManager()->persist($metaInfo);
-    			$this->getEntityManager()->persist($article);
-    			
-    			
-    		}
-			
-    		// Make sure the validators and required flags are added again.
-    		if (!$formData['internal']) {
-    			 
-    			foreach ($internal_form->getElements() as $formelement) {
-    				if (array_key_exists ($formelement->getName(), $validators))
-    					$formelement->setValidators($validators[$formelement->getName()]);
-    					$formelement->setRequired($required[$formelement->getName()]);
-    			}
-    		}
-    		
-    	}
+                    // Insert additional information needed for internal textbooks in variables
+                    $nrbwpages = $formData['nrbwpages'];
+                    $nrcolorpages = $formData['nrcolorpages'];
+                    $official = $formData['official'];
+                    $rectoverso = $formData['rectoverso'];
+
+                    $article = new Internal(
+                        $title, $metaInfo, $purchase_price, $sellPrice, $sellPriceMembers, $barcode,
+                        $bookable, $unbookable, $nrbwpages, $nrcolorpages, $official, $rectoverso
+                    );
+
+                }
+                 
+                $this->getEntityManager()->persist($metaInfo);
+                $this->getEntityManager()->persist($article);
+                 
+                 
+            }
+            	
+            // Make sure the validators and required flags are added again.
+            if (!$formData['internal']) {
+
+                foreach ($internal_form->getElements() as $formelement) {
+                    if (array_key_exists ($formelement->getName(), $validators))
+                        $formelement->setValidators($validators[$formelement->getName()]);
+                    if (array_key_exists ($formelement->getName(), $required))
+                        $formelement->setRequired($required[$formelement->getName()]);
+                }
+            }
+
+        }
     }
 }
