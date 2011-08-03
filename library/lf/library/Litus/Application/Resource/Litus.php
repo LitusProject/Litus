eb094583fac68855d376a59b9eeae33ff7ca7b01
Bram Gotink <bram.gotink@litus.cc>
2011-08-04 00:43:46 +0200

    litus.ini added for configuration, everything in it will be put in the registry with "litus.<key>" as key
    Modified the contract/invoice/letter downloader to use the new litus.ini

---
+++ library/lf/library/Litus/Application/Resource/Litus.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Litus\Application\Resource;
+
+use \Zend\Registry;
+use \Zend\Config\Ini as ConfigFile;
+
+use \Zend\Application\Resource\Exception\InitializationException;
+
+use \Zend\Application\Resource\AbstractResource;
+
+class Litus extends AbstractResource {
+
+    public function init()
+    {
+        $options = $this->getOptions();
+
+        if(!array_key_exists('config', $options))
+            throw new InitializationException('resources.litus.config not defined in application.ini');
+
+        $env = APPLICATION_ENV;
+        if(array_key_exists('environment', $options))
+            $env = $options['environment'];
+
+        $options = new ConfigFile($options['config'], $env);
+        $options = $options->toArray();
+
+        foreach ($options as $key => $value) {
+            Registry::set('litus.' . $key, $value);
+        }
+    }
+
+}
