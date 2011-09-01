b14bb159e5097cc78b3ede61201853d8b398d4be
Pieter Maene <pieter.maene@litus.cc>
2011-09-01 18:08:28 +0200

    Added two fieldsets to the Company form to clarify a new contact is being created.

--- application/modules/admin/forms/Company/Add.php
+++ application/modules/admin/forms/Company/Add.php
@@ -20,6 +20,22 @@ class Add extends \Admin\Form\User\Add
         $this->removeElement('roles');
         $this->removeElement('submit');
 
+        $this->addDisplayGroup(
+            array(
+                'username',
+                'credential',
+                'verify_credential',
+                'first_name',
+                'last_name',
+                'email'
+            ),
+            'contact_information'
+        );
+        $this->getDisplayGroup('contact_information')
+            ->setLegend('Contact Information')
+            ->setAttrib('id', 'contact_information')
+            ->removeDecorator('DtDdWrapper');
+
         $field = new Text('company_name');
         $field->setLabel('Company Name')
                 ->setRequired()
@@ -31,6 +47,18 @@ class Add extends \Admin\Form\User\Add
                 ->setRequired()
                 ->setDecorators(array(new FieldDecorator()));
         $this->addElement($field);
+
+        $this->addDisplayGroup(
+            array(
+                'company_name',
+                'vat_number',
+            ),
+            'company_information'
+        );
+        $this->getDisplayGroup('company_information')
+            ->setLegend('Company Information')
+            ->setAttrib('id', 'company_information')
+            ->removeDecorator('DtDdWrapper');
 
         $field = new Submit('submit');
         $field->setLabel('Add')
