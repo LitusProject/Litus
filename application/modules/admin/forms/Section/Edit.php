6f58f08de82fd8d995ce5d890402fa0d74b72904
Bram Gotink <bram.gotink@litus.cc>
2011-09-09 13:29:54 +0200

    * forgot to commit the section edit form before, whoops * the indexAction for contracts (which pmaene so eagerly deleted) is now under listAction with id == 0 * fixed file permissions for bin/doctrine yet again * instead of linking default.xsl, I just copied it so it doesn't break once _someone_ using a samba share commits and breaks all links/file permissions * Contract->getParts() was renamed, but not updated in ContractGenerator and InvoiceGenerator

---
+++ application/modules/admin/forms/Section/Edit.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Admin\Form\Section;
+
+use \Litus\Form\Decorator\ButtonDecorator;
+use \Litus\Entity\Br\Contracts\Section;
+
+use \Zend\Form\Element\Submit;
+
+class Edit extends Add
+{
+
+    public function __construct(Section $section, $options = null)
+    {
+        parent::__construct($options);
+
+        $this->setAction('/admin/section/edit/id/' . $section->getId());
+
+        $this->removeElement('submit');
+
+        $field = new Submit('submit');
+        $field->setLabel('Save changes')
+                ->setAttrib('class', 'sections_edit')
+                ->setDecorators(array(new ButtonDecorator()));
+        $this->addElement($field);
+
+        $this->populate(
+            array(
+                'name'      => $section->getName(),
+                'price'     => $section->getPrice(),
+                'vat_type'  => $section->getVatType(),
+                'content'   => $section->getContent()
+            )
+        );
+    }
+}
