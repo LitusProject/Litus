3623376cd2c79a8df6461af81e419ec2a6dbcfbf
Alan Szepieniec <alan.szepieniec@litus.cc>
2011-10-19 15:05:10 +0200

    got salesession working (finally)

--- application/models/Litus/Entity/Cudi/Sales/SaleSession.php
+++ application/models/Litus/Entity/Cudi/Sales/SaleSession.php
@@ -14,50 +14,98 @@ class SaleSession
 	 * @Column(type="bigint")
 	 */
 	private $id;
+
+	public function setId( $id_ ) {
+		$this->id = $id_;
+	}
+
+	public function getId() {
+		return $this->id;
+	}
 	
 	/**
 	 * @Column(name="open_date", type="datetime")
 	 */
 	private $openDate;
+
+	public function setOpenDate( $openDate_ ) {
+		$this->openDate = $openDate_;
+	}
+
+	public function getOpenDate() {
+		return $this->openDate;
+	}
 	
 	/**
 	 * @Column(name="close_date", type="datetime")
 	 */
 	private $closeDate;
+
+	public function setCloseDate( $closeDate_ ) {
+		$this->closeDate = $closeDate_;
+	}
+
+	public function getCloseDate() {
+		return $this->closeDate;
+	}
 	
 	/**
 	 * @Column(name="confirm_date", type="datetime")
 	 */
-	private $confirmDate;
+//	private $confirmDate;
 	
 	/**
-	 * @Column(name="open_amount", type="float")
+	 * @Column(name="open_amount", type="integer")
 	 */
 	private $openAmount;
+
+	public function setOpenAmount( $openAmount_ ) {
+		$this->openAmount = $openAmount_;
+	}
+
+	public function getOpenAmount() {
+		return $this->openAmount;
+	}
 	
 	/**
-	 * @Column(name="close_amount", type="float")
+	 * @Column(name="close_amount", type="integer")
 	 */
 	private $closeAmount;
+
+	public function setCloseAmount( $closeAmount_ ) {
+		$this->closeAmount = $closeAmount_;
+	}
+
+	public function getCloseAmount() {
+		return $this->closeAmount;
+	}
 	
 	/**
 	 * @Column(type="datetime")
 	 */
-	private $registerStart;
+//	private $registerStart;
 	
 	/**
 	 * @Column(type="datetime")
 	 */
-	private $registerEnd;
+//	private $registerEnd;
 	
 	/**
 	 * @ManyToOne(targetEntity="\Litus\Entity\Users\Person")
 	 * @JoinColumn(name="manager_id", referencedColumnName="id")
 	 */
 	private $manager;
+
+	public function setManager( $manager_ ) {
+		$this->manager = $manager_;
+	}
+
+	public function getManager() {
+		return $this->manager;
+	}
 	
 	/**
 	 * @todo ManyToOne(targetEntity="Litus\Entity\Unions\Union")
 	 */
-	private $union;
+	//private $union;
 }
