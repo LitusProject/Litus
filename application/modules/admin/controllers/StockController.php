ecfee6ff6d0098cdbc522461f69b721e4f96579c
Kristof Marien <kristof.marien@litus.cc>
2011-10-30 21:51:51 +0100

    Cudi.Stock order can be created

--- application/modules/admin/controllers/StockController.php
+++ application/modules/admin/controllers/StockController.php
@@ -4,9 +4,9 @@ namespace Admin;
 
 use \Doctrine\ORM\EntityManager;
 
-use \Admin\Form\Booking\Add;
+use \Admin\Form\Stock\AddOrder;
 
-use \Litus\Entity\Cudi\Sales\Booking;
+use \Litus\Entity\Cudi\Stock\Order;
 use \Litus\FlashMessenger\FlashMessage;
 
 /**
@@ -35,6 +35,28 @@ class StockController extends \Litus\Controller\Action
 	public function ordersAction()
 	{
         $this->view->orders = $this->getEntityManager()->getRepository('Litus\Entity\Cudi\Stock\Order')->findAll();        		
+	}
+	
+	public function addorderAction()
+	{
+		$form = new AddOrder();
+		$this->view->form = $form;
+		
+		if($this->getRequest()->isPost()) {
+            $formData = $this->getRequest()->getPost();
+
+            if($form->isValid($formData)) {
+                $supplier = $this->getEntityManager()
+					->getRepository('Litus\Entity\Cudi\Supplier')
+					->findOneById($formData['supplier']);
+				
+				$order = new Order($supplier, $formData['price']);
+                 
+                $this->getEntityManager()->persist($order);
+                $this->broker('flashmessenger')->addMessage(new FlashMessage(FlashMessage::SUCCESS, "SUCCESS", "The order was successfully created!"));
+				$this->_redirect('orders');
+			}
+        }
 	}
 	
 	public function deliveriesAction()
