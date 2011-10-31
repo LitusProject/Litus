851f7109553aa27897099dfa03a58c4281527c20
Pieter Maene <pieter.maene@litus.cc>
2011-10-31 18:06:31 +0100

    Even more refactoring!

--- application/models/Litus/Repository/General/Config.php
+++ application/models/Litus/Repository/General/Config.php
@@ -12,4 +12,31 @@ use Doctrine\ORM\EntityRepository;
  */
 class Config extends EntityRepository
 {
+    public function getConfigValue($key)
+    {
+        $config = $this->find($key);
+
+        if($config === null)
+            throw new \RuntimeException('Configuration entry ' . $key . ' not found');
+        return $config->getValue();
+    }
+
+    public function findAllByPrefix($prefix)
+    {
+        $configs = $this->_em
+            ->createQuery('SELECT c FROM Litus\Entity\General\Config c WHERE c.key LIKE \'' . $prefix . '.%\'')
+            ->getResult();
+
+        $result = array();
+        foreach ($configs as $config) {
+            $key = $config->getKey();
+            $value = $config->getValue();
+
+            $key = str_replace($prefix . '.','', $key);
+
+            $result[$key] = $value;
+        }
+
+        return $result;
+    }
 }
