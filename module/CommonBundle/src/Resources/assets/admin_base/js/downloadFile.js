07abdc207e2d63a10811ecef875b3125fb52ade5
Kristof Marien <krmarien@gmail.com>
2012-02-04 16:06:34 +0100

    Changed the layout and navigation toggle of admin is in external js file

--- module/CommonBundle/src/Resources/assets/admin_base/js/downloadFile.js
+++ module/CommonBundle/src/Resources/assets/admin_base/js/downloadFile.js
@@ -12,4 +12,4 @@
 			return false;
 	    });
     }
-})(jQuery)
+})(jQuery);
--- module/CommonBundle/src/Resources/assets/admin_base/js/sidebarNavigation.js
+++ module/CommonBundle/src/Resources/assets/admin_base/js/sidebarNavigation.js
@@ -1,10 +1,9 @@
-$(document).ready(function (){
-
-	$('.submenu .subtitle p').click(function(event) {
-		var target = event.target;
-		if(event.target.nodeName == 'B')
-			var target = $(event.target).parent();
-		$('#submenu_' + $(target).attr('id').substring(6, $(target).attr('id').length).toLowerCase()).toggle();
-	});
-
-});
+(function ($) {
+	$.fn.sidebarNavigation = function() {
+		return $(this).find('.subtitle').each(function () {
+				$(this).click(function () {
+						$(this).parent().find('ul').toggle();
+					});
+			});
+	};
+}) (jQuery)
