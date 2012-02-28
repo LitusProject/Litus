bbd90081533747c45dacb5a3acafc01cfeaf57a3
Pieter Maene <pieter@maene.eu>
2012-02-28 12:29:12 +0100

    Installation controllers

---
+++ module/CommonBundle/src/Component/Controller/ActionController/InstallerController.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Litus is a project by a group of students from the K.U.Leuven. The goal is to create
+ * various applications to support the IT needs of student unions.
+ *
+ * @author Karsten Daemen <karsten.daemen@litus.cc>
+ * @author Bram Gotink <bram.gotink@litus.cc>
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ * @author Michiel Staessen <michiel.staessen@litus.cc>
+ * @author Alan Szepieniec <alan.szepieniec@litus.cc>
+ *
+ * @license http://litus.cc/LICENSE
+ */
+ 
+namespace CommonBundle\Component\Controller\ActionController;
+
+/**
+ * This abstract function should be implemented by all controller that want to provide
+ * installation functionality for a bundle.
+ *
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ */
+abstract class InstallerController extends \CommonBundle\Component\Controller\IActionController
+{
+	/**
+	 * Running all installation methods.
+	 *
+	 * @return void
+	 */
+	public function indexAction()
+	{
+		$this->_initConfig();
+		$this->_initAcl();
+		
+		return array(
+			'installerReady' => true
+		);
+	}
+	
+	/**
+	 * Initiliazes all configuration values for the bundle.
+	 *
+	 * @return void
+	 */
+	abstract private function _initConfig();
+	
+	/**
+	 * Initializes the ACL tree for the bundle.
+	 *
+	 * @return void
+	 */
+	abstract private function _initAcl();
+}
