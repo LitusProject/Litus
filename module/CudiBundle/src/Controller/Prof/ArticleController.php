e0d4ff143638b73f5bee53451f2ac71183f477d8
Kristof Marien <kristof.marien@litus.cc>
2012-03-21 20:36:24 +0100

    Added layout for Prof and Supplier app

---
+++ module/CudiBundle/src/Controller/Prof/ArticleController.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Litus is a project by a group of students from the K.U.Leuven. The goal is to create
+ * various applications to support the IT needs of student unions.
+ *
+ * @author Karsten Daemen <karsten.daemen@litus.cc>
+ * @author Bram Gotink <bram.gotink@litus.cc>
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ * @author Michiel Staessen <michiel.staessen@litus.cc>
+ * @author Alan Szepieniec <alan.szepieniec@litus.cc>
+ *
+ * @license http://litus.cc/LICENSE
+ */
+ 
+namespace CudiBundle\Controller\Prof;
+
+use CommonBundle\Component\FlashMessenger\FlashMessage;
+
+/**
+ * ArticleController
+ *
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ */
+class ArticleController extends \CommonBundle\Component\Controller\ActionController
+{
+    public function manageAction()
+    {
+    	return array();
+    }
+    
+    public function editAction()
+    {
+    	return array();
+    }
+    
+    public function addAction()
+    {
+    	return array();
+    }
+}
