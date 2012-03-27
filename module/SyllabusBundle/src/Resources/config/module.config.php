b27edb888ce5bef834b1f1eaea2dca84f160e51e
Kristof Marien <kristof.marien@litus.cc>
2012-03-27 17:59:10 +0200

    Start Syllabus

--- module/SyllabusBundle/src/Resources/config/module.config.php
+++ module/SyllabusBundle/src/Resources/config/module.config.php
@@ -1,9 +1,9 @@
 <?php
 return array(
-    'display_exceptions'    => true,
     'di'                    => array(
         'instance' => array(
             'alias' => array(
+                'syllabus_update' => 'SyllabusBundle\Controller\Admin\UpdateController',
             ),
             'doctrine_config' => array(
                 'parameters' => array(
@@ -15,5 +15,19 @@ return array(
         ),
     ),
     'routes' => array(
+        'syllabus_update' => array(
+        	'type'    => 'Zend\Mvc\Router\Http\Segment',
+        	'options' => array(
+        		'route' => '/admin/syllabus[/:action[/:id]]',
+        		'constraints' => array(
+        			'action'  => '[a-zA-Z][a-zA-Z0-9_-]*',
+        			'id'      => '[0-9]*',
+        		),
+        		'defaults' => array(
+        			'controller' => 'syllabus_update',
+        			'action'     => 'update',
+        		),
+        	),
+        ),
     ),
 );
