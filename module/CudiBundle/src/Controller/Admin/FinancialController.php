78ba88509c3aaeceadf533688ebd869e79bbf20b
alan <alan.szepieniec@litus.cc>
2012-03-30 18:25:59 +0200

    sales overview in financial overview

--- module/CudiBundle/src/Controller/Admin/FinancialController.php
+++ module/CudiBundle/src/Controller/Admin/FinancialController.php
@@ -31,99 +31,21 @@ use CommonBundle\Component\FlashMessenger\FlashMessage,
  */
 class FinancialController extends \CommonBundle\Component\Controller\ActionController
 {
-    public function manageAction()
-    {
     
-        $sessions = $this->getEntityManager()
-                         ->createQuery("select s from CudiBundle\Entity\Sales\Session s order by s.openDate DESC")
-                         ->getResult();
-                         
-        $i = 0;
-        foreach( $sessions as $session )
-        {
-            $i++;
-            
-            $rec[$i]['id'] = $session->getId();
-            $rec[$i]['username'] = $session->getManager()->getUsername();
-            $rec[$i]['openDate'] = $session->getOpenDate()->format( 'Y-m-d H:i' );
-            
-            if( $session->getCloseDate() != null )
-            	$rec[$i]['closeDate'] = $session->getCloseDate()->format( 'Y-m-d H:i' );
-            else
-                $rec[$i]['closeDate'] = 0;
-        	
-            if( $session->getOpenDate() < $session->getCloseDate() )
-                $rec[$i]['open'] = false;
-            else
-                $rec[$i]['open'] = true;
-                
-            
-            $rec[$i]['theoreticalrevenue'] = 500;
-            $rec[$i]['actualrevenue'] = 495;
-        }
-
-        $paginator = $this->paginator()->createFromArray(
-            $rec,
-            $this->getParam('page')
+    public function salesAction()
+    {
+        $paginator = $this->paginator()->createFromEntity(
+            'CudiBundle\Entity\Sales\Session',
+            $this->getParam('page'),
+            array(),
+			array('openDate' => 'DESC')
         );
-        
-        // todo: display sale session comment on mouse over
         
         return array(
         	'paginator' => $paginator,
+        	'em' => $this->getEntityManager(),
         	'paginationControl' => $this->paginator()->createControl(true)
         );
-    }
-    
-    /**
-     * getTheoreticalRevenue
-     * calculates the theoretical revenue of a given session --
-     * that is, the revenue expected on the basis of sold stock items
-     * @param $sessionId -- the session id which uniquely identifies
-     * the session
-     */
-    private function getTheoreticalRevenue( $sessionId )
-    {
-        $qb = $this->getEntityManager()->createQueryBuilder();
-    	$qb->select( 'sum(s.price)' )
-           ->from( 'CudiBundle\Entity\Sales\SaleItem', 's' )
-           ->where( 's.session = ' . $sessionId );
-        $revenue = $qb->getQuery()->getSingleScalarResult();
-        if( $revenue === NULL )
-            return 0;
-        else
-            return $revenue;
-    }
-    
-    /**
-     * getActualRevenue
-     * calculates the actual revenue of a given session --
-     * that is, the register difference between opening and closure of
-     * a session
-     * @param $sessionId -- the session id which uniquely identifies
-     * the session
-     */
-    private function getActualRevenue( $sessionId )
-    {
-        $session = $this->getEntityManager()
-                    ->getRepository('CudiBundle\Entity\Sales\Session')
-                    ->findOneById($sessionId);
-        $openamount = $this->getEntityManager()
-            ->getRepository('CommonBundle\Entity\General\Bank\CashRegister')
-            ->findOneById(
-                $session->getOpenAmount()
-            );
-        
-        if( $session->isOpen() )
-            return 0;
-        
-        $closeamount = $this->getEntityManager()
-            ->getRepository('CommonBundle\Entity\General\Bank\CashRegister')
-            ->findOneById(
-                $session->getCloseAmount()
-            );
-        
-        return $closeamount->getTotalAmount() - $openamount->getTotalAmount();
     }
     
 }
