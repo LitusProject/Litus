b165a1007a2f8ef05771c2e46d0295ed87ce2e11
Kristof Marien <kristof.marien@litus.cc>
2012-05-18 21:25:51 +0200

    Added some extra files for website Bundles

---
+++ module/CommonBundle/src/Component/Localisation/Translator.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace CommonBundle\Component\Localisation;
+
+use Zend\Translator\Exception\InvalidArgumentException;
+
+class Translator extends \Zend\Translator\Translator
+{
+	/**
+     * Creates a new translator instance. 
+     *
+     * Override from \Zend\Translator\Translator::__construct($options).
+     * 
+     * @param string $adapter
+     * @param array $translations 
+     */
+	public function __construct($adapter, $translations)
+    {
+    	if (sizeof($translations) === 0)
+    		throw new InvalidArgumentException("At least one translator file must be given.");
+    	
+    	$first = current($translations);
+    	parent::__construct($adapter, $first['content'], $first['locale']);
+    	
+  		foreach ($translations as $alias => $translation) {
+  			$this->addTranslation($translation['content'], $translation['locale']);
+  		}
+    }
+}
---
+++ module/CommonBundle/src/Entity/General/Language.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Litus is a project by a group of students from the K.U.Leuven. The goal is to create
+ * various applications to support the IT needs of student unions.
+ *
+ * @author Karsten Daemen <karsten.daemen@litus.cc>
+ * @author Bram Gotink <bram.gotink@litus.cc>
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ * @author Michiel Staessen <michiel.staessen@litus.cc>
+ * @author Alan Szepieniec <alan.szepieniec@litus.cc>
+ *
+ * @license http://litus.cc/LICENSE
+ */
+ 
+namespace CommonBundle\Entity\General;
+
+/**
+ * This class represents a language entry that is saved in the database
+ *
+ * @Entity(repositoryClass="CommonBundle\Repository\General\Language")
+ * @Table(name="general.language")
+ */
+class Language 
+{
+    /**
+     * @var integer The ID of the language 
+     *
+     * @Id
+     * @GeneratedValue
+     * @Column(type="bigint")
+     */
+    private $id;
+
+    /**
+     * @var string The language abbrev
+     *
+     * @Column(type="string", length=4)
+     */
+    private $abbrev;
+
+    /**
+     * @var string The language name
+     *
+     * @Column(type="string")
+     */
+    private $name;
+	
+	/**
+	 * @return integer
+	 */
+    public function getId()
+    {
+        return $this->id;
+    }
+	
+	/**
+	 * @return string
+	 */
+    public function getAbbrev()
+    {
+        return $this->abbrev;
+    }
+	
+	/**
+	 * @return string
+	 */
+	public function getName()
+	{
+	    return $this->name;
+	}
+}
