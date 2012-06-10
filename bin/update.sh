5334ee7d4e7d36ec557838b632c80787fdcef3b3
Pieter Maene <pieter.maene@litus.cc>
2012-06-10 16:53:15 +0200

    Modified the update script a little bit

--- bin/update.sh
+++ bin/update.sh
@@ -13,7 +13,11 @@ bin/MistDoctrine/doctrine orm:schema-tool:update --force
 
 # Making sure our LESS stylesheets are recompiled
 touch module/CommonBundle/src/Resources/assets/admin/less/admin.less
+touch module/CommonBundle/src/Resources/assets/site/less/base.less
+
 touch module/CommonBundle/src/Resources/assets/bootstrap/less/bootstrap.less
+
 touch module/CudiBundle/src/Resources/assets/sale/less/base.less
 touch module/CudiBundle/src/Resources/assets/supplier/less/base.less
+
 touch module/ProfBundle/src/Resources/assets/prof/less/base.less
