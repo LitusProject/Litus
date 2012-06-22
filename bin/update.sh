6b4d473c05e0843f649c6e937fa5fc6f3460794c
Pieter Maene <p.maene@gmail.com>
2012-06-22 19:35:09 +0200

    Fixed some irritating proxy errors

--- bin/update.sh
+++ bin/update.sh
@@ -21,6 +21,9 @@ git pull
 # Updating the database
 bin/MistDoctrine/doctrine orm:schema-tool:update --force
 
+bin/MistDoctrine/doctrine orm:generate-proxies data/proxies/
+chown -R www-data:www-data data/proxies
+
 # Making sure our LESS stylesheets are recompiled
 touch module/CommonBundle/src/Resources/assets/admin/less/admin.less
 touch module/CommonBundle/src/Resources/assets/bootstrap/less/bootstrap.less
