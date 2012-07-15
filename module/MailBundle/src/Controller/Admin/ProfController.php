1ba256a07e082c652912d4a8d28ac47d75c7bb28
Kristof Marien <kristof.marien@litus.cc>
2012-07-15 21:39:12 +0200

    * Still working on MailBundle * Updated syllabus update code

---
+++ module/MailBundle/src/Controller/Admin/ProfController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Litus is a project by a group of students from the K.U.Leuven. The goal is to create
+ * various applications to support the IT needs of student unions.
+ *
+ * @author Karsten Daemen <karsten.daemen@litus.cc>
+ * @author Bram Gotink <bram.gotink@litus.cc>
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ * @author Michiel Staessen <michiel.staessen@litus.cc>
+ * @author Alan Szepieniec <alan.szepieniec@litus.cc>
+ *
+ * @license http://litus.cc/LICENSE
+ */
+
+namespace MailBundle\Controller\Admin;
+
+use CommonBundle\Component\FlashMessenger\FlashMessage,
+	Zend\View\Model\ViewModel;
+
+/**
+ * ProfController
+ *
+ * @autor Kristof Mariën <kristof.marien@litus.cc>
+ */	
+class ProfController extends \CommonBundle\Component\Controller\ActionController\AdminController
+{
+	public function startCudiAction()
+	{
+	    return new ViewModel();
+	}
+}
