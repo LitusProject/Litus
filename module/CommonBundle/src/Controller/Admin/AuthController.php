08d3db9fdd573d22e43f5289277f97f873ce0618
Pieter Maene <pieter.maene@litus.cc>
2012-08-02 00:12:29 +0200

    And another small error

--- module/CommonBundle/src/Controller/Admin/AuthController.php
+++ module/CommonBundle/src/Controller/Admin/AuthController.php
@@ -96,7 +96,7 @@ class AuthController extends \CommonBundle\Component\Controller\ActionController
     
     public function shibbolethAction()
     {   
-        if (isset($this->getParam('identification'), $this->getParam('hash'))) {
+        if (isset($this->getParam('identification', $this->getParam('hash'))) {
             $authentication = new Authentication(
                 new ShibbolethAdapter(
                     $this->getEntityManager(),
