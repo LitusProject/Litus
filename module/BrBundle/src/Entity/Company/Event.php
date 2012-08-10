5c34ae8c49f275b340a1f6d038150f559d7eaecd
Kristof Marien <kristof.marien@litus.cc>
2012-08-10 08:01:41 +0200

    Company's event is a calendar event

--- module/BrBundle/src/Entity/Company/Event.php
+++ module/BrBundle/src/Entity/Company/Event.php
@@ -16,6 +16,7 @@
 namespace BrBundle\Entity\Company;
 
 use BrBundle\Entity\Company,
+    CalendarBundle\Entity\Nodes\Event as CommonEvent,
     DateTime;
 
 /**
@@ -24,10 +25,10 @@ use BrBundle\Entity\Company,
  * @Entity(repositoryClass="BrBundle\Repository\Company\Event")
  * @Table(name="br.company_events")
  */
-class Event 
+class Event
 {
     /**
-     * @var string The event's ID
+     * @var string The company event's ID
      *
      * @Id
      * @Column(type="bigint")
@@ -36,39 +37,13 @@ class Event
     private $id;
     
     /**
-     * @var string The event's name
+     * @var string The event
      *
-     * @Column(type="string", length=50)
-     */
-    private $name;
-    
-    /**
-     * @var string The event's location
      *
-     * @Column(type="string")
+     * @OneToOne(targetEntity="CalendarBundle\Entity\Nodes\Event", cascade={"persist", "remove"})
+     * @JoinColumn(name="event", referencedColumnName="id")
      */
-    private $location;
-    
-    /**
-     * @var string The event's start date
-     *
-     * @Column(name="start_date", type="datetime")
-     */
-    private $startDate;
-    
-    /**
-     * @var string The event's end date
-     *
-     * @Column(name="end_date", type="datetime")
-     */
-    private $endDate;
-    
-    /**
-     * @var string The description of the event
-     *
-     * @Column(type="text")
-     */
-    private $description;
+    private $event;
     
     /**
      * @var \BrBundle\Entity\Company The company of the event
@@ -79,26 +54,17 @@ class Event
     private $company;
     
     /**
-     * @param string $name The event's name
-     * @param string $location The company's location
-     * @param \DateTime $startDate The event's start date
-     * @param \DateTime $endDate The event's end date
-     * @param string $description The event's description
+     * @param \CalendarBundle\Entity\Nodes\Event $event
      * @param \BrBundle\Entity\Company $company The event's company
      */
-    public function __construct($name, $location, DateTime $startDate, DateTime $endDate, $description, Company $company)
+    public function __construct(CommonEvent $event, Company $company)
     {
-        $this->setName($name);
-        $this->setLocation($location);
-        $this->setStartDate($startDate);
-        $this->setEndDate($endDate);
-        $this->setDescription($description);
-        
+        $this->event = $event;
         $this->company = $company;
     }
     
     /**
-     * @return string
+     * @return integer
      */
     public function getId()
     {
@@ -106,104 +72,11 @@ class Event
     }
     
     /**
-     * @param string $name
-     * @return \BrBundle\Entity\Company\Event
+     * @return \BrBundle\Entity\Company
      */
-    public function setName($name)
+    public function getEvent()
     {
-        if ((null === $name) || !is_string($name))
-            throw new \InvalidArgumentException('Invalid name');
-            
-        $this->name = $name;
-        
-        return $this;
-    }
-    
-    /**
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-    
-    /**
-     * @param string $location
-     * @return \BrBundle\Entity\Company\Event
-     */
-    public function setLocation($location)
-    {
-        if ((null === $location) || !is_string($location))
-            throw new \InvalidArgumentException('Invalid location');
-            
-        $this->location = $location;
-        
-        return $this;
-    }
-    
-    /**
-     * @return string
-     */
-    public function getLocation()
-    {
-        return $this->location;
-    }
-    
-    /**
-     * @param \DateTime $startDate
-     * @return \BrBundle\Entity\Company\Event
-     */
-    public function setStartDate(DateTime $startDate)
-    {
-        $this->startDate = $startDate;
-        
-        return $this;
-    }
-    
-    /**
-     * @return \DateTime
-     */
-    public function getStartDate()
-    {
-        return $this->startDate;
-    }
-    
-    /**
-     * @param \DateTime $endDate
-     * @return \BrBundle\Entity\Company\Event
-     */
-    public function setEndDate(DateTime $endDate)
-    {
-        $this->endDate = $endDate;
-        
-        return $this;
-    }
-    
-    /**
-     * @return \DateTime
-     */
-    public function getEndDate()
-    {
-        return $this->endDate;
-    }
-    
-    /**
-     * @param string $description
-     * @return \BrBundle\Entity\Company\Event
-     */
-    public function setDescription($description)
-    {
-        $this->description = $description;
-        
-        return $this;
-    }
-    
-    /**
-     * @return string
-     */
-    public function getDescription()
-    {
-        return $this->description;
+        return $this->event;
     }
     
     /**
