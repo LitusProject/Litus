96f665d2e1751ce2ff68754a70deb9579cd3083a
Kristof Marien <kristof.marien@litus.cc>
2012-08-11 14:26:28 +0200

    Forgot a program in syllabus

--- module/SyllabusBundle/src/Controller/Admin/InstallController.php
+++ module/SyllabusBundle/src/Controller/Admin/InstallController.php
@@ -138,6 +138,7 @@ class InstallController extends \CommonBundle\Component\Controller\ActionControl
                             'http://onderwijsaanbod.kuleuven.be/opleidingen/n/xml/SC_50630521.xml',
                             'http://onderwijsaanbod.kuleuven.be/opleidingen/n/xml/SC_50630513.xml',
                             'http://onderwijsaanbod.kuleuven.be/opleidingen/v/e/xml/SC_51016928.xml',
+                            'http://onderwijsaanbod.kuleuven.be/opleidingen/v/e/xml/SC_51016872.xml',
                         )
                     ),
                     'description' => 'The url to the xml',
