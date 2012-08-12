ac8f970a66883e8c8b24f27f67996a51b3ca8d45
Kristof Marien <kristof.marien@litus.cc>
2012-08-12 19:30:21 +0200

    Started with corporate app

---
+++ module/BrBundle/src/Controller/Corporate/IndexController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Litus is a project by a group of students from the K.U.Leuven. The goal is to create
+ * various applications to support the IT needs of student unions.
+ *
+ * @author Karsten Daemen <karsten.daemen@litus.cc>
+ * @author Bram Gotink <bram.gotink@litus.cc>
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ * @author Michiel Staessen <michiel.staessen@litus.cc>
+ * @author Alan Szepieniec <alan.szepieniec@litus.cc>
+ *
+ * @license http://litus.cc/LICENSE
+ */
+
+namespace BrBundle\Controller\Corporate;
+
+use CommonBundle\Component\FlashMessenger\FlashMessage,
+    Zend\View\Model\ViewModel;
+
+/**
+ * IndexController
+ *
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ */
+class IndexController extends \BrBundle\Component\Controller\CorporateController
+{
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+}
