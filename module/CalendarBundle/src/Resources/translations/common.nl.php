818303d7c7450ed11db3010fab75168e2e74a726
Kristof Marien <kristof.marien@litus.cc>
2012-08-18 17:06:40 +0200

    Updated calendar view

--- module/CalendarBundle/src/Resources/translations/common.nl.php
+++ module/CalendarBundle/src/Resources/translations/common.nl.php
@@ -2,7 +2,6 @@
 return array(
     'Calendar' => 'Kalender',
     'No events were created yet.' => 'Er zijn nog geen evenementen aangemaakt.',
-    'View' => 'View',
     'Previous' => 'Vorige',
     'Next' => 'Volgende',
 );
