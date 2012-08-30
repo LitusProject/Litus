fb6f00ff8079cec40189dc0847295b5b0f6d4bfb
Kristof Marien <kristof.marien@litus.cc>
2012-08-30 20:13:39 +0200

    Basic Sale Article management

--- module/CudiBundle/src/Form/Admin/Sales/Article/Add.php
+++ module/CudiBundle/src/Form/Admin/Sales/Article/Add.php
@@ -15,18 +15,17 @@
 
 namespace CudiBundle\Form\Admin\Sales\Article;
 
-use CommonBundle\Component\Form\Admin\Decorator\ButtonDecorator,
-    CommonBundle\Component\Form\Admin\Decorator\FieldDecorator,
+use CommonBundle\Component\Form\Admin\Element\Checkbox,
+    CommonBundle\Component\Form\Admin\Element\Select,
+    CommonBundle\Component\Form\Admin\Element\Text,
     CommonBundle\Component\Validator\Price as PriceValidator,
     CommonBundle\Entity\General\AcademicYear as AcademicYear,
     CudiBundle\Component\Validator\UniqueArticleBarcode as UniqueArticleBarcodeValidator,
     CudiBundle\Entity\Sales\Article,
     Doctrine\ORM\EntityManager,
-    Zend\Form\Element\Checkbox,
-    Zend\Form\Element\Select,
-    Zend\Form\Element\Submit,
-    Zend\Form\Element\Text,
-    Zend\Validator\Barcode as BarcodeValidator;
+    Zend\InputFilter\InputFilter,
+    Zend\InputFilter\Factory as InputFactory,
+    Zend\Form\Element\Submit;
 
 /**
  * Add Article
@@ -40,69 +39,61 @@ use CommonBundle\Component\Form\Admin\Decorator\ButtonDecorator,
      */
     protected $_entityManager = null;
 
-    public function __construct(EntityManager $entityManager, AcademicYear $academicYear, $opts = null)
+    /**
+     * @var \CommonBundle\Entity\General\AcademicYear
+     */
+    protected $_academicYear;
+
+    /**
+     * @param \Doctrine\ORM\EntityManager $entityManager The EntityManager instance
+     * @param \CommonBundle\Entity\General\AcademicYear $academicYear
+     * @param null|string|int $name Optional name for the element
+     */
+    public function __construct(EntityManager $entityManager, AcademicYear $academicYear, $name = null)
     {
-        parent::__construct($opts);
+        parent::__construct($name);
 
         $this->_entityManager = $entityManager;
+        $this->_academicYear = $academicYear;
 
         $field = new Text('purchase_price');
         $field->setLabel('Purchase Price')
-            ->setRequired()
-            ->setDecorators(array(new FieldDecorator()))
-            ->addValidator(new PriceValidator());
-        $this->addElement($field);
+            ->setRequired();
+        $this->add($field);
 
         $field = new Text('sell_price');
         $field->setLabel('Sell Price')
-            ->setRequired()
-            ->setDecorators(array(new FieldDecorator()))
-            ->addValidator(new PriceValidator());
-        $this->addElement($field);
+            ->setRequired();
+        $this->add($field);
 
         $field = new Text('barcode');
         $field->setLabel('Barcode')
-            ->setAttrib('class', 'disableEnter')
-            ->setRequired()
-            ->addValidator(
-                new BarcodeValidator(
-                    array(
-                        'adapter'     => 'Ean12',
-                        'useChecksum' => false,
-                    )
-                )
-            )
-            ->addValidator(new UniqueArticleBarcodeValidator($this->_entityManager, $academicYear))
-            ->setDecorators(array(new FieldDecorator()));
-        $this->addElement($field);
+            ->setAttribute('class', 'disableEnter')
+            ->setRequired();
+        $this->add($field);
 
         $field = new Select('supplier');
         $field->setLabel('Supplier')
             ->setRequired()
-            ->setMultiOptions($this->_getSuppliers())
-            ->setDecorators(array(new FieldDecorator()));
-        $this->addElement($field);
+            ->setAttribute('options', $this->_getSuppliers());
+        $this->add($field);
 
         $field = new Checkbox('bookable');
-        $field->setLabel('Bookable')
-            ->setDecorators(array(new FieldDecorator()));
-        $this->addElement($field);
+        $field->setLabel('Bookable');
+        $this->add($field);
 
         $field = new Checkbox('unbookable');
-        $field->setLabel('Unbookable')
-            ->setDecorators(array(new FieldDecorator()));
-        $this->addElement($field);
+        $field->setLabel('Unbookable');
+        $this->add($field);
 
         $field = new Checkbox('can_expire');
-        $field->setLabel('Can Expire')
-            ->setDecorators(array(new FieldDecorator()));
-        $this->addElement($field);
+        $field->setLabel('Can Expire');
+        $this->add($field);
 
         $field = new Submit('submit');
-        $field->setLabel('Add')
-                ->setAttrib('class', 'article_add')
-                ->setDecorators(array(new ButtonDecorator()));
-        $this->addElement($field);
+        $field->setValue('Add')
+            ->setAttribute('class', 'article_add');
+        $this->add($field);
     }
 
     private function _getSuppliers()
@@ -119,14 +110,88 @@ use CommonBundle\Component\Form\Admin\Decorator\ButtonDecorator,
 
     public function populateFromArticle(Article $article)
     {
-        $this->populate(array(
-            'purchase_price' => number_format($article->getPurchasePrice()/100, 2),
-            'sell_price' => number_format($article->getSellPrice()/100, 2),
-            'barcode' => $article->getBarcode(),
-            'supplier' => $article->getSupplier()->getId(),
-            'bookable' => $article->isBookable(),
-            'unbookable' => $article->isUnbookable(),
-            'can_expire' => $article->canExpire()
-        ));
+        $this->setData(
+            array(
+                'purchase_price' => number_format($article->getPurchasePrice()/100, 2),
+                'sell_price' => number_format($article->getSellPrice()/100, 2),
+                'barcode' => $article->getBarcode(),
+                'supplier' => $article->getSupplier()->getId(),
+                'bookable' => $article->isBookable(),
+                'unbookable' => $article->isUnbookable(),
+                'can_expire' => $article->canExpire()
+            )
+        );
+    }
+
+    public function getInputFilter()
+    {
+        if ($this->_inputFilter == null) {
+            $inputFilter = new InputFilter();
+            $factory = new InputFactory();
+
+            $inputFilter->add(
+                $factory->createInput(
+                    array(
+                        'name'     => 'purchase_price',
+                        'required' => true,
+                        'filters'  => array(
+                            array('name' => 'StringTrim'),
+                        ),
+                        'validators' => array(
+                            new PriceValidator(),
+                        ),
+                    )
+                )
+            );
+
+            $inputFilter->add(
+                $factory->createInput(
+                    array(
+                        'name'     => 'sell_price',
+                        'required' => true,
+                        'filters'  => array(
+                            array('name' => 'StringTrim'),
+                        ),
+                        'validators' => array(
+                            new PriceValidator(),
+                        ),
+                    )
+                )
+            );
+
+            $inputFilter->add(
+                $factory->createInput(
+                    array(
+                        'name'     => 'barcode',
+                        'required' => true,
+                        'filters'  => array(
+                            array('name' => 'StringTrim'),
+                        ),
+                        'validators' => array(
+                            array(
+                                'name' => 'barcode',
+                                'options' => array(
+                                    'adapter'     => 'Ean12',
+                                    'useChecksum' => false,
+                                ),
+                            ),
+                            new UniqueArticleBarcodeValidator($this->_entityManager, $this->_academicYear),
+                        ),
+                    )
+                )
+            );
+
+            $inputFilter->add(
+                $factory->createInput(
+                    array(
+                        'name'     => 'supplier',
+                        'required' => true,
+                    )
+                )
+            );
+
+            $this->_inputFilter = $inputFilter;
+        }
+        return $this->_inputFilter;
     }
 }
