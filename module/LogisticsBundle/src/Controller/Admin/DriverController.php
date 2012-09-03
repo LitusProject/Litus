967288593b3908e912d7695f78a4abbccb8e8d58
Niels Avonds <niels.avonds@litus.cc>
2012-09-03 21:56:47 +0200

    Added navigation bar and add action

--- module/LogisticsBundle/src/Controller/Admin/DriverController.php
+++ module/LogisticsBundle/src/Controller/Admin/DriverController.php
@@ -20,7 +20,13 @@ use \Zend\View\Model\ViewModel;
 
 class DriverController extends \CommonBundle\Component\Controller\ActionController\AdminController
 {
-    public function manageAction() {
+    public function manageAction()
+    {
+        return new ViewModel();
+    }
+    
+    public function addAction()
+    {
         return new ViewModel();
     }
 }
--- module/LogisticsBundle/src/Resources/config/module.config.php
+++ module/LogisticsBundle/src/Resources/config/module.config.php
@@ -3,13 +3,13 @@
  * Litus is a project by a group of students from the K.U.Leuven. The goal is to create
  * various applications to support the IT needs of student unions.
  *
+ * @author Niels Avonds <niels.avonds@litus.cc>
  * @author Karsten Daemen <karsten.daemen@litus.cc>
  * @author Bram Gotink <bram.gotink@litus.cc>
  * @author Pieter Maene <pieter.maene@litus.cc>
  * @author Kristof Mariën <kristof.marien@litus.cc>
  * @author Michiel Staessen <michiel.staessen@litus.cc>
  * @author Alan Szepieniec <alan.szepieniec@litus.cc>
- * @author Niels Avonds <niels.avonds@litus.cc>
  *
  * @license http://litus.cc/LICENSE
  */
@@ -20,7 +20,14 @@ return array(
 			'admin_driver' => array(
 				'type'    => 'Zend\Mvc\Router\Http\Segment',
 				'options' => array(
-					'route' => '/admin/logistics/driver',
+                    'route' => '/admin/driver[/:action[/:id][/page/:page][/:field/:string]]',
+                    'constraints' => array(
+                        'action'  => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'      => '[0-9]*',
+                        'field'   => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'string'  => '[%a-zA-Z0-9_-]*',
+                        'page'    => '[0-9]*',
+                    ),
 					'defaults' => array(
 						'controller' => 'admin_driver',
 						'action'     => 'manage',
@@ -31,7 +38,7 @@ return array(
 	),
     'view_manager' => array(
         'template_path_stack' => array(
-            'mail_view' => __DIR__ . '/../views',
+            'driver_view' => __DIR__ . '/../views',
         ),
     ),
     'doctrine' => array(
