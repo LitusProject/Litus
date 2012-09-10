ecfb67764a9197eca13ce1a429a73be6470fc08b
Niels Avonds <niels.avonds@litus.cc>
2012-09-10 17:27:55 +0200

    Added reservation controller

---
+++ module/CudiBundle/src/Controller/Reservation/ReservationController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Litus is a project by a group of students from the K.U.Leuven. The goal is to create
+ * various applications to support the IT needs of student unions.
+ *
+ * @author Niels Avonds <niels.avonds@litus.cc>
+ * @author Karsten Daemen <karsten.daemen@litus.cc>
+ * @author Bram Gotink <bram.gotink@litus.cc>
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ * @author Michiel Staessen <michiel.staessen@litus.cc>
+ * @author Alan Szepieniec <alan.szepieniec@litus.cc>
+ *
+ * @license http://litus.cc/LICENSE
+ */
+
+namespace CudiBundle\Controller\Reservation;
+
+use Zend\View\Model\ViewModel;
+
+/**
+ * ReservationController
+ *
+ * @author Niels Avonds <niels.avonds@litus.cc>
+ */
+class ReservationController extends \CommonBundle\Component\Controller\ActionController\SiteController
+{
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+}
