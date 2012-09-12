3698a0f8abed5812ac5e12f675e18f32923b73a1
Niels Avonds <niels.avonds@litus.cc>
2012-09-12 14:56:25 +0200

    Automatic return to logistics view

--- module/LogisticsBundle/src/Resources/config/module.config.php
+++ module/LogisticsBundle/src/Resources/config/module.config.php
@@ -47,14 +47,13 @@ return array(
 			'admin_van_reservation' => array(
 			    'type'    => 'Zend\Mvc\Router\Http\Segment',
 			    'options' => array(
-			        'route' => '/admin/van_reservation[/:action[/:id][/page/:page][/start/:start][/end/:end][/:field/:string]]',
+			        'route' => '/admin/van_reservation[/:action[/:id][/page/:page][/start/:start][/end/:end][/return/:return]]',
 			        'constraints' => array(
 			            'action'  => '[a-zA-Z][a-zA-Z0-9_-]*',
 			            'id'      => '[0-9]*',
-			            'start'      => '[0-9]*',
-			            'end'      => '[0-9]*',
-			            'field'   => '[a-zA-Z][a-zA-Z0-9_-]*',
-			            'string'  => '[%a-zA-Z0-9_-]*',
+			            'start'   => '[0-9]*',
+                        'return'  => '[a-zA-Z][a-zA-Z0-9_-]*',
+			            'end'     => '[0-9]*',
 			            'page'    => '[0-9]*',
 			        ),
 			        'defaults' => array(
