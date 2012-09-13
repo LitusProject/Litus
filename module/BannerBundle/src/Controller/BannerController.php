ce604237d2c1927f1d293a1f014bd024578bd31b
Niels Avonds <niels.avonds@litus.cc>
2012-09-13 17:17:48 +0200

    Working banners

---
+++ module/BannerBundle/src/Controller/BannerController.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Litus is a project by a group of students from the K.U.Leuven. The goal is to create
+ * various applications to support the IT needs of student unions.
+ *
+ * @author Niels Avonds <niels.avonds@litus.cc>
+ * @author Karsten Daemen <karsten.daemen@litus.cc>
+ * @author Bram Gotink <bram.gotink@litus.cc>
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ *
+ * @license http://litus.cc/LICENSE
+ */
+
+namespace BannerBundle\Controller;
+
+use CommonBundle\Component\FlashMessenger\FlashMessage,
+    DateTime,
+    BannerBundle\Entity\Nodes\Banner,
+    BannerBundle\Form\Admin\Banner\Add as AddForm,
+    BannerBundle\Form\Admin\Banner\Edit as EditForm,
+    Zend\File\Transfer\Adapter\Http as FileUpload,
+    Zend\Http\Headers,
+    Zend\View\Model\ViewModel;
+
+/**
+ * BannerController
+ *
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ * @author Niels Avonds <niels.avonds@litus.cc>
+ */
+class BannerController extends \CommonBundle\Component\Controller\ActionController\AdminController
+{
+
+    public function viewAction()
+    {
+        $imagePath = $this->getEntityManager()
+            ->getRepository('CommonBundle\Entity\General\Config')
+            ->getConfigValue('banner.image_path') . '/' . $this->getParam('image');
+
+        $headers = new Headers();
+        $headers->addHeaders(array(
+            'Content-Disposition' => 'inline; filename="' . $this->getParam('image') . '"',
+            'Content-type' => mime_content_type($imagePath),
+            'Content-Length' => filesize($imagePath),
+        ));
+        $this->getResponse()->setHeaders($headers);
+
+        $handle = fopen($imagePath, 'r');
+        $data = fread($handle, filesize($imagePath));
+        fclose($handle);
+
+        return new ViewModel(
+            array(
+                'data' => $data,
+            )
+        );
+    }
+}
