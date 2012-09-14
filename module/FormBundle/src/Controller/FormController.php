60ce9e2bf59e69f0a1f7e1517564db8eea91c7d8
Niels Avonds <niels.avonds@litus.cc>
2012-09-14 18:56:38 +0200

    Linked up admin pages

--- module/FormBundle/src/Controller/FormController.php
+++ module/FormBundle/src/Controller/FormController.php
@@ -56,7 +56,6 @@ class FormController extends \CommonBundle\Component\Controller\ActionController
             );
         }
 
-
         $form = new SpecifiedForm($formSpecification);
 
         if ($this->getRequest()->isPost()) {
