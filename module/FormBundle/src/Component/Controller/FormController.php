80d90882b0eace8e44bd600916a0b978612e07a5
Niels Avonds <niels.avonds@litus.cc>
2012-09-15 21:44:49 +0200

    Basic form manage page

---
+++ module/FormBundle/src/Component/Controller/FormController.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Litus is a project by a group of students from the K.U.Leuven. The goal is to create
+ * various applications to support the IT needs of student unions.
+ *
+ * @author Niels Avonds <niels.avonds@litus.cc>
+ * @author Karsten Daemen <karsten.daemen@litus.cc>
+ * @author Bram Gotink <bram.gotink@litus.cc>
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ *
+ * @license http://litus.cc/LICENSE
+ */
+
+namespace FormBundle\Component\Controller;
+
+use CommonBundle\Component\Controller\Exception\HasNoAccessException,
+    CommonBundle\Component\FlashMessenger\FlashMessage,
+    CommonBundle\Form\Auth\Login as LoginForm,
+    Zend\Mvc\MvcEvent;
+
+/**
+ * We extend the CommonBundle controller.
+ *
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ */
+class FormController extends \CommonBundle\Component\Controller\ActionController
+{
+    /**
+     * Execute the request
+     *
+     * @param \Zend\Mvc\MvcEvent $e The MVC event
+     * @return array
+     * @throws \CommonBundle\Component\Controller\Exception\HasNoAccessException The user does not have permissions to access this resource
+     */
+    public function onDispatch(MvcEvent $e)
+    {
+        $result = parent::onDispatch($e);
+
+        $result->loginForm = new LoginForm($this->url()->fromRoute('form_manage_auth', array('action' => 'login')));
+        $result->unionUrl = $this->getEntityManager()
+            ->getRepository('CommonBundle\Entity\General\Config')
+            ->getConfigValue('union_url');
+
+        $e->setResult($result);
+        return $result;
+    }
+
+    /**
+     * We need to be able to specify all required authentication information,
+     * which depends on the part of the site that is currently being used.
+     *
+     * @return array
+     */
+    public function getAuthenticationHandler()
+    {
+        return array(
+            'action'         => 'index',
+            'controller'     => 'index',
+
+            'auth_route'     => 'form_manage_index',
+            'redirect_route' => 'form_manage_index'
+        );
+    }
+}
