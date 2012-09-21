bc90cb6f2f8c69096c0fc4685995b18eb42ee969
Niels Avonds <niels.avonds@vtk.be>
2012-09-21 20:02:50 +0200

    Static translations for manage page

---
+++ module/FormBundle/src/Resources/translations/manage.en.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Litus is a project by a group of students from the K.U.Leuven. The goal is to create
+ * various applications to support the IT needs of student unions.
+ *
+ * @author Niels Avonds <niels.avonds@litus.cc>
+ * @author Karsten Daemen <karsten.daemen@litus.cc>
+ * @author Bram Gotink <bram.gotink@litus.cc>
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ *
+ * @license http://litus.cc/LICENSE
+ */
+
+return array(
+    'Forms' => 'Forms',
+
+    'Your Forms' => 'Your Forms',
+    'Start Date' => 'Start Date',
+    'End Date' => 'End Date',
+    'Active' => 'Active',
+
+    'Submitter' => 'Submitter',
+    'Submitted' => 'Submitted',
+
+    'The entry was succesfully removed.' => 'The entry was succesfully removed.',
+    'An error occurred while removing the entry.' => 'An error occurred while removing the entry.',
+
+    'Delete Entry' => 'Delete Entry',
+    'You are about to delete the following user\'s entry' => 'You are about to delete the following user\'s entry',
+
+);
