a7639a2c5be9b86c992943dc292e6e0dc297fde5
Niels Avonds <niels.avonds@vtk.be>
2012-09-21 15:47:43 +0200

    Fixed calendar page config

--- module/CalendarBundle/src/Resources/config/module.config.php
+++ module/CalendarBundle/src/Resources/config/module.config.php
@@ -49,7 +49,7 @@ return array(
                     'constraints' => array(
                         'language' => '[a-z]{2}',
                         'action'   => '[a-zA-Z][a-zA-Z0-9_-]*',
-                        'id'       => '[0-9\-]*',
+                        'id'       => '[0-9\-_]*',
                     ),
                     'defaults' => array(
                         'controller' => 'calendar',
