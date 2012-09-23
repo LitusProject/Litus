b482132b41de967ef6c801ce17d66fe4a6132b1c
Niels Avonds <niels.avonds@vtk.be>
2012-09-23 13:43:11 +0200

    Forgot these files

---
+++ module/FormBundle/src/Entity/Fields/Checkbox.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Litus is a project by a group of students from the K.U.Leuven. The goal is to create
+ * various applications to support the IT needs of student unions.
+ *
+ * @author Niels Avonds <niels.avonds@litus.cc>
+ * @author Karsten Daemen <karsten.daemen@litus.cc>
+ * @author Bram Gotink <bram.gotink@litus.cc>
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ *
+ * @license http://litus.cc/LICENSE
+ */
+
+namespace FormBundle\Entity\Fields;
+
+use CommonBundle\Entity\General\Language,
+    CommonBundle\Entity\Users\Person,
+    CommonBundle\Component\Util\Url,
+    DateTime,
+    Doctrine\Common\Collections\ArrayCollection,
+    Doctrine\ORM\Mapping as ORM,
+    FormBundle\Entity\Field;
+
+/**
+ * This entity stores the node item.
+ *
+ * @ORM\Entity(repositoryClass="FormBundle\Repository\Fields\Checkbox")
+ * @ORM\Table(name="forms.field_checkbox")
+ */
+class Checkbox extends Field
+{
+
+    /**
+     * @param FormBundle\Entity\Nodes\Form $form
+     * @param integer $order
+     * @param bool $required
+     */
+    public function __construct($form, $order, $required)
+    {
+        parent::__construct($form, $order, $required);
+    }
+
+    public function getValueString(Language $language, $value) {
+        return $value ? 'X' : '';
+    }
+
+}
