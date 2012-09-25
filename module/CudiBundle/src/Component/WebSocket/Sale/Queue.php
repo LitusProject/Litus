c9e25eeaad8ac1f95bf1a7c975548a4ac7f5481d
Kristof Marien <krmarien@gmail.com>
2012-09-25 13:57:03 +0200

    Fix

--- module/CudiBundle/src/Component/WebSocket/Sale/Queue.php
+++ module/CudiBundle/src/Component/WebSocket/Sale/Queue.php
@@ -361,7 +361,7 @@ class Queue extends \CommonBundle\Component\WebSocket\Server
         if (!isset($item))
             return;
 
-        $this->_printQueueTicket($item, 'collect');
+        $this->_printQueueTicket($item, $user->getExtraData('payDesk'));
 
         if ($enableCollectScanning !== '1')
             return;
@@ -704,9 +704,6 @@ class Queue extends \CommonBundle\Component\WebSocket\Server
 
     private function _getCurrentAcademicYear()
     {
-        return $this->_entityManager
-            ->getRepository('CommonBundle\Entity\General\AcademicYear')
-            ->findOneById(2); // TODO: remove this
         $startAcademicYear = AcademicYear::getStartOfAcademicYear();
         $startAcademicYear->setTime(0, 0);
 
