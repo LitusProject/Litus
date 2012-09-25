4896e62e7e57ce28209318d0fb61982aea443a45
Kristof Marien <kristof.marien@litus.cc>
2012-09-25 09:55:13 +0200

    socket improvement

--- module/CudiBundle/src/Component/WebSocket/Sale/Printer.php
+++ module/CudiBundle/src/Component/WebSocket/Sale/Printer.php
@@ -56,6 +56,8 @@ class Printer {
             return;
 
         $data = 'PRINT ' . $printers[$printer] . ' ' . $type . ' ' . $data;
+        $now = new DateTime();
+        echo '[' . $now->format('d/m/Y H:i:s') . ']:' . $data . PHP_EOL;
         $socket = socket_create(AF_INET, SOCK_STREAM, getprotobyname('tcp'));
         socket_connect(
             $socket,
