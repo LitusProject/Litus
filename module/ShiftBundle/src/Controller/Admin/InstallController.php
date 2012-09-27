0df42284ec308b3ca198c375d89f3180d0e52d30
Niels Avonds <niels.avonds@vtk.be>
2012-09-27 11:23:24 +0200

    Added mail to shifts

--- module/ShiftBundle/src/Controller/Admin/InstallController.php
+++ module/ShiftBundle/src/Controller/Admin/InstallController.php
@@ -38,6 +38,61 @@ class InstallController extends \CommonBundle\Component\Controller\ActionControl
                     'value'       => 'P12H',
                     'description' => 'The date interval after which a responsible cannot be signed out from a shift',
                 ),
+                array(
+                    'key'         => 'shiftbundle.mail',
+                    'value'       => 'it@vtk.be',
+                    'description' => 'The mail address from which shift notifications are sent',
+                ),
+                array(
+                    'key'         => 'shiftbundle.mail_name',
+                    'value'       => 'VTK IT',
+                    'description' => 'The name of the mail address from which shift notifications are sent',
+                ),
+                array(
+                    'key'         => 'shiftbundle.praesidium_removed_mail',
+                    'value'       => 'Dear,
+
+You have been removed from the following shift by a non-praesidium volunteer:
+{{ shift }}
+
+-- This is an automatically generated email, please do not reply --',
+                    'description' => 'The mail sent to a praesidium member when a volunteer removes him from a shift.',
+                ),
+                array(
+                    'key'         => 'shiftbundle.praesidium_removed_mail_subject',
+                    'value'       => 'Shift Unsubscription',
+                    'description' => 'The subject of the mail sent to a praesidium member when a volunteer removes him from a shift.',
+                ),
+                array(
+                    'key'         => 'shiftbundle.shift_deleted_mail',
+                    'value'       => 'Dear,
+
+The following shift to which you were subscribed has been deleted:
+{{ shift }}
+
+-- This is an automatically generated email, please do not reply --',
+                    'description' => 'The mail sent to a shift subscriber when the shift is deleted.',
+                ),
+                array(
+                    'key'         => 'shiftbundle.shift_deleted_mail_subject',
+                    'value'       => 'Shift Deleted',
+                    'description' => 'The subject of the mail sent to a shift subscriber when the shift is deleted.',
+                ),
+                array(
+                    'key'         => 'shiftbundle.subscription_deleted_mail',
+                    'value'       => 'Dear,
+
+You have been removed from the following shift by an administrator:
+{{ shift }}
+
+-- This is an automatically generated email, please do not reply --',
+                    'description' => 'The mail sent to a shift subscriber when he is removed from the shift.',
+                ),
+                array(
+                    'key'         => 'shiftbundle.subscription_deleted_mail_subject',
+                    'value'       => 'Shift Unsubscription',
+                    'description' => 'The subject of the mail sent to a shift subscriber when he is removed from the shift.',
+                ),
             )
         );
     }
