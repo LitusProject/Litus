0a8f678fb9d7c0b55a4ddf381898a6b979c1a1be
Kristof Marien <krmarien@gmail.com>
2012-09-27 12:51:39 +0200

    Queue fix

--- module/CudiBundle/src/Resources/assets/sale/js/shortKey.js
+++ module/CudiBundle/src/Resources/assets/sale/js/shortKey.js
@@ -1,6 +1,8 @@
 (function ($) {
 	$('body').bind('keydown.shortKey', function (e) {
 		$('[data-key]:visible:not([data-dismiss="key"])').each(function () {
+            if ($(this).offset().top < 0)
+                return;
 			if (e.which == $(this).data('key')) {
 				e.preventDefault();
 				$(this).click();
