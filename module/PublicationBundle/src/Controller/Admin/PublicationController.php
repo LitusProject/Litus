6b888ca3f270f56a4d28d4a497100a72f1f0cbc9
Niels Avonds <niels.avonds@litus.cc>
2012-09-28 16:17:34 +0200

    Publication bundle start

---
+++ module/PublicationBundle/src/Controller/Admin/PublicationController.php
@@ -0,0 +1,166 @@
+<?php
+/**
+ * Litus is a project by a group of students from the K.U.Leuven. The goal is to create
+ * various applications to support the IT needs of student unions.
+ *
+ * @author Niels Avonds <niels.avonds@litus.cc>
+ * @author Karsten Daemen <karsten.daemen@litus.cc>
+ * @author Bram Gotink <bram.gotink@litus.cc>
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ *
+ * @license http://litus.cc/LICENSE
+ */
+
+namespace PublicationBundle\Controller\Admin;
+
+use CommonBundle\Component\FlashMessenger\FlashMessage,
+    Zend\View\Model\ViewModel;
+
+class PublicationController extends \CommonBundle\Component\Controller\ActionController\AdminController
+{
+    public function manageAction()
+    {
+        $paginator = $this->paginator()->createFromEntity(
+            'PublicationBundle\Entity\Publication',
+            $this->getParam('page')
+        );
+
+        return new ViewModel(
+            array(
+                'paginator' => $paginator,
+                'paginationControl' => $this->paginator()->createControl(true),
+            )
+        );
+    }
+
+    public function addAction()
+    {
+        $form = new Add($this->getEntityManager());
+
+        if($this->getRequest()->isPost()) {
+            // Form is being posted, persist the new driver.
+            $formData = $this->getRequest()->getPost();
+            $form->setData($formData);
+
+            if ($form->isValid()) {
+                $repository = $this->getEntityManager()
+                    ->getRepository('CommonBundle\Entity\Users\People\Academic');
+                if ($formData['person_id'] == '') {
+
+                    // No autocompletion used, we assume the username was entered
+                    $person = $repository->findOneByUsername($formData['person_name']);
+                } else {
+                    $person = $repository->findOneById($formData['person_id']);
+                }
+
+                $yearIds = $formData['years'];
+                $years = array();
+                $repository = $this->getEntityManager()
+                    ->getRepository('CommonBundle\Entity\General\AcademicYear');
+                foreach($yearIds as $yearId) {
+                    $years[] = $repository->findOneById($yearId);
+                }
+
+                $color = $formData['color'];
+
+                $driver = new Driver($person, $color);
+                $driver->setYears($years);
+                $this->getEntityManager()->persist($driver);
+                $this->getEntityManager()->flush();
+
+                $this->flashMessenger()->addMessage(
+                    new FlashMessage(
+                        FlashMessage::SUCCESS,
+                        'SUCCES',
+                        'The driver was succesfully created!'
+                    )
+                );
+
+                $this->redirect()->toRoute(
+                    'admin_driver',
+                    array(
+                        'action' => 'manage',
+                    )
+                );
+
+                return new ViewModel();
+            }
+        }
+
+        return new ViewModel(
+            array(
+                'form' => $form,
+            )
+        );
+    }
+
+    public function editAction()
+    {
+        if (!($driver = $this->_getDriver()))
+            return new ViewModel();
+
+        $form = new Edit($this->getEntityManager(), $driver);
+
+        if($this->getRequest()->isPost()) {
+            $formData = $this->getRequest()->getPost();
+            $form->setData($formData);
+
+            if ($form->isValid()) {
+
+                $yearIds = $formData['years'];
+                $years = array();
+                $repository = $this->getEntityManager()
+                    ->getRepository('CommonBundle\Entity\General\AcademicYear');
+                foreach($yearIds as $yearId) {
+                    $years[] = $repository->findOneById($yearId);
+                }
+
+                $driver->setColor($formData['color']);
+                $driver->setYears($years);
+
+                $this->getEntityManager()->flush();
+
+                $this->flashMessenger()->addMessage(
+                    new FlashMessage(
+                        FlashMessage::SUCCESS,
+                        'SUCCESS',
+                        'The driver was successfully updated!'
+                    )
+                );
+
+                $this->redirect()->toRoute(
+                    'admin_driver',
+                    array(
+                        'action' => 'manage'
+                    )
+                );
+
+                return new ViewModel();
+            }
+        }
+
+        return new ViewModel(
+            array(
+                'form' => $form,
+            )
+        );
+    }
+
+    public function deleteAction()
+    {
+        $this->initAjax();
+
+        if (!($driver = $this->_getDriver()))
+            return new ViewModel();
+
+        $this->getEntityManager()->remove($driver);
+        $this->getEntityManager()->flush();
+
+        return new ViewModel(
+            array(
+                'result' => (object) array("status" => "success"),
+            )
+        );
+    }
+}
---
+++ module/PublicationBundle/src/Entity/Publication.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Litus is a project by a group of students from the K.U.Leuven. The goal is to create
+ * various applications to support the IT needs of student unions.
+ *
+ * @author Niels Avonds <niels.avonds@litus.cc>
+ * @author Karsten Daemen <karsten.daemen@litus.cc>
+ * @author Bram Gotink <bram.gotink@litus.cc>
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ *
+ * @license http://litus.cc/LICENSE
+ */
+namespace PublicationBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM,
+    Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * This is the entity for a publication
+ *
+ * @ORM\Entity(repositoryClass="PublicationBundle\Repository\Publication")
+ * @ORM\Table(name="publications.publications")
+ */
+class Publication
+{
+
+    /**
+     * @var integer The ID of this article
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="bigint")
+     */
+    private $id;
+
+    /**
+     * @var string The title of this publication.
+     *
+     * @ORM\Column(type="string", nullable=false)
+     */
+    private $title;
+
+    /**
+     * @var boolean Indicates whether this publication is history.
+     *
+     * @ORM\Column(type="boolean", nullable=false)
+     */
+    private $deleted;
+
+    /**
+     * Creates a new publication with the given title
+     *
+     * @param string $title The title of this publication
+     */
+    public function __construct($title)
+    {
+        $this->title = $title;
+        $this->deleted = false;
+    }
+
+    /**
+     * @return string The title of this publication
+     */
+    public function getTitle()
+    {
+        return $this->person;
+    }
+
+    /**
+     * @param $title The new title
+     * @return \PublicationBundle\Entity\Publiation This
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+        return $this;
+    }
+}
