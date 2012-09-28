8b56886677303574c01aa6544404a60821d62eea
Niels Avonds <niels.avonds@litus.cc>
2012-09-28 23:46:43 +0200

    Forgot config

--- config/assetic/publicationbundle.config.php
+++ config/assetic/publicationbundle.config.php
@@ -34,6 +34,13 @@ return array(
             '@bootstrap_js_transition',
             '@bootstrap_js_modal',
         ),
+        'admin_edition_html' => array(
+            '@common_jquery',
+            '@admin_css',
+            '@admin_js',
+            '@bootstrap_js_transition',
+            '@bootstrap_js_modal',
+        ),
     ),
     'routes' => array(),
 );
