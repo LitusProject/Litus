3e02168ea39a2d879ab37b2c337a4b095565a9e5
Pieter Maene <pieter.maene@litus.cc>
2012-09-30 22:00:07 +0200

    Bugfix

--- module/MailBundle/autoload_classmap.php
+++ module/MailBundle/autoload_classmap.php
@@ -7,6 +7,7 @@ return array(
     'MailBundle\Controller\Admin\MailController'    => __DIR__ . '//src/Controller/Admin/MailController.php',
     'MailBundle\Controller\Admin\ProfController'    => __DIR__ . '//src/Controller/Admin/ProfController.php',
     'MailBundle\Controller\Admin\StudyController'   => __DIR__ . '//src/Controller/Admin/StudyController.php',
+    'MailBundle\Form\Admin\Bakske\Mail'             => __DIR__ . '//src/Form/Admin/Bakske/Mail.php',
     'MailBundle\Form\Admin\Cudi\Mail'               => __DIR__ . '//src/Form/Admin/Cudi/Mail.php',
     'MailBundle\Form\Admin\Mail\Mail'               => __DIR__ . '//src/Form/Admin/Mail/Mail.php',
     'MailBundle\Form\Admin\Study\Mail'              => __DIR__ . '//src/Form/Admin/Study/Mail.php',
