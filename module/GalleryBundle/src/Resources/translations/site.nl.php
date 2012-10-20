93d3f6929279f32a02d197a3b108de584558a764
Kristof Marien <krmarien@gmail.com>
2012-10-20 15:13:39 +0200

    Forgot translations

--- module/GalleryBundle/src/Resources/translations/site.nl.php
+++ module/GalleryBundle/src/Resources/translations/site.nl.php
@@ -4,6 +4,8 @@ return array(
     'Close' => 'Sluit',
     'Previous' => 'Vorige',
     'Next' => 'Volgende',
+    'Censor' => 'Censureer',
+    'Uncensor' => 'Decensureer',
     'Archive' => 'Archief',
     'The complete archive can be viewed <a href="%url%">here</a>.' => 'Het volledige archief kan <a href="%url%">hier</a> geraadpleegd worden',
 );
