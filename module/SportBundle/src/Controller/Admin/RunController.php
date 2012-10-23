8864f84eabce8f43235c8f212061fb33c322cddb
Kristof Marien <krmarien@gmail.com>
2012-10-23 20:35:59 +0200

    Show previous laps

--- module/SportBundle/src/Controller/Admin/RunController.php
+++ module/SportBundle/src/Controller/Admin/RunController.php
@@ -15,6 +15,9 @@
 namespace SportBundle\Controller\Admin;
 
 use CommonBundle\Component\FlashMessenger\FlashMessage,
+    CommonBundle\Component\Util\AcademicYear,
+    DateInterval,
+    DateTime,
     SportBundle\Form\Admin\Runner\Edit as EditForm,
     Zend\View\Model\ViewModel;
 
@@ -39,6 +42,23 @@ class RunController extends \CommonBundle\Component\Controller\ActionController\
         return new ViewModel(
             array(
                 'socketUrl' => $this->getSocketUrl(),
+            )
+        );
+    }
+
+    public function lapsAction()
+    {
+        $paginator = $this->paginator()->createFromArray(
+            $this->getEntityManager()
+                ->getRepository('SportBundle\Entity\Lap')
+                ->findAllPreviousLaps($this->_getAcademicYear()),
+            $this->getParam('page')
+        );
+
+        return new ViewModel(
+            array(
+                'paginator' => $paginator,
+                'paginationControl' => $this->paginator()->createControl(true),
             )
         );
     }
@@ -182,5 +202,65 @@ class RunController extends \CommonBundle\Component\Controller\ActionController\
             ->getConfigValue('sport.queue_socket_port');
 
         return 'ws://' . $address . ':' . $port;
+    }
+
+    private function _getAcademicYear()
+    {
+        if (null === $this->getParam('academicyear')) {
+            $startAcademicYear = AcademicYear::getStartOfAcademicYear();
+
+            $start = new DateTime(
+                str_replace(
+                    '{{ year }}',
+                    $startAcademicYear->format('Y'),
+                    $this->getEntityManager()
+                        ->getRepository('CommonBundle\Entity\General\Config')
+                        ->getConfigValue('start_organization_year')
+                )
+            );
+
+            $next = clone $start;
+            $next->add(new DateInterval('P1Y'));
+            if ($next <= new DateTime())
+                $start = $next;
+        } else {
+            $startAcademicYear = AcademicYear::getDateTime($this->getParam('academicyear'));
+
+            $start = new DateTime(
+                str_replace(
+                    '{{ year }}',
+                    $startAcademicYear->format('Y'),
+                    $this->getEntityManager()
+                        ->getRepository('CommonBundle\Entity\General\Config')
+                        ->getConfigValue('start_organization_year')
+                )
+            );
+        }
+        $startAcademicYear->setTime(0, 0);
+
+        $academicYear = $this->getEntityManager()
+            ->getRepository('CommonBundle\Entity\General\AcademicYear')
+            ->findOneByStart($start);
+
+        if (null === $academicYear) {
+            $this->flashMessenger()->addMessage(
+                new FlashMessage(
+                    FlashMessage::ERROR,
+                    'Error',
+                    'No academic year was found!'
+                )
+            );
+
+            $this->redirect()->toRoute(
+                'admin_shift_counter',
+                array(
+                    'action' => 'index'
+                )
+            );
+
+            return;
+        }
+
+        return $academicYear;
     }
 }
