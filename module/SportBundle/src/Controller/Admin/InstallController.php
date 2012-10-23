7b6f45c870aa06d638353e22ccfa2aaff0b429b7
Kristof Marien <krmarien@gmail.com>
2012-10-23 20:40:55 +0200

    Forgot acl

--- module/SportBundle/src/Controller/Admin/InstallController.php
+++ module/SportBundle/src/Controller/Admin/InstallController.php
@@ -62,7 +62,7 @@ class InstallController extends \CommonBundle\Component\Controller\ActionControl
             array(
                 'sportbundle' => array(
                     'admin_run' => array(
-                        'edit', 'groups', 'identification', 'queue', 'update'
+                        'edit', 'groups', 'identification', 'queue', 'update', 'laps'
                     ),
                     'run_group' => array(
                         'add', 'getName'
