e002ea4a5a7bda9d3af9fe2ef61a856a14bd5b3f
Niels Avonds <niels.avonds@gmail.com>
2012-10-28 00:11:22 +0200

    Old tab for van reservations

--- module/LogisticsBundle/src/Controller/Admin/InstallController.php
+++ module/LogisticsBundle/src/Controller/Admin/InstallController.php
@@ -35,7 +35,7 @@ class InstallController extends \CommonBundle\Component\Controller\ActionControl
                         'add', 'delete', 'edit', 'manage'
                     ),
                     'admin_van_reservation' => array(
-                        'add', 'delete', 'edit', 'manage'
+                        'add', 'delete', 'edit', 'manage', 'old'
                     ),
                     'logistics_index' => array(
                         'fetch', 'index'
