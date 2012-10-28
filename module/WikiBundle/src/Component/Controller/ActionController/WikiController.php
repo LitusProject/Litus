7f3728ad26f4e1e29e99b204b78fd5b4ea7bffd7
Pieter Maene <pieter.maene@litus.cc>
2012-10-28 22:41:42 +0100

    Jay, more fixes

--- module/WikiBundle/src/Component/Controller/ActionController/WikiController.php
+++ module/WikiBundle/src/Component/Controller/ActionController/WikiController.php
@@ -12,7 +12,7 @@
  * @license http://litus.cc/LICENSE
  */
 
-namespace CommonBundle\Component\Controller\ActionController;
+namespace WikiBundle\Component\Controller\ActionController;
 
 /**
  * We extend the CommonBundle controller.
