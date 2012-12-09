028515bfd430c17ab7d84a04875c79e2f38f5d6a
Niels Avonds <niels.avonds@gmail.com>
2012-12-09 17:52:56 +0100

    Added CV Edit mode

--- module/BrBundle/src/Entity/Cv/Entry.php
+++ module/BrBundle/src/Entity/Cv/Entry.php
@@ -301,153 +301,608 @@ class Entry
         $this->about = $about;
     }
 
+    /**
+     * Retrieves the firstName of this entry.
+     *
+     * @return firstName
+     */
     public function getFirstName()
     {
         return $this->firstName;
     }
 
+    /**
+     * Changes the firstName of this cv entry to the given value.
+     *
+     * @param firstName The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setFirstName($firstName) {
+        $this->firstName = $firstName;
+        return $this;
+    }
+
+    /**
+     * Retrieves the lastName of this entry.
+     *
+     * @return lastName
+     */
     public function getLastName()
     {
         return $this->lastName;
     }
 
+    /**
+     * Changes the lastName of this cv entry to the given value.
+     *
+     * @param lastName The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setLastName($lastName) {
+        $this->lastName = $lastName;
+        return $this;
+    }
+
+    /**
+     * Retrieves the birthday of this entry.
+     *
+     * @return birthday
+     */
     public function getBirthday()
     {
         return $this->birthday;
     }
 
+    /**
+     * Changes the birthday of this cv entry to the given value.
+     *
+     * @param birthday The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setBirthday($birthday) {
+        $this->birthday = $birthday;
+        return $this;
+    }
+
+    /**
+     * Retrieves the sex of this entry.
+     *
+     * @return sex
+     */
     public function getSex()
     {
         return $this->sex;
     }
 
+    /**
+     * Changes the sex of this cv entry to the given value.
+     *
+     * @param sex The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setSex($sex) {
+        $this->sex = $sex;
+        return $this;
+    }
+
+    /**
+     * Retrieves the phoneNumber of this entry.
+     *
+     * @return phoneNumber
+     */
     public function getPhoneNumber()
     {
         return $this->phoneNumber;
     }
 
+    /**
+     * Changes the phoneNumber of this cv entry to the given value.
+     *
+     * @param phoneNumber The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setPhoneNumber($phoneNumber) {
+        $this->phoneNumber = $phoneNumber;
+        return $this;
+    }
+
+    /**
+     * Retrieves the email of this entry.
+     *
+     * @return email
+     */
     public function getEmail()
     {
         return $this->email;
     }
 
+    /**
+     * Changes the email of this cv entry to the given value.
+     *
+     * @param email The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setEmail($email) {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * Retrieves the address of this entry.
+     *
+     * @return address
+     */
     public function getAddress()
     {
         return $this->address;
     }
 
+    /**
+     * Changes the address of this cv entry to the given value.
+     *
+     * @param address The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setAddress($address) {
+        $this->address = $address;
+        return $this;
+    }
+
+    /**
+     * Retrieves the priorStudy of this entry.
+     *
+     * @return priorStudy
+     */
     public function getPriorStudy()
     {
         return $this->priorStudy;
     }
 
+    /**
+     * Changes the priorStudy of this cv entry to the given value.
+     *
+     * @param priorStudy The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setPriorStudy($priorStudy) {
+        $this->priorStudy = $priorStudy;
+        return $this;
+    }
+
+    /**
+     * Retrieves the priorGrade of this entry.
+     *
+     * @return priorGrade
+     */
     public function getPriorGrade()
     {
         return $this->priorGrade;
     }
 
+    /**
+     * Changes the priorGrade of this cv entry to the given value.
+     *
+     * @param priorGrade The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setPriorGrade($priorGrade) {
+        $this->priorGrade = $priorGrade;
+        return $this;
+    }
+
+    /**
+     * Retrieves the study of this entry.
+     *
+     * @return study
+     */
     public function getStudy()
     {
         return $this->study;
     }
 
+    /**
+     * Changes the study of this cv entry to the given value.
+     *
+     * @param study The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setStudy($study) {
+        $this->study = $study;
+        return $this;
+    }
+
+    /**
+     * Retrieves the grade of this entry.
+     *
+     * @return grade
+     */
     public function getGrade()
     {
         return $this->grade;
     }
 
+    /**
+     * Changes the grade of this cv entry to the given value.
+     *
+     * @param grade The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setGrade($grade) {
+        $this->grade = $grade;
+        return $this;
+    }
+
+    /**
+     * Retrieves the bachelorStart of this entry.
+     *
+     * @return bachelorStart
+     */
     public function getBachelorStart()
     {
         return $this->bachelorStart;
     }
 
+    /**
+     * Changes the bachelorStart of this cv entry to the given value.
+     *
+     * @param bachelorStart The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setBachelorStart($bachelorStart) {
+        $this->bachelorStart = $bachelorStart;
+        return $this;
+    }
+
+    /**
+     * Retrieves the bachelorEnd of this entry.
+     *
+     * @return bachelorEnd
+     */
     public function getBachelorEnd()
     {
         return $this->bachelorEnd;
     }
 
+    /**
+     * Changes the bachelorEnd of this cv entry to the given value.
+     *
+     * @param bachelorEnd The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setBachelorEnd($bachelorEnd) {
+        $this->bachelorEnd = $bachelorEnd;
+        return $this;
+    }
+
+    /**
+     * Retrieves the masterStart of this entry.
+     *
+     * @return masterStart
+     */
     public function getMasterStart()
     {
         return $this->masterStart;
     }
 
+    /**
+     * Changes the masterStart of this cv entry to the given value.
+     *
+     * @param masterStart The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setMasterStart($masterStart) {
+        $this->masterStart = $masterStart;
+        return $this;
+    }
+
+    /**
+     * Retrieves the masterEnd of this entry.
+     *
+     * @return masterEnd
+     */
     public function getMasterEnd()
     {
         return $this->masterEnd;
     }
 
+    /**
+     * Changes the masterEnd of this cv entry to the given value.
+     *
+     * @param masterEnd The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setMasterEnd($masterEnd) {
+        $this->masterEnd = $masterEnd;
+        return $this;
+    }
+
+    /**
+     * Retrieves the additionalDiplomas of this entry.
+     *
+     * @return additionalDiplomas
+     */
     public function getAdditionalDiplomas()
     {
         return $this->additionalDiplomas;
     }
 
+    /**
+     * Changes the additionalDiplomas of this cv entry to the given value.
+     *
+     * @param additionalDiplomas The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setAdditionalDiplomas($additionalDiplomas) {
+        $this->additionalDiplomas = $additionalDiplomas;
+        return $this;
+    }
+
+    /**
+     * Retrieves the erasmusPeriod of this entry.
+     *
+     * @return erasmusPeriod
+     */
     public function getErasmusPeriod()
     {
         return $this->erasmusPeriod;
     }
 
+    /**
+     * Changes the erasmusPeriod of this cv entry to the given value.
+     *
+     * @param erasmusPeriod The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setErasmusPeriod($erasmusPeriod) {
+        $this->erasmusPeriod = $erasmusPeriod;
+        return $this;
+    }
+
+    /**
+     * Retrieves the erasmusLocation of this entry.
+     *
+     * @return erasmusLocation
+     */
     public function getErasmusLocation()
     {
         return $this->erasmusLocation;
     }
 
+    /**
+     * Changes the erasmusLocation of this cv entry to the given value.
+     *
+     * @param erasmusLocation The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setErasmusLocation($erasmusLocation) {
+        $this->erasmusLocation = $erasmusLocation;
+        return $this;
+    }
+
+    /**
+     * Retrieves the - of this entry.
+     *
+     * @return -
+     */
     public function getLanguages()
     {
         return $this->languages->toArray();
     }
 
+    /**
+     * Retrieves the languageExtra of this entry.
+     *
+     * @return languageExtra
+     */
     public function getLanguageExtra()
     {
         return $this->languageExtra;
     }
 
+    /**
+     * Changes the languageExtra of this cv entry to the given value.
+     *
+     * @param languageExtra The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setLanguageExtra($languageExtra) {
+        $this->languageExtra = $languageExtra;
+        return $this;
+    }
+
+    /**
+     * Retrieves the computerSkills of this entry.
+     *
+     * @return computerSkills
+     */
     public function getComputerSkills()
     {
         return $this->computerSkills;
     }
 
+    /**
+     * Changes the computerSkills of this cv entry to the given value.
+     *
+     * @param computerSkills The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setComputerSkills($computerSkills) {
+        $this->computerSkills = $computerSkills;
+        return $this;
+    }
+
+    /**
+     * Retrieves the experiences of this entry.
+     *
+     * @return experiences
+     */
     public function getExperiences()
     {
         return $this->experiences;
     }
 
+    /**
+     * Changes the experiences of this cv entry to the given value.
+     *
+     * @param experiences The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setExperiences($experiences) {
+        $this->experiences = $experiences;
+        return $this;
+    }
+
+    /**
+     * Retrieves the thesisSummary of this entry.
+     *
+     * @return thesisSummary
+     */
     public function getThesisSummary()
     {
         return $this->thesisSummary;
     }
 
+    /**
+     * Changes the thesisSummary of this cv entry to the given value.
+     *
+     * @param thesisSummary The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setThesisSummary($thesisSummary) {
+        $this->thesisSummary = $thesisSummary;
+        return $this;
+    }
+
+    /**
+     * Retrieves the futureInterest of this entry.
+     *
+     * @return futureInterest
+     */
     public function getFutureInterest()
     {
         return $this->futureInterest;
     }
 
+    /**
+     * Changes the futureInterest of this cv entry to the given value.
+     *
+     * @param futureInterest The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setFutureInterest($futureInterest) {
+        $this->futureInterest = $futureInterest;
+        return $this;
+    }
+
+    /**
+     * Retrieves the mobilityEurope of this entry.
+     *
+     * @return mobilityEurope
+     */
     public function getMobilityEurope()
     {
         return $this->mobilityEurope;
     }
 
+    /**
+     * Changes the mobilityEurope of this cv entry to the given value.
+     *
+     * @param mobilityEurope The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setMobilityEurope($mobilityEurope) {
+        $this->mobilityEurope = $mobilityEurope;
+        return $this;
+    }
+
+    /**
+     * Retrieves the mobilityWorld of this entry.
+     *
+     * @return mobilityWorld
+     */
     public function getMobilityWorld()
     {
         return $this->mobilityWorld;
     }
 
+    /**
+     * Changes the mobilityWorld of this cv entry to the given value.
+     *
+     * @param mobilityWorld The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setMobilityWorld($mobilityWorld) {
+        $this->mobilityWorld = $mobilityWorld;
+        return $this;
+    }
+
+    /**
+     * Retrieves the careerExpectations of this entry.
+     *
+     * @return careerExpectations
+     */
     public function getCareerExpectations()
     {
         return $this->careerExpectations;
     }
 
+    /**
+     * Changes the careerExpectations of this cv entry to the given value.
+     *
+     * @param careerExpectations The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setCareerExpectations($careerExpectations) {
+        $this->careerExpectations = $careerExpectations;
+        return $this;
+    }
+
+    /**
+     * Retrieves the hobbies of this entry.
+     *
+     * @return hobbies
+     */
     public function getHobbies()
     {
         return $this->hobbies;
     }
 
+    /**
+     * Changes the hobbies of this cv entry to the given value.
+     *
+     * @param hobbies The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setHobbies($hobbies) {
+        $this->hobbies = $hobbies;
+        return $this;
+    }
+
+    /**
+     * Retrieves the about of this entry.
+     *
+     * @return about
+     */
     public function getAbout()
     {
         return $this->about;
     }
 
     /**
-     * @return The id.
+     * Changes the about of this cv entry to the given value.
+     *
+     * @param about The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setAbout($about) {
+        $this->about = $about;
+        return $this;
+    }
+
+    /**
+     * Retrieves the id of this entry.
+     *
+     * @return id
      */
     public function getId()
     {
@@ -455,7 +910,20 @@ class Entry
     }
 
     /**
-     * @return \CommonBundle\Entity\Users\People\Academic
+     * Changes the id of this cv entry to the given value.
+     *
+     * @param id The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setId($id) {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * Retrieves the academic of this entry.
+     *
+     * @return academic
      */
     public function getAcademic()
     {
@@ -463,11 +931,35 @@ class Entry
     }
 
     /**
-     * @return \CommonBundle\Entity\General\AcademicYear
+     * Changes the academic of this cv entry to the given value.
+     *
+     * @param academic The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setAcademic($academic) {
+        $this->academic = $academic;
+        return $this;
+    }
+
+    /**
+     * Retrieves the year of this entry.
+     *
+     * @return year
      */
     public function getYear()
     {
         return $this->year;
+    }
+
+    /**
+     * Changes the year of this cv entry to the given value.
+     *
+     * @param year The new value
+     * @return BrBundle\Entity\Cv\Entry this
+     */
+    public function setYear($year) {
+        $this->year = $year;
+        return $this;
     }
 
 }
---
+++ module/BrBundle/src/Form/Cv/Edit.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Litus is a project by a group of students from the K.U.Leuven. The goal is to create
+ * various applications to support the IT needs of student unions.
+ *
+ * @author Niels Avonds <niels.avonds@litus.cc>
+ * @author Karsten Daemen <karsten.daemen@litus.cc>
+ * @author Bram Gotink <bram.gotink@litus.cc>
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ *
+ * @license http://litus.cc/LICENSE
+ */
+
+namespace BrBundle\Form\Cv;
+
+use CommonBundle\Component\Form\Bootstrap\Element\Submit,
+    CommonBundle\Entity\General\AcademicYear,
+    CommonBundle\Entity\General\Language,
+    CommonBundle\Entity\Users\People\Academic,
+    Doctrine\ORM\EntityManager;
+
+/**
+ * The form used to edit an existing cv
+ *
+ * @author Niels Avonds <niels.avonds@litus.cc>
+ */
+class Edit extends Add
+{
+
+    /**
+     * The entity manager.
+     */
+    private $_entityManager;
+
+    /**
+     * The academic this form is for.
+     */
+    private $_academic;
+
+    /**
+     * @param \Doctrine\ORM\EntityManager $entityManager The EntityManager instance
+     * @param null|string|int $name Optional name for the element
+     */
+    public function __construct(EntityManager $entityManager, Academic $academic, AcademicYear $year, Language $language, $name = null)
+    {
+        parent::__construct($entityManager, $academic, $year, $language, $name);
+
+        $this->remove('submit');
+
+        $field = new Submit('submit');
+        $field->setValue('Save Changes')
+            ->setAttribute('class', 'btn btn-primary');
+        $this->add($field);
+    }
+
+}
