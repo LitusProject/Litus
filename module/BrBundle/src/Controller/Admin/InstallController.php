2a382d7842b8e93969466ffaed676323349c40e5
Niels Avonds <niels.avonds@litus.cc>
2012-12-17 11:52:41 +0100

    Moved photo action to corporate controller

--- module/BrBundle/src/Controller/Admin/InstallController.php
+++ module/BrBundle/src/Controller/Admin/InstallController.php
@@ -111,7 +111,7 @@ class InstallController extends \CommonBundle\Component\Controller\ActionControl
                         'login', 'logout',
                     ),
                     'corporate_index' => array(
-                        'index', 'cv',
+                        'index', 'cv', 'cvPhoto',
                     ),
                 )
             )
