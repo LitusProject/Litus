f139aea981e610f91feaed1d5f1b8063b1e5af23
Niels Avonds <niels.avonds@gmail.com>
2012-12-17 14:37:20 +0100

    Less looping, more querying

--- module/BrBundle/src/Repository/Cv/Entry.php
+++ module/BrBundle/src/Repository/Cv/Entry.php
@@ -4,6 +4,7 @@ namespace BrBundle\Repository\Cv;
 
 use Doctrine\ORM\EntityRepository,
     CommonBundle\Entity\General\AcademicYear,
+    SyllabusBundle\Entity\Group,
     SyllabusBundle\Entity\Study;
 
 /**
@@ -28,7 +29,7 @@ class Entry extends EntityRepository
         return $resultSet;
     }
 
-    public function findAllStudies() {
+    public function findAllUngroupedStudies() {
         $query = $this->_em->createQueryBuilder();
         $subQuery = $this->_em->createQueryBuilder();
 
@@ -38,14 +39,65 @@ class Entry extends EntityRepository
                 $subQuery->expr()->eq('e.study', 's')
             );
 
+        $groupQuery = $this->_em->createQueryBuilder();
+
+        $groupQuery->select('g')
+            ->from('SyllabusBundle\Entity\StudyGroupMap', 'g')
+            ->innerJoin('g.group', 'd')
+            ->where(
+                $groupQuery->expr()->andx(
+                    $groupQuery->expr()->eq('g.study', 's'),
+                    $groupQuery->expr()->eq('d.cvBook', 'true')
+                )
+            );
+
         $resultSet = $query->select('s')
             ->from('SyllabusBundle\Entity\Study', 's')
             ->where(
-                $query->expr()->exists(
-                    $subQuery->getDql()
+                $query->expr()->andx(
+                    $query->expr()->exists(
+                        $subQuery->getDql()
+                    ),
+                    $query->expr()->not(
+                        $query->expr()->exists(
+                            $groupQuery->getDql()
+                        )
+                    )
                 )
             )
             ->orderBy('s.title', 'ASC')
+            ->getQuery()
+            ->getResult();
+
+        return $resultSet;
+    }
+
+    public function findAllByGroupAndAcademicYear(Group $group, AcademicYear $year) {
+        $query = $this->_em->createQueryBuilder();
+
+        $subQuery = $this->_em->createQueryBuilder();
+
+        $subQuery->select('g')
+            ->from('SyllabusBundle\Entity\StudyGroupMap', 'g')
+            ->where(
+                $subQuery->expr()->andx(
+                    $subQuery->expr()->eq('g.study', 's'),
+                    $subQuery->expr()->eq('g.group', ':group')
+                )
+            );
+
+        $resultSet = $query->select('e')
+            ->from('BrBundle\Entity\Cv\Entry', 'e')
+            ->innerJoin('e.study', 's')
+            ->where(
+                $query->expr()->andx(
+                    $query->expr()->exists($subQuery->getDql()),
+                    $query->expr()->eq('e.year', ':year')
+                )
+            )
+            ->setParameter('group', $group)
+            ->setParameter('year', $year)
+            ->orderBy('e.lastName', 'ASC')
             ->getQuery()
             ->getResult();
 
@@ -65,6 +117,7 @@ class Entry extends EntityRepository
             )
             ->setParameter('study', $study)
             ->setParameter('year', $year)
+            ->orderBy('e.lastName', 'ASC')
             ->getQuery()
             ->getResult();
 
