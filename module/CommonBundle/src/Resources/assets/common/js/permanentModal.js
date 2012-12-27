36a70e227ccc1ebfaf93f0c9a263b63b580f6f44
Kristof Marien <kristof.marien@litus.cc>
2012-12-27 18:37:04 +0100

    Improved permanent modal js

--- module/CommonBundle/src/Resources/assets/common/js/permanentModal.js
+++ module/CommonBundle/src/Resources/assets/common/js/permanentModal.js
@@ -6,42 +6,30 @@
     var methods = {
         open: function (options) {
             options = $.extend(defaults, options);
-            $this = $(this);
 
-            $(this).on('shown', function () {
-            	if (options.closable == true) {
-            		$(this).find('.close').show();
-            		$('.modal-backdrop').unbind('click').click(function () {
-            			$this.modal('hide');
-            		});
-            		$(document).on('keyup.dismiss.modal', function ( e ) {
-            		  e.which == 27 && $this.modal('hide');
-            		});
-            	} else {
-            		$this.find('.modal-header .close').hide();
-            		$('.modal-backdrop').unbind('click');
-                    $this.off('keyup.dismiss.modal');
-            	}
-            });
+            $(this).find('.modal-header .close').toggle(options.closable);
 
-            $(this).modal('show', {keyboard: options.closable});
+            $(this).modal({
+                keyboard: options.closable,
+                backdrop: options.closable ? true : 'static',
+            }).modal('show');
 
             return this;
         },
         hide: function () {
-			$(this).modal('hide');
+            $(this).modal('hide');
 
-			return this;
+            return this;
         }
     };
 
     $.fn.permanentModal = function (method) {
-    	if (methods[method]) {
-    		return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-    	} else if (typeof method === 'object' || ! method) {
-    		return methods.open.apply(this, arguments);
-    	} else {
-    		$.error('Method ' +  method + ' does not exist on $.formUploadProgress');
-    	}
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || ! method) {
+            return methods.open.apply(this, arguments);
+        } else {
+            $.error('Method ' +  method + ' does not exist on $.formUploadProgress');
+        }
     };
 }) (jQuery);
