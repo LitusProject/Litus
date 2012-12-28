fc1ce0155f8fc686f3f77e4b8d8582154269c3d8
Pieter Maene <pieter.maene@litus.cc>
2012-12-28 17:43:26 +0100

    Started working on our e-mail parser

---
+++ module/MailBundle/src/Resources/bin/parser.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Litus is a project by a group of students from the K.U.Leuven. The goal is to create
+ * various applications to support the IT needs of student unions.
+ *
+ * @author Niels Avonds <niels.avonds@litus.cc>
+ * @author Karsten Daemen <karsten.daemen@litus.cc>
+ * @author Bram Gotink <bram.gotink@litus.cc>
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ *
+ * @license http://litus.cc/LICENSE
+ */
+
+/**
+ * Parser for received e-mail messages.
+ *
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ */
+
+chdir(dirname(dirname(dirname(dirname(dirname(__DIR__))))));
+
+include 'init_autoloader.php';
+
+$application = Zend\Mvc\Application::init(include 'config/application.config.php');
+$em = $application->getServiceManager()->get('doctrine.entitymanager.orm_default');
