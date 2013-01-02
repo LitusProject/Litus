97c6f27f31d168167e6a7aa8e4458aeede40d2d6
Niels Avonds <niels.avonds@gmail.com>
2013-01-02 01:22:12 +0100

    First version of cv book generation

--- module/BrBundle/src/Component/Document/Generator/Pdf/CvBook.php
+++ module/BrBundle/src/Component/Document/Generator/Pdf/CvBook.php
@@ -14,7 +14,8 @@
 
 namespace BrBundle\Component\Document\Pdf;
 
-use BrBundle\Entity\Cv,
+use BrBundle\Entity\Cv\Util,
+    BrBundle\Entity\Cv\Entry,
     CommonBundle\Component\Util\File\TmpFile,
     CommonBundle\Component\Util\Xml\Generator,
     CommonBundle\Component\Util\Xml\Object,
@@ -63,27 +64,319 @@ class CvBook extends \CommonBundle\Component\Document\Generator\Pdf
         $xml = new Generator($tmpFile);
 
         // Generate the xml
-        $entries = $this->getEntityManager()
-            ->getRepository('BrBundle\Entity\Cv\Entry')
-            ->findAllByAcademicYear($this->_year);
+        $data = Util::getGrouped($this->_entityManager, $this->_year);
 
-        // TODO: this is dummy xml
+        // Process each cv
+        $cvs = array();
+        foreach ($data as $studyData) {
+            foreach ($studyData['entries'] as $entry) {
+                $cvs[] = $this->_generateCv($entry);
+            }
+        }
+
         $xml->append(
             new Object(
-                'xmlname',
-                array(
-                    'paramname' => 'paramvalue',
-                ),
-                array(
-                    new Object(
-                        'child',
-                        array(
-                            'childparam' => 'value',
+                'cvbook',
+                null,
+                $cvs
+            )
+        );
+    }
+
+    private function _generateCv(Entry $cv) {
+        return new Object(
+            'cv',
+            array(
+                'id'        => $cv->getId(),
+                'firstname' => $cv->getFirstName(),
+                'lastname'  => $cv->getLastName(),
+                'email'     => $cv->getEmail(),
+                'phone'     => $cv->getPhoneNumber(),
+                'img'       => $cv->getAcademic()->getPhotoPath(),
+            ),
+            $this->_getSections($cv)
+        );
+    }
+
+    private function _getSections(Entry $cv)
+    {
+        $result = array();
+
+        // The address
+        $result[] = new Object(
+            'address',
+            $this->_getAddressArray($cv),
+            null
+        );
+
+        // Studies
+        $result[] = new Object(
+            'section',
+            array(
+                'title' => 'Studies',
+            ),
+            array(
+                new Object(
+                    'sec-special-studies',
+                    null,
+                    array(
+                        new Object(
+                            'study',
+                            array(
+                                'start'         => (string) $cv->getMasterStart(),
+                                'end'           => (string) $cv->getMasterEnd(),
+                                'percentage'    => (string) ($cv->getGrade() / 100),
+                            ),
+                            array(
+                                new Object(
+                                    'title',
+                                    null,
+                                    $cv->getStudy()->getFullTitle()
+                                )
+                            )
                         ),
-                        null
+                        new Object(
+                            'study',
+                            array(
+                                'start'         => (string) $cv->getBachelorStart(),
+                                'end'           => (string) $cv->getBachelorEnd(),
+                                'percentage'    => (string) ($cv->getPriorGrade() / 100),
+                            ),
+                            array(
+                                new Object(
+                                    'title',
+                                    null,
+                                    $cv->getPriorStudy()
+                                )
+                            )
+                        )
                     )
                 )
             )
         );
+
+        // Erasmus
+        if ((null !== $cv->getErasmusLocation() && '' !== $cv->getErasmusLocation() )
+            || (null !== $cv->getErasmusPeriod() && '' !== $cv->getErasmusPeriod() ))
+        {
+            $result[] = new Object(
+                'section',
+                array(
+                    'title' => 'Erasmus',
+                ),
+                array(
+                    new Object(
+                        'sec-special-erasmus',
+                        null,
+                        array(
+                            new Object(
+                                'location',
+                                null,
+                                $cv->getErasmusLocation()
+                            ),
+                            new Object(
+                                'period',
+                                null,
+                                $cv->getErasmusPeriod()
+                            ),
+                        )
+                    )
+                )
+            );
+        }
+
+        // Languages
+        $result[] = new Object(
+            'section',
+            array(
+                'title' => 'Languages',
+            ),
+            array(
+                new Object(
+                    'sec-special-languages',
+                    null,
+                    $this->_getLanguageArray($cv)
+                ),
+                new Object(
+                    'subsection',
+                    array(
+                        'title' => 'Additional Info',
+                    ),
+                    array(
+                        new Object(
+                            'content',
+                            null,
+                            $cv->getLanguageExtra()
+                        )
+                    )
+                ),
+            )
+        );
+
+        // Capabilities
+        $result[] = new Object(
+            'section',
+            array(
+                'title' => 'Capabilities',
+            ),
+            array(
+                new Object(
+                    'subsection',
+                    array(
+                        'title' => 'Computer Skills',
+                    ),
+                    new Object(
+                        'content',
+                        null,
+                        $cv->getComputerSkills()
+                    )
+                ),
+                new Object(
+                    'subsection',
+                    array(
+                        'title' => 'Experiences',
+                    ),
+                    new Object(
+                        'content',
+                        null,
+                        $cv->getExperiences()
+                    )
+                )
+            )
+        );
+
+        // Thesis
+        $result[] = new Object(
+            'section',
+            array(
+                'title' => 'Thesis',
+            ),
+            array(
+                new Object(
+                    'content',
+                    null,
+                    $cv->getThesisSummary()
+                )
+            )
+        );
+
+        // Career
+        $result[] = new Object(
+            'section',
+            array(
+                'title' => 'Career',
+            ),
+            array(
+                new Object(
+                    'subsection',
+                    array(
+                        'title' => 'Future Interest',
+                    ),
+                    new Object(
+                        'content',
+                        null,
+                        $cv->getFutureInterest()
+                    )
+                ),
+                new Object(
+                    'subsection',
+                    array(
+                        'title' => 'Mobility in Europe',
+                    ),
+                    new Object(
+                        'content',
+                        null,
+                        $cv->getMobilityEurope()
+                    )
+                ),
+                new Object(
+                    'subsection',
+                    array(
+                        'title' => 'Mobility in the World',
+                    ),
+                    new Object(
+                        'content',
+                        null,
+                        $cv->getMobilityWorld()
+                    )
+                ),
+                new Object(
+                    'subsection',
+                    array(
+                        'title' => 'Career Expectations',
+                    ),
+                    new Object(
+                        'content',
+                        null,
+                        $cv->getCareerExpectations()
+                    )
+                ),
+            )
+        );
+
+        // Personal
+        $result[] = new Object(
+            'section',
+            array(
+                'title' => 'Personal',
+            ),
+            array(
+                new Object(
+                    'subsection',
+                    array(
+                        'title' => 'Hobbies',
+                    ),
+                    new Object(
+                        'content',
+                        null,
+                        $cv->getHobbies()
+                    )
+                ),
+                new Object(
+                    'subsection',
+                    array(
+                        'title' => 'About Myself',
+                    ),
+                    new Object(
+                        'content',
+                        null,
+                        $cv->getAbout()
+                    )
+                )
+            )
+        );
+
+        return $result;
+    }
+
+    private function _getAddressArray(Entry $cv)
+    {
+        $result = array(
+            'street'    => $cv->getAddress()->getStreet(),
+            'nr'        => $cv->getAddress()->getNumber(),
+            'postal'    => $cv->getAddress()->getPostal(),
+            'city'      => $cv->getAddress()->getCity(),
+            'country'   => $cv->getAddress()->getCountry(),
+        );
+        if (null !== $cv->getAddress()->getMailbox() && '' !== $cv->getAddress()->getMailbox())
+            $result['bus'] = $cv->getAddress()->getMailbox();
+        return $result;
+    }
+
+    private function _getLanguageArray(Entry $cv)
+    {
+        $languages = array();
+        foreach ($cv->getLanguages() as $language) {
+            $languages[] = new Object(
+                'language',
+                array(
+                    'name'      => $language->getName(),
+                    'oral'      => $language->getOralSkill(),
+                    'written'   => $language->getWrittenSkill(),
+                ),
+                null
+            );
+        }
+        return $languages;
     }
 }
