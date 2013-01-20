a54453ecae92ab9758557e490bb6263ade625256
Kristof Marien <kristof.marien@litus.cc>
2013-01-20 17:00:54 +0100

    Started implementing sale session restrictions

---
+++ module/CudiBundle/src/Controller/Admin/Sales/Session/RestrictionController.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * Litus is a project by a group of students from the K.U.Leuven. The goal is to create
+ * various applications to support the IT needs of student unions.
+ *
+ * @author Niels Avonds <niels.avonds@litus.cc>
+ * @author Karsten Daemen <karsten.daemen@litus.cc>
+ * @author Bram Gotink <bram.gotink@litus.cc>
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ *
+ * @license http://litus.cc/LICENSE
+ */
+
+namespace CudiBundle\Controller\Admin\Sales\Session;
+
+use CommonBundle\Component\FlashMessenger\FlashMessage,
+    CudiBundle\Entity\Sales\Session\Restriction,
+    CudiBundle\Form\Admin\Sales\Session\Restriction\Add as AddForm,
+    Zend\View\Model\ViewModel;
+
+/**
+ * RestrictionController
+ *
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ */
+class RestrictionController extends \CudiBundle\Component\Controller\ActionController
+{
+    public function manageAction()
+    {
+        if (!($session = $this->_getSession()))
+            return new ViewModel();
+
+        $restrictions = $this->getEntityManager()
+            ->getRepository('CudiBundle\Entity\Sales\Session\Restriction')
+            ->findBySession($session);
+
+        $form = new AddForm($this->getEntityManager(), $session);
+
+        if($this->getRequest()->isPost()) {
+            $formData = $this->getRequest()->getPost();
+            $form->setData($formData);
+
+            if ($form->isValid()) {
+                $formData = $form->getFormData($formData);
+
+                $restriction = new Restriction($session, $formData['type'], $formData['start_value'], $formData['end_value']);
+
+                $this->getEntityManager()->persist($restriction);
+
+                $this->getEntityManager()->flush();
+
+                $this->flashMessenger()->addMessage(
+                    new FlashMessage(
+                        FlashMessage::SUCCESS,
+                        'SUCCESS',
+                        'The restriction was successfully created!'
+                    )
+                );
+
+                $this->redirect()->toRoute(
+                    'admin_sales_session_restriction',
+                    array(
+                        'action' => 'manage',
+                        'id' => $session->getId(),
+                    )
+                );
+
+                return new ViewModel();
+            }
+        }
+
+        return new ViewModel(
+            array(
+                'restrictions' => $restrictions,
+                'session' => $session,
+                'form' => $form,
+            )
+        );
+    }
+
+    public function deleteAction()
+    {
+
+    }
+
+    private function _getSession()
+    {
+        if (null === $this->getParam('id')) {
+            $this->flashMessenger()->addMessage(
+                new FlashMessage(
+                    FlashMessage::ERROR,
+                    'Error',
+                    'No ID was given to identify the session!'
+                )
+            );
+
+            $this->redirect()->toRoute(
+                'admin_sales_session',
+                array(
+                    'action' => 'manage'
+                )
+            );
+
+            return;
+        }
+
+        $session = $this->getEntityManager()
+            ->getRepository('CudiBundle\Entity\Sales\Session')
+            ->findOneById($this->getParam('id'));
+
+        if (null === $session) {
+            $this->flashMessenger()->addMessage(
+                new FlashMessage(
+                    FlashMessage::ERROR,
+                    'Error',
+                    'No session with the given ID was found!'
+                )
+            );
+
+            $this->redirect()->toRoute(
+                'admin_sales_session',
+                array(
+                    'action' => 'manage'
+                )
+            );
+
+            return;
+        }
+
+        return $session;
+    }
+}
