502d94f25bd93777bc816a79ac493ce894c5c4ba
Niels Avonds <niels.avonds@gmail.com>
2013-02-16 14:55:11 +0100

    More unused imports in that file

--- module/PublicationBundle/src/Controller/Edition/HtmlController.php
+++ module/PublicationBundle/src/Controller/Edition/HtmlController.php
@@ -15,13 +15,7 @@
 namespace PublicationBundle\Controller\Edition;
 
 use CommonBundle\Component\FlashMessenger\FlashMessage,
-    PublicationBundle\Entity\Publication,
-    PublicationBundle\Entity\Editions\Html as HtmlEdition,
-    PublicationBundle\Form\Admin\Edition\Html\Add as AddForm,
-    Zend\File\Transfer\Adapter\Http as FileUpload,
-    Zend\Validator\File\Size as SizeValidator,
-    Zend\View\Model\ViewModel,
-    \ZipArchive;
+    Zend\View\Model\ViewModel;
 
 /**
  * HtmlController
