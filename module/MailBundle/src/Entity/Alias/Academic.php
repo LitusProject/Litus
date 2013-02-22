7ea95a0496a15574fd0194526e45779b3f6b1264
Niels Avonds <niels.avonds@gmail.com>
2013-02-22 14:54:30 +0100

    Added entities for alias

---
+++ module/MailBundle/src/Entity/Alias/Academic.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Litus is a project by a group of students from the K.U.Leuven. The goal is to create
+ * various applications to support the IT needs of student unions.
+ *
+ * @author Niels Avonds <niels.avonds@litus.cc>
+ * @author Karsten Daemen <karsten.daemen@litus.cc>
+ * @author Bram Gotink <bram.gotink@litus.cc>
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ *
+ * @license http://litus.cc/LICENSE
+ */
+
+namespace MailBundle\Entity\Alias;
+
+use CommonBundle\Entity\Users\People\Academic as AcademicPerson,
+    Doctrine\ORM\Mapping as ORM,
+    Doctrine\Common\Collections\ArrayCollection,
+    MailBundle\Entity\MailingList;
+
+/**
+ * This is the entity for an academic alias.
+ *
+ * @ORM\Entity(repositoryClass="MailBundle\Repository\Alias\Academic")
+ * @ORM\Table(name="mail.alias_academic")
+ */
+class Academic extends \MailBundle\Entity\Alias
+{
+    /**
+     * @var CommonBundle\Entity\Users\People\Academic The academic associated with this alias.
+     *
+     * @ORM\ManyToOne(targetEntity="CommonBundle\Entity\Users\People\Academic", cascade={"persist"})
+     * @ORM\JoinColumn(name="academic", referencedColumnName="id", nullable=false)
+     */
+    private $academic;
+
+    /**
+     * Creates a new alias for the given academic with the given name.
+     *
+     * @param string $name The name for this alias.
+     * @param \CommonBundle\Entity\Users\People\Academic $academic The academic to create the alias for.
+     */
+    public function __construct($name, AcademicPerson $academic)
+    {
+        parent::__construct($name);
+        $this->academic = $academic;
+    }
+
+    /**
+     * @return \CommonBundle\Entity\Users\People\Academic
+     */
+    public function getAcademic() {
+        return $this->academic;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmailAddress()
+    {
+        return $this->academic->getEmail();
+    }
+}
