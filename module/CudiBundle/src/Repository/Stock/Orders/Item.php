8e157f1d48e4aa92e0f4dc14a99aa6466f4ff864
Kristof Marien <krmarien@gmail.com>
2013-02-28 12:04:46 +0100

    Added order overview

--- module/CudiBundle/src/Repository/Stock/Orders/Item.php
+++ module/CudiBundle/src/Repository/Stock/Orders/Item.php
@@ -3,8 +3,8 @@
 namespace CudiBundle\Repository\Stock\Orders;
 
 use CudiBundle\Entity\Sales\Article,
-    Doctrine\ORM\EntityRepository,
-    Doctrine\ORM\Query\Expr\Join;
+    CudiBundle\Entity\Stock\Period,
+    Doctrine\ORM\EntityRepository;
 
 /**
  * Item
@@ -19,12 +19,11 @@ class Item extends EntityRepository
         $query = $this->_em->createQueryBuilder();
         $resultSet = $query->select('i')
             ->from('CudiBundle\Entity\Stock\Orders\Item', 'i')
-            ->innerJoin('i.order', 'o', Join::WITH,
-                $query->expr()->isNull('o.dateCreated')
-            )
+            ->innerJoin('i.order', 'o')
             ->where(
                 $query->expr()->andX(
-                    $query->expr()->eq('i.article', ':article')
+                    $query->expr()->eq('i.article', ':article'),
+                    $query->expr()->isNull('o.dateCreated')
                 )
             )
             ->setParameter('article', $article->getId())
@@ -36,5 +35,84 @@ class Item extends EntityRepository
             return $resultSet[0];
 
         return null;
+    }
+
+    public function findAllByPeriod(Period $period)
+    {
+        $query = $this->_em->createQueryBuilder();
+        $query->select('i')
+            ->from('CudiBundle\Entity\Stock\Orders\Item', 'i')
+            ->innerJoin('i.order', 'o')
+            ->where(
+                $query->expr()->andX(
+                    $query->expr()->gt('o.dateCreated', ':startDate'),
+                    $period->isOpen() ? '1=1' : $query->expr()->lt('o.dateCreated', ':endDate')
+                )
+            )
+            ->orderBy('o.dateOrdered', 'DESC')
+            ->setParameter('startDate', $period->getStartDate());
+
+        if (!$period->isOpen())
+            $query->setParameter('endDate', $period->getEndDate());
+
+        $resultSet = $query->getQuery()
+            ->getResult();
+
+        return $resultSet;
+    }
+
+    public function findAllByTitleAndPeriod($title, Period $period)
+    {
+        $query = $this->_em->createQueryBuilder();
+        $query->select('i')
+            ->from('CudiBundle\Entity\Stock\Orders\Item', 'i')
+            ->innerJoin('i.order', 'o')
+            ->innerJoin('i.article', 'a')
+            ->innerJoin('a.mainArticle', 'm')
+            ->where(
+                $query->expr()->andX(
+                    $query->expr()->like($query->expr()->lower('m.title'), ':title'),
+                    $query->expr()->gt('o.dateCreated', ':startDate'),
+                    $period->isOpen() ? '1=1' : $query->expr()->lt('o.dateCreated', ':endDate')
+                )
+            )
+            ->orderBy('o.dateOrdered', 'DESC')
+            ->setParameter('title', '%'.strtolower($title).'%')
+            ->setParameter('startDate', $period->getStartDate());
+
+        if (!$period->isOpen())
+            $query->setParameter('endDate', $period->getEndDate());
+
+        $resultSet = $query->getQuery()
+            ->getResult();
+
+        return $resultSet;
+    }
+
+    public function findAllBySupplierStringAndPeriod($supplier, Period $period)
+    {
+        $query = $this->_em->createQueryBuilder();
+        $query->select('i')
+            ->from('CudiBundle\Entity\Stock\Orders\Item', 'i')
+            ->innerJoin('i.order', 'o')
+            ->innerJoin('o.supplier', 's')
+            ->where(
+                $query->expr()->andX(
+                    $query->expr()->like($query->expr()->lower('s.name'), ':supplier'),
+                    $query->expr()->gt('o.dateCreated', ':startDate'),
+                    $period->isOpen() ? '1=1' : $query->expr()->lt('o.dateCreated', ':endDate')
+                )
+            )
+            ->orderBy('o.dateOrdered', 'DESC')
+            ->setParameter('supplier', '%'.strtolower($supplier).'%')
+            ->setParameter('startDate', $period->getStartDate());
+
+        if (!$period->isOpen())
+            $query->setParameter('endDate', $period->getEndDate());
+
+        $resultSet = $query->getQuery()
+            ->getResult();
+
+        return $resultSet;
     }
 }
