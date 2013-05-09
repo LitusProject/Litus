17ae9b90f265394c7acbf3637e5f2a39b5d4eb5e
Kristof Marien <kristof.marien@litus.cc>
2013-05-09 12:26:22 +0200

    Bugfix

--- config/assetic/newsbundle.config.php
+++ config/assetic/newsbundle.config.php
@@ -26,6 +26,8 @@ return array(
             '@bootstrap_js_tab',
             '@bootstrap_js_transition',
             '@bootstrap_js_modal',
+            '@common_jquery_form',
+            '@common_form_upload_progress',
             '@gollum_css',
             '@gollum_js',
             '@admin_css',
