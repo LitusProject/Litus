6fd3857efeee4c0a9fd8be5017ba32db90e4cba7
Kristof Marien <kristof.marien@litus.cc>
2013-06-13 13:25:40 +0200

    Users can subscribe for catalog update notifications

---
+++ module/CudiBundle/Entity/Articles/Notifications/Subscription.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Litus is a project by a group of students from the K.U.Leuven. The goal is to create
+ * various applications to support the IT needs of student unions.
+ *
+ * @author Niels Avonds <niels.avonds@litus.cc>
+ * @author Karsten Daemen <karsten.daemen@litus.cc>
+ * @author Bram Gotink <bram.gotink@litus.cc>
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ *
+ * @license http://litus.cc/LICENSE
+ */
+
+namespace CudiBundle\Entity\Articles\Notifications;
+
+use CommonBundle\Entity\Users\Person,
+    Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="CudiBundle\Repository\Articles\Notifications\Subscription")
+ * @ORM\Table(name="cudi.articles_notifications_subscriptions")
+ */
+class Subscription
+{
+    /**
+     * @var integer The ID of the subscription
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="bigint")
+     */
+    private $id;
+
+    /**
+     * @var \CommonBundle\Entity\Users\Person The person of this subscription
+     *
+     * @ORM\ManyToOne(targetEntity="CommonBundle\Entity\Users\Person")
+     * @ORM\JoinColumn(referencedColumnName="id")
+     */
+    private $person;
+
+    /**
+     * @param \CommonBundle\Entity\Users\Person $person
+     */
+    public function __construct(Person $person)
+    {
+        $this->person = $person;
+    }
+
+    /**
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return \CommonBundle\Entity\Users\Person
+     */
+    public function getPerson()
+    {
+        return $this->person;
+    }
+}
