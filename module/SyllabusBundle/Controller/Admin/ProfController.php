f08bd30f6eea34dd1341d247ee023b1164e11e21
Pieter Maene <pieter.maene@litus.cc>
2013-07-21 22:04:22 +0200

    Never enough bug fixes

--- module/SyllabusBundle/Controller/Admin/ProfController.php
+++ module/SyllabusBundle/Controller/Admin/ProfController.php
@@ -51,7 +51,7 @@ class ProfController extends \CommonBundle\Component\Controller\ActionController
                 $formData = $form->getFormData($formData);
 
                 $docent = $this->getEntityManager()
-                    ->getRepository('CommonBundle\Entity\Users\People\Academic')
+                    ->getRepository('CommonBundle\Entity\User\Person\Academic')
                     ->findOneById($formData['prof_id']);
 
                 $mapping = $this->getEntityManager()
@@ -114,10 +114,10 @@ class ProfController extends \CommonBundle\Component\Controller\ActionController
     {
         $docents = array_merge(
             $this->getEntityManager()
-                ->getRepository('CommonBundle\Entity\Users\People\Academic')
+                ->getRepository('CommonBundle\Entity\User\Person\Academic')
                 ->findAllByName($this->getParam('string')),
             $this->getEntityManager()
-                ->getRepository('CommonBundle\Entity\Users\People\Academic')
+                ->getRepository('CommonBundle\Entity\User\Person\Academic')
                 ->findAllByUniversityIdentification($this->getParam('string'))
         );
 
