5f9b8c8ac959ac54299920701c5cdb2279219991
Lars Vierbergen <vierbergenlars@gmail.com>
2013-08-29 14:00:38 +0200

    [LogisticsBundle] Add Lease & Return forms

---
+++ module/LogisticsBundle/Form/Lease/AddLease.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * Litus is a project by a group of students from the KU Leuven. The goal is to create
+ * various applications to support the IT needs of student unions.
+ *
+ * @author Niels Avonds <niels.avonds@litus.cc>
+ * @author Karsten Daemen <karsten.daemen@litus.cc>
+ * @author Bram Gotink <bram.gotink@litus.cc>
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ *
+ * @license http://litus.cc/LICENSE
+ */
+
+namespace LogisticsBundle\Form\Lease;
+
+use CommonBundle\Component\Form\Bootstrap\Element\Select,
+    CommonBundle\Component\Form\Bootstrap\Element\Text,
+    CommonBundle\Component\Validator\PositiveNumber as PositiveNumberValidator,
+    Doctrine\ORM\EntityManager,
+    Zend\InputFilter\InputFilter,
+    Zend\InputFilter\Factory as InputFactory,
+    Zend\Form\Element\Submit;
+
+/**
+ * The form used to add a new Lease.
+ *
+ * @author Lars Vierbergen <vierbergenlars@gmail.com>
+ */
+class AddLease extends \CommonBundle\Component\Form\Bootstrap\Form
+{
+    /**
+     * @var \Doctrine\ORM\EntityManager The EntityManager instance
+     */
+    protected $_entityManager = null;
+
+    /**
+     * @param \Doctrine\ORM\EntityManager $entityManager The EntityManager instance
+     * @param null|string|int $name Optional name for the element
+     */
+    public function __construct(EntityManager $entityManager, $name = null)
+    {
+        parent::__construct($name);
+
+        $this->_entityManager = $entityManager;
+
+        $field = new Text('item');
+        $field->setLabel('Search item');
+        $this->add($field);
+
+        $field = new Text('barcode');
+        $field->setLabel('Barcode')
+                ->setRequired();
+        $this->add($field);
+
+        $field = new Text('leased_to');
+        $field->setLabel('Leased to')
+                ->setRequired();
+        $this->add($field);
+
+        $field = new Text('leased_pawn');
+        $field->setLabel('Pawn')
+                ->setRequired();
+        $this->add($field);
+
+        $field = new Submit('add');
+        $field->setValue('Add')
+            ->setAttribute('class', 'btn btn-primary');
+        $this->add($field);
+
+    }
+
+    public function getInputFilter()
+    {
+        $inputFilter = new InputFilter();
+        $factory = new InputFactory();
+
+        $inputFilter->add(
+            $factory->createInput(
+                array(
+                    'name'     => 'barcode',
+                    'required' => true,
+                    'filters'  => array(
+                        array('name' => 'StringTrim'),
+                    ),
+                    'validators' => array(
+                        array(
+                            'name' => 'barcode',
+                            'options' => array(
+                                'adapter'     => 'Ean12',
+                                'useChecksum' => false,
+                            ),
+                        ),
+                    ),
+                )
+            )
+        );
+
+        $inputFilter->add(
+            $factory->createInput(
+                array(
+                    'name'     => 'leased_to',
+                    'required' => true,
+                    'filters'  => array(
+                        array('name' => 'StringTrim'),
+                    ),
+                )
+            )
+        );
+
+        $inputFilter->add(
+            $factory->createInput(
+                array(
+                    'name'     => 'leased_pawn',
+                    'required' => true,
+                    'filters'  => array(
+                        array('name' => 'StringTrim'),
+                    ),
+                    'validators' => array(
+                        new PositiveNumberValidator(false)
+                    ),
+                )
+            )
+        );
+
+        return $inputFilter;
+
+    }
+}
