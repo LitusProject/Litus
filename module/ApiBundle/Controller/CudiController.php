7e63067e614c14ec990ff5848593d20439520ed5
koencertyn <koencertyn@gmail.com>
2013-09-08 18:45:00 +0200

    added path for CalendarController and raw skeleton CudiController

---
+++ module/ApiBundle/Controller/CudiController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Litus is a project by a group of students from the KU Leuven. The goal is to create
+ * various applications to support the IT needs of student unions.
+ *
+ * @author Niels Avonds <niels.avonds@litus.cc>
+ * @author Karsten Daemen <karsten.daemen@litus.cc>
+ * @author Bram Gotink <bram.gotink@litus.cc>
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ *
+ * @license http://litus.cc/LICENSE
+ */
+
+/**
+ * CudiController
+ *
+ * @author Koen Certyn
+ */
+class CudiController extends \ApiBundle\Component\Controller\ActionController\ApiController{
+
+	
+
+}
