b8d497ec9092e5f987ed6286aa50bd1cdd5cad63
Lars Vierbergen <vierbergenlars@gmail.com>
2013-09-24 11:13:49 +0200

    [LogisticsBundle] Add lease acls

--- module/LogisticsBundle/Controller/Admin/InstallController.php
+++ module/LogisticsBundle/Controller/Admin/InstallController.php
@@ -37,8 +37,14 @@ class InstallController extends \CommonBundle\Component\Controller\ActionControl
                     'logistics_admin_van_reservation' => array(
                         'add', 'delete', 'edit', 'manage', 'old'
                     ),
+                    'logistics_admin_lease' => array(
+                        'add', 'delete', 'edit', 'manage'
+                    ),
                     'logistics_index' => array(
                         'add', 'delete', 'edit', 'fetch', 'index', 'move'
+                    ),
+                    'logistics_lease' => array(
+                        'availabilityCheck', 'history', 'index', 'show', 'typeahead'
                     ),
                     'logistics_auth' => array(
                         'login', 'logout', 'shibboleth',
