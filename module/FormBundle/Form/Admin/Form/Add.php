fb4ea9d0466a61eae71671ce39455cd62416a326
Kristof Marien <kristof.marien@litus.cc>
2013-10-10 21:18:32 +0200

    Doodle can be created

--- module/FormBundle/Form/Admin/Form/Add.php
+++ module/FormBundle/Form/Admin/Form/Add.php
@@ -16,6 +16,7 @@ namespace FormBundle\Form\Admin\Form;
 
 use CommonBundle\Component\Form\Admin\Element\Checkbox,
     CommonBundle\Component\Form\Admin\Element\Collection,
+    CommonBundle\Component\Form\Admin\Element\Select,
     CommonBundle\Component\Form\Admin\Element\Tabs,
     CommonBundle\Component\Form\Admin\Form\SubForm\TabContent,
     CommonBundle\Component\Form\Admin\Form\SubForm\TabPane,
@@ -52,7 +53,15 @@ class Add extends \CommonBundle\Component\Form\Admin\Form\Tabbable
 
         $this->_entityManager = $entityManager;
 
+        $field = new Select('type');
+        $field->setLabel('Type')
+            ->setAttribute('options', array('form' => 'Form', 'doodle' => 'Doodle'))
+            ->setAttribute('class', 'form doodle')
+            ->setRequired();
+        $this->add($field);
+
         $tabs = new Tabs('languages');
+        $tabs->setAttribute('class', 'form doodle');
         $this->add($tabs);
 
         $tabContent = new TabContent('tab_content');
@@ -64,23 +73,26 @@ class Add extends \CommonBundle\Component\Form\Admin\Form\Tabbable
 
             $field = new Text('title_' . $language->getAbbrev());
             $field->setLabel('Title')
+                ->setAttribute('class', 'form doodle')
                 ->setRequired($language->getAbbrev() == \Locale::getDefault());
             $pane->add($field);
 
             $field = new Textarea('introduction_' . $language->getAbbrev());
             $field->setLabel('Introduction')
-                ->setAttribute('class', 'md')
+                ->setAttribute('class', 'md form doodle')
                 ->setAttribute('rows', 20)
                 ->setRequired($language->getAbbrev() == \Locale::getDefault());
             $pane->add($field);
 
             $field = new Text('submittext_' . $language->getAbbrev());
             $field->setLabel('Submit Button Text')
+                ->setAttribute('class', 'form doodle')
                 ->setRequired($language->getAbbrev() == \Locale::getDefault());
             $pane->add($field);
 
             $field = new Text('updatetext_' . $language->getAbbrev());
             $field->setLabel('Update Button Text')
+                ->setAttribute('class', 'form doodle')
                 ->setRequired($language->getAbbrev() == \Locale::getDefault());
             $pane->add($field);
 
@@ -91,6 +103,7 @@ class Add extends \CommonBundle\Component\Form\Admin\Form\Tabbable
 
         $field = new Text('start_date');
         $field->setLabel('Start Date')
+            ->setAttribute('class', 'form doodle')
             ->setAttribute('placeholder', 'dd/mm/yyyy hh:mm')
             ->setAttribute('data-datepicker', true)
             ->setAttribute('data-timepicker', true)
@@ -99,6 +112,7 @@ class Add extends \CommonBundle\Component\Form\Admin\Form\Tabbable
 
         $field = new Text('end_date');
         $field->setLabel('End Date')
+            ->setAttribute('class', 'form doodle')
             ->setAttribute('placeholder', 'dd/mm/yyyy hh:mm')
             ->setAttribute('data-datepicker', true)
             ->setAttribute('data-timepicker', true)
@@ -106,51 +120,62 @@ class Add extends \CommonBundle\Component\Form\Admin\Form\Tabbable
         $this->add($field);
 
         $field = new Checkbox('active');
-        $field->setLabel('Active');
+        $field->setLabel('Active')
+            ->setAttribute('class', 'form doodle');
         $this->add($field);
 
         $field = new Text('max');
-        $field->setLabel('Total Max Entries');
+        $field->setLabel('Total Max Entries')
+            ->setAttribute('class', 'form');
         $this->add($field);
 
         $field = new Checkbox('non_members');
-        $field->setLabel('Allow Entry Without Login');
+        $field->setLabel('Allow Entry Without Login')
+            ->setAttribute('class', 'form doodle');
         $this->add($field);
 
         $field = new Checkbox('editable_by_user');
-        $field->setLabel('Allow Users To Edit Their Info');
+        $field->setLabel('Allow Users To Edit Their Info')
+            ->setAttribute('class', 'form doodle');
         $this->add($field);
 
         $field = new Checkbox('multiple');
-        $field->setLabel('Multiple Entries per Person');
+        $field->setLabel('Multiple Entries per Person')
+            ->setAttribute('class', 'form doodle');
         $this->add($field);
 
         $field = new Checkbox('mail');
-        $field->setLabel('Send Confirmation Mail');
+        $field->setLabel('Send Confirmation Mail')
+            ->setAttribute('class', 'form doodle');
         $this->add($field);
 
         $mail = new Collection('mail_form');
         $mail->setLabel('Mail')
-            ->setAttribute('id', 'mail_form');
+            ->setAttribute('id', 'mail_form')
+            ->setAttribute('class', 'form doodle');
         $this->add($mail);
 
         $field = new Text('mail_from');
         $field->setLabel('Mail Sender Address')
+            ->setAttribute('class', 'form doodle')
             ->setRequired();
         $mail->add($field);
 
         $field = new Checkbox('mail_bcc');
         $field->setLabel('Send BCC to sender for every entry')
+            ->setAttribute('class', 'form doodle')
             ->setValue(true);
         $mail->add($field);
 
         $field = new Text('mail_subject');
         $field->setLabel('Subject')
+            ->setAttribute('class', 'form doodle')
             ->setRequired();
         $mail->add($field);
 
         $field = new Textarea('mail_body');
         $field->setLabel('Body')
+            ->setAttribute('class', 'form doodle')
             ->setAttribute('rows', 20)
             ->setValue('Example mail:
 
