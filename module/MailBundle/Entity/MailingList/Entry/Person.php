6f026133784cf51a38e67aa397820a769702d741
Pieter Maene <pieter.maene@litus.cc>
2013-10-13 15:11:14 +0200

    Some small fixes

--- module/MailBundle/Entity/MailingList/Entry/Person.php
+++ module/MailBundle/Entity/MailingList/Entry/Person.php
@@ -15,7 +15,7 @@
 namespace MailBundle\Entity\MailingList\Entry;
 
 /**
- * This is the entity for the list entry of a person.
+ * This is an abstract class all person entries should inherit from.
  *
  * @author Pieter Maene <pieter.maene@litus.cc>
  */
