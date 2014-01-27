437968c9c97a9b7a5321732c0b5a3db54f1bda4a
Daan Wendelen <daan.wendelen@litus.cc>
2014-01-27 11:42:44 +0100

    plugging a security-hole

--- module/DoorBundle/Resources/bin/door.py
+++ module/DoorBundle/Resources/bin/door.py
@@ -16,7 +16,7 @@ import RPi.GPIO as GPIO
 # Parameters
 GPIO_PORT       = 15
 
-API_HOST        = 'http://litus'
+API_HOST        = 'https://litus'
 API_KEY         = ''
 
 CACHE_FILE      = '/tmp/door_rules'
