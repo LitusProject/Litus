c8895cd6b1c48874e2b1a6cb4b36b453b60e4adc
koencertyn <koencertyn@gmail.com>
2014-02-20 16:55:52 +0100

    temp commit - br request

---
+++ module/BrBundle/Form/Career/Search/Sector.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Litus is a project by a group of students from the KU Leuven. The goal is to create
+ * various applications to support the IT needs of student unions.
+ *
+ * @author Niels Avonds <niels.avonds@litus.cc>
+ * @author Karsten Daemen <karsten.daemen@litus.cc>
+ * @author Koen Certyn <koen.certyn@litus.cc>
+ * @author Bram Gotink <bram.gotink@litus.cc>
+ * @author Dario Incalza <dario.incalza@litus.cc>
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ * @author Lars Vierbergen <lars.vierbergen@litus.cc>
+ * @author Daan Wendelen <daan.wendelen@litus.cc>
+ *
+ * @license http://litus.cc/LICENSE
+ */
+
+namespace BrBundle\Form\Career\Search;
+
+use BrBundle\Entity\Company,
+    CommonBundle\Component\Form\Bootstrap\Element\Select,
+    CommonBundle\Component\Form\Bootstrap\Element\Submit,
+    CommonBundle\Entity\General\Language,
+    Doctrine\ORM\EntityManager,
+    Zend\InputFilter\InputFilter,
+    Zend\InputFilter\Factory as InputFactory;
+
+/**
+ * Search for companies in a certain section
+ *
+ * @author Koen Certyn <koen.certyn@litus.cc>
+ */
+class Sector extends \CommonBundle\Component\Form\Bootstrap\Form
+{
+    /**
+     * @var \Doctrine\ORM\EntityManager The EntityManager instance
+     */
+    private $_entityManager = null;
+
+    /**
+     * @param \Doctrine\ORM\EntityManager $entityManager The EntityManager instance
+     * @param null|string|int $name Optional name for the element
+     */
+    public function __construct(EntityManager $entityManager, $name = null)
+    {
+        parent::__construct($name, false, false);
+
+        $this->_entityManager = $entityManager;
+
+        $this->setAttribute('class', 'form-inline');
+
+        $field = new Select('sector');
+        $field->setAttribute('options', $this->_createSectionArray());
+        $this->add($field);
+    }
+
+    private function _createSectionArray()
+    {
+        $sectorArray = array();
+        foreach (Company::$possibleSectors as $key => $sector)
+            $sectorArray[$key] = $sector;
+
+        return $sectorArray;
+    }
+
+    public function getInputFilter()
+    {
+        $inputFilter = new InputFilter();
+        $factory = new InputFactory();
+
+        $inputFilter->add(
+            $factory->createInput(
+                array(
+                    'name'     => 'sector',
+                    'required' => true,
+                )
+            )
+        );
+
+        return $inputFilter;
+    }
+}
