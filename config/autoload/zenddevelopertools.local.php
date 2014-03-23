LitusProject/Litus

commit a615f5e383e4184bd7ac282cd3bbe0f6a31f8c0c

Base layout finished

config/autoload/zenddevelopertools.local.php
@@ -30,7 +30,7 @@ return array(
             ),
         ),
         'toolbar' => array(
-            'enabled'       => ('development' == getenv('APPLICATION_ENV')),
+            'enabled'       => ('developmendt' == getenv('APPLICATION_ENV')),
             'auto_hide'     => true,
             'position'      => 'bottom',
             'version_check' => false,

module/BootstrapBundle/Resources/assets/less/bootstrap.less
@@ -1,0 +1,50 @@
+// Core variables and mixins
+@import "../../../../../vendor/twitter/bootstrap/less/variables.less";
+@import "variables.less";
+@import "../../../../../vendor/twitter/bootstrap/less/mixins.less";
+
+// Reset
+@import "../../../../../vendor/twitter/bootstrap/less/normalize.less";
+@import "../../../../../vendor/twitter/bootstrap/less/print.less";
+
+// Core CSS
+@import "../../../../../vendor/twitter/bootstrap/less/scaffolding.less";
+@import "../../../../../vendor/twitter/bootstrap/less/type.less";
+@import "../../../../../vendor/twitter/bootstrap/less/code.less";
+@import "../../../../../vendor/twitter/bootstrap/less/grid.less";
+@import "../../../../../vendor/twitter/bootstrap/less/tables.less";
+@import "../../../../../vendor/twitter/bootstrap/less/forms.less";
+@import "../../../../../vendor/twitter/bootstrap/less/buttons.less";
+
+// Components
+@import "../../../../../vendor/twitter/bootstrap/less/component-animations.less";
+@import "../../../../../vendor/twitter/bootstrap/less/glyphicons.less";
+@import "../../../../../vendor/twitter/bootstrap/less/dropdowns.less";
+@import "../../../../../vendor/twitter/bootstrap/less/button-groups.less";
+@import "../../../../../vendor/twitter/bootstrap/less/input-groups.less";
+@import "../../../../../vendor/twitter/bootstrap/less/navs.less";
+@import "../../../../../vendor/twitter/bootstrap/less/navbar.less";
+@import "../../../../../vendor/twitter/bootstrap/less/breadcrumbs.less";
+@import "../../../../../vendor/twitter/bootstrap/less/pagination.less";
+@import "../../../../../vendor/twitter/bootstrap/less/pager.less";
+@import "../../../../../vendor/twitter/bootstrap/less/labels.less";
+@import "../../../../../vendor/twitter/bootstrap/less/badges.less";
+@import "../../../../../vendor/twitter/bootstrap/less/jumbotron.less";
+@import "../../../../../vendor/twitter/bootstrap/less/thumbnails.less";
+@import "../../../../../vendor/twitter/bootstrap/less/alerts.less";
+@import "../../../../../vendor/twitter/bootstrap/less/progress-bars.less";
+@import "../../../../../vendor/twitter/bootstrap/less/media.less";
+@import "../../../../../vendor/twitter/bootstrap/less/list-group.less";
+@import "../../../../../vendor/twitter/bootstrap/less/panels.less";
+@import "../../../../../vendor/twitter/bootstrap/less/wells.less";
+@import "../../../../../vendor/twitter/bootstrap/less/close.less";
+
+// Components w/ JavaScript
+@import "../../../../../vendor/twitter/bootstrap/less/modals.less";
+@import "../../../../../vendor/twitter/bootstrap/less/tooltip.less";
+@import "../../../../../vendor/twitter/bootstrap/less/popovers.less";
+@import "../../../../../vendor/twitter/bootstrap/less/carousel.less";
+
+// Utility classes
+@import "../../../../../vendor/twitter/bootstrap/less/utilities.less";
+@import "../../../../../vendor/twitter/bootstrap/less/responsive-utilities.less";

module/BootstrapBundle/Resources/assets/less/variables.less
@@ -1,0 +1,7 @@
+// Navbar collapse
+//** Point at which the navbar becomes uncollapsed.
+@grid-float-breakpoint:     @screen-md-min;
+//** Point at which the navbar begins collapsing.
+@grid-float-breakpoint-max: (@grid-float-breakpoint - 1);
+
+@icon-font-path:          "/_site/fonts/";

module/BootstrapBundle/Resources/config/module.config.php
@@ -20,7 +20,7 @@ return array(
     'assetic_configuration' => array(
         'modules' => array(
             'bootstrapbundle' => array(
-                'root_path' => __DIR__ . '/../../../../vendor/twitter/bootstrap/',
+                'root_path' => __DIR__ . '/../assets',
                 'collections' => array(
                     'bootstrap_css' => array(
                         'assets' => array(
@@ -44,62 +44,62 @@ return array(
                     ),
                     'bootstrap_js_affix' => array(
                         'assets' => array(
-                            'js/affix.js',
+                            __DIR__ . '/../../../../vendor/twitter/bootstrap/js/affix.js',
                         ),
                     ),
                     'bootstrap_js_alert' => array(
                         'assets' => array(
-                            'js/alert.js',
+                            __DIR__ . '/../../../../vendor/twitter/bootstrap/js/alert.js',
                         ),
                     ),
                     'bootstrap_js_button' => array(
                         'assets' => array(
-                            'js/button.js',
+                            __DIR__ . '/../../../../vendor/twitter/bootstrap/js/button.js',
                         ),
                     ),
                     'bootstrap_js_carousel' => array(
                         'assets' => array(
-                            'js/carousel.js',
+                            __DIR__ . '/../../../../vendor/twitter/bootstrap/js/carousel.js',
                         ),
                     ),
                     'bootstrap_js_collapse' => array(
                         'assets' => array(
-                            'js/collapse.js',
+                            __DIR__ . '/../../../../vendor/twitter/bootstrap/js/collapse.js',
                         ),
                     ),
                     'bootstrap_js_dropdown' => array(
                         'assets' => array(
-                            'js/dropdown.js',
+                            __DIR__ . '/../../../../vendor/twitter/bootstrap/js/dropdown.js',
                         ),
                     ),
                     'bootstrap_js_modal' => array(
                         'assets' => array(
-                            'js/modal.js',
+                            __DIR__ . '/../../../../vendor/twitter/bootstrap/js/modal.js',
                         ),
                     ),
                     'bootstrap_js_popover' => array(
                         'assets' => array(
-                            'js/popover.js',
+                            __DIR__ . '/../../../../vendor/twitter/bootstrap/js/popover.js',
                         ),
                     ),
                     'bootstrap_js_scrollspy' => array(
                         'assets' => array(
-                            'js/scrollspy.js',
+                            __DIR__ . '/../../../../vendor/twitter/bootstrap/js/scrollspy.js',
                         ),
                     ),
                     'bootstrap_js_tab' => array(
                         'assets' => array(
-                            'js/tab.js',
+                            __DIR__ . '/../../../../vendor/twitter/bootstrap/js/tab.js',
                         ),
                     ),
                     'bootstrap_js_tooltip' => array(
                         'assets' => array(
-                            'js/tooltip.js',
+                            __DIR__ . '/../../../../vendor/twitter/bootstrap/js/tooltip.js',
                         ),
                     ),
                     'bootstrap_js_transition' => array(
                         'assets' => array(
-                            'js/transition.js',
+                            __DIR__ . '/../../../../vendor/twitter/bootstrap/js/transition.js',
                         ),
                     ),
                 ),

module/CommonBundle/Component/Controller/ActionController/SiteController.php
@@ -97,12 +97,15 @@ class SiteController extends \CommonBundle\Component\Controller\ActionController
 
         $menu = array();
 
+        $activeItem = -1;
+
         $i = 0;
         foreach ($categories as $category) {
             $menu[$i] = array(
                 'type'  => 'category',
                 'name'  => $category->getName($this->getLanguage()),
-                'items' => array()
+                'items' => array(),
+                'active' => false,
             );
 
             $pages = $this->getEntityManager()
@@ -130,14 +133,21 @@ class SiteController extends \CommonBundle\Component\Controller\ActionController
                     'name'  => $page->getName(),
                     'title' => $page->getTitle($this->getLanguage())
                 );
+
+                if ($activeItem < 0 && $this->getParam('controller') == 'page' && $this->getParam('name') == $page->getName())
+                    $activeItem = $i;
             }
 
             foreach ($links as $link) {
                 $menu[$i]['items'][] = array(
                     'type' => 'link',
                     'id'   => $link->getId(),
-                    'name' => $link->getName($this->getLanguage())
+                    'name' => $link->getName($this->getLanguage()),
+                    'url' => $link->getUrl($this->getLanguage()),
                 );
+
+                if ($activeItem < 0 && strpos($this->getRequest()->getRequestUri(), $link->getUrl($this->getLanguage())) === 0)
+                    $activeItem = $i;
             }
 
             $sort = array();
@@ -148,6 +158,9 @@ class SiteController extends \CommonBundle\Component\Controller\ActionController
 
             $i++;
         }
+
+        if ($activeItem >= 0)
+            $menu[$activeItem]['active'] = true;
 
         $sort = array();
         foreach ($menu as $key => $value)

module/CommonBundle/Resources/assets/site/less/base.less
@@ -7,9 +7,10 @@
  */
 
 // Variables and Global Definitions
+@import "../../../../../../vendor/twitter/bootstrap/less/variables.less";
 @import "variables.less";
 
 // Layout
 @import "layout.less";
-@import "bootstrap-fileupload.less";
-@import "bootstrap-pagealert.less";
+//@import "bootstrap-fileupload.less";
+//@import "bootstrap-pagealert.less";

module/CommonBundle/Resources/assets/site/less/layout.less
@@ -8,154 +8,49 @@
 */
 
 body {
+    padding-top: 50px;
     background: #252525;
     word-wrap: break-word;
 }
 
-header {
-    .verticalGradient(@cHeaderDark, @cHeaderLight);
-    margin-bottom: 15px;
-
-    h1 {
-        color: @white;
-        text-shadow: 0 1px 1px rgba(0,0,0,.3);
-        padding: 10px 0 20px 20px;
-    }
-
-    .inner {
-        background: transparent url(/_site/img/grid-18px.png) top center;
-        padding: 60px 0 10px 0;
-        .box-shadow(inset 0 10px 30px rgba(0,0,0,.3));
-    }
-
-    .carousel {
-        img {
-            width: 940px;
-            height: 100px;
+.navbar-main {
+    #accountDropdown {
+        .profile-wrapper {
+            height: 18px;
+            display: block;
+            position: relative;
+            float: left;
         }
-    }
-}
 
-.main-navbar {
-    .title {
-        float: left;
-        padding: 8px 20px 12px;
-        font-size: 20px;
-        font-weight: 200;
-        line-height: 1;
-        color: @white;
-
-        &:hover {
-            text-decoration: none;
+        .profile {
+            margin: 0 4px 0 0;
+            background: transparent url(/_site/img/profile.png) 0 0;
+            display: inline-block;
+            width: 20px;
+            height: 20px;
         }
-    }
 
-    .btn-navbar {
-        .buttonBackground(@navbarBackgroundHighlight, @navbarBackground);
-    }
-
-    .pull-right {
-        margin-right: 0;
-    }
-
-    .auth-nav {
-        margin-right: 10px;
-
-        #accountDropdown {
-            .profile-wrapper {
-                height: 18px;
-                display: block;
-                position: relative;
-                float: left;
-            }
-
+        &:hover,
+        &.open {
             .profile {
-                margin: 0 4px 0 0;
-                background: transparent url(/_site/img/profile.png) 0 0;
-                display: inline-block;
-                width: 20px;
-                height: 20px;
+                background-position: 0 -30px;
             }
+        }
 
-            &:hover,
-            &.open {
-                .profile {
-                    background-position: 0 -30px;
-                }
-            }
+        .fullName {
+            display: block;
+        }
 
-            .fullName {
-                display: block;
-            }
-
-            .metadata {
-                display: block;
-                font-size: 12px;
-                color: @cGray;
-            }
+        .metadata {
+            display: block;
+            font-size: 12px;
+            color: @grayLight;
         }
     }
 }
 
-.main-navbar-fixed-top {
-    margin: 0px !important;
-}
-
-.main-navbar-inner {
-    min-height: @navbarHeight;
-    padding-left:  20px;
-    padding-right: 20px;
-    background-color: @navbarBackgroundHighlight;
-    background-image: none;
-    .border-radius(0px);
-    .box-shadow(~"0 1px 3px rgba(0,0,0,.25)");
-}
-
-.main-navbar .nav > li > a {
-    float: none;
-    @elementHeight: 20px;
-    padding: ((@navbarHeight - @elementHeight) / 2) + 2 10px ((@navbarHeight - @elementHeight) / 2) - 1;
-    line-height: 19px;
-    color: @navbarLinkColor;
-    text-decoration: none;
-    text-shadow: none !important;
-
-    &.highlighted {
-        @elementHeight: 28px;
-        @elementMargin: 4px;
-        padding: ((@navbarHeight - @elementHeight - @elementMargin) / 2) + 2 10px ((@navbarHeight - @elementHeight) / 2) - 1;
-        margin: @elementMargin 10px 0 0;
-        line-height: 19px;
-        color: @navbarLinkColorActive;
-        background-color: @navbarLinkBackgroundActive;
-        .border-radius(4px);
-        border: 1px solid @grayLight;
-    }
-}
-
-.main-navbar .nav > li.open > a {
-    color: @navbarLinkColorActive;
-}
-
-.main-navbar .nav > li > a:hover {
-    background-color: @navbarLinkBackgroundHover;
-    color: @navbarLinkColorHover;
-    text-decoration: none;
-}
-
-.main-navbar .nav li.dropdown .dropdown-toggle .caret,
-.main-navbar .nav li.dropdown.open .caret {
-    border-top-color: @navbarLinkColorActive;
-    border-bottom-color: @navbarLinkColorActive;
-}
-
-.carousel-control {
-    top: 50%;
-}
-
-#page {
-    background: #fff;
-    padding-bottom: 30px;
+header {
+    background: @cHeaderDark;
 
     .site-link {
         text-decoration: none;
@@ -171,33 +66,102 @@ header {
         }
     }
 
+    h1 {
+        color: @white;
+        text-shadow: 0 1px 1px rgba(0,0,0,.3);
+        padding: 10px 0 20px 20px;
+    }
+
+    .container {
+        padding: 10px;
+    }
+}
+
+#carouselHeader {
+    margin: 0 auto 10px;
+    width: 940px;
+    height: 100px;
+
+    .carousel-control {
+        span {
+            margin-top: -10px;
+        }
+    }
+}
+
+#page {
+    background: @white;
+    padding: 15px 0 30px 0;
+
+    .heading-icon {
+        float: left;
+        margin: 20px 10px 0 0;
+        opacity: .8;
+    }
+
+    .heading-spacer {
+        margin: 10px;
+    }
+}
+
+footer {
+    color: @grayMiddleLight;
+    border-top: 1px solid @grayLighter;
+
+    .light {
+        padding: 16px 0 16px 0;
+        background: @grayDark;
+    }
+
+    .dark {
+        padding: 20px 0;
+
+        h3 {
+            line-height: 30px;
+        }
+
+        img {
+            height: 30px;
+        }
+    }
+
+    h3 {
+        margin: 0;
+        font-weight: normal;
+        font-size: 18px;
+        color: @gray
+    }
+
+    .nav-list {
+        margin: 0 0 10px 0;
+        padding-left: 20px;
+        list-style: none;
+
+        li {
+            margin: 0;
+            a {
+                padding-top: 0;
+                font-size: 14px;
+                color: @grayMiddleLight;
+            }
+
+            a:hover {
+                background: none;
+                text-decoration: none;
+                color: @grayLighter;
+            }
+        }
+    }
+}
+
+/*
+
+#page {
+    background: #fff;
+    padding-bottom: 30px;
+
     #content {
         padding: 0 20px;
-
-        .container {
-            padding: 20px 0px;
-            margin: 0 auto;
-        }
-
-        .notification {
-            p:last-child {
-                margin-bottom: 0;
-            }
-        }
-
-        .heading-icon {
-            float: left;
-            margin: 17px 10px 0 0;
-            opacity: .8;
-
-            &.no-margin {
-                margin: 8px 10px 0 0;
-            }
-        }
-
-        .heading-spacer {
-            margin: 10px;
-        }
 
         .item {
             margin-top: 10px;
@@ -322,72 +286,6 @@ header {
     h6 {
         font-size: 11px;
         line-height: 21px;
-    }
-}
-
-footer {
-    margin-bottom: 20px;
-    color: #fff;
-
-    .container {
-        padding: 0 20px;
-        color: @headingsColor;
-    }
-
-    h3 {
-        margin: 0;
-
-        font-family: @headingsFontFamily;
-        font-weight: @headingsFontWeight;
-        font-size: 18px;
-        text-shadow: 0 1px 0 rgba(255, 255, 255, .1);
-
-        color: @black;
-
-        line-height: 40px;
-    }
-
-    .light {
-        margin-bottom: 20px;
-        padding: 16px 0 16px 0;
-        background: #333;
-
-        .nav-list {
-            margin: 0 0 10px 0;
-
-            li {
-                a {
-                    padding-top: 0;
-
-                    background: none;
-
-                    font-family: @headingsFontFamily;
-                    font-weight: @headingsFontWeight;
-                    font-size: 14px;
-                    text-shadow: 0 1px 0 rgba(255, 255, 255, .2);
-
-                    color: @grayLight;
-                }
-
-                a:hover {
-                    background: none;
-                    text-decoration: none;
-                    color: @white;
-                }
-            }
-        }
-
-        .companies {
-            margin-left: 15px;
-        }
-    }
-
-    .copyright {
-        .icons {
-            margin: 0;
-            line-height: 40px;
-            text-align: right;
-        }
     }
 }
 
@@ -519,4 +417,4 @@ table {
     .dropdown ul.dropdown-menu {
         display: block;
     }
-}
+}*/

module/CommonBundle/Resources/assets/site/less/variables.less
@@ -18,6 +18,22 @@
 @grayMiddleLighter:             #ddd;
 @grayLighter:                   #eee;
 
+@cHeaderDark:                   #22325a;
+@cHeaderLight:                  #121b35;
+
+/*
+// Colors
+@black:                         #000;
+@white:                         #fff;
+
+@grayDarker:                    #222;
+@grayDark:                      #333;
+@gray:                          #555;
+@grayMiddleLight:               #888;
+@grayLight:                     #bbb;
+@grayMiddleLighter:             #ddd;
+@grayLighter:                   #eee;
+
 @cGray:                         #ccc;
 @cHeaderDark:                   #22325a;
 @cHeaderLight:                  #121b35;
@@ -275,4 +291,4 @@
     }
 }
 
-
+*/

commit 38a05b9bd56cd0099d76ecc28d1d9d3c2950b5d4

fixed typo introduced in a615f5e3

config/autoload/zenddevelopertools.local.php
@@ -30,7 +30,7 @@ return array(
             ),
         ),
         'toolbar' => array(
-            'enabled'       => ('developmendt' == getenv('APPLICATION_ENV')),
+            'enabled'       => ('development' == getenv('APPLICATION_ENV')),
             'auto_hide'     => true,
             'position'      => 'bottom',
             'version_check' => false,