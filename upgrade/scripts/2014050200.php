e0c149b0fc879755b4607d6d8fd07900b2f71d25
Kristof Mariën <kristof.marien@litus.cc>
2014-05-02 18:59:29 +0200

    Added flag for guest login mail

---
+++ upgrade/scripts/2014050200.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Litus is a project by a group of students from the KU Leuven. The goal is to create
+ * various applications to support the IT needs of student unions.
+ *
+ * @author Niels Avonds <niels.avonds@litus.cc>
+ * @author Karsten Daemen <karsten.daemen@litus.cc>
+ * @author Koen Certyn <koen.certyn@litus.cc>
+ * @author Bram Gotink <bram.gotink@litus.cc>
+ * @author Dario Incalza <dario.incalza@litus.cc>
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ * @author Lars Vierbergen <lars.vierbergen@litus.cc>
+ * @author Daan Wendelen <daan.wendelen@litus.cc>
+ *
+ * @license http://litus.cc/LICENSE
+ */
+
+pg_query($connection, 'ALTER TABLE nodes.forms ADD send_guest_login_mail BOOLEAN');
+pg_query($connection, 'UPDATE nodes.forms SET send_guest_login_mail = FALSE');
