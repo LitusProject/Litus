294f07f29d50d61e11f7e3cb3d457730afce9eb6
Kristof Mariën <kristof.marien@litus.cc>
2014-09-04 18:04:50 +0200

    Added privacy policy

--- module/CommonBundle/Resources/config/install/configuration.config.php
+++ module/CommonBundle/Resources/config/install/configuration.config.php
@@ -307,8 +307,140 @@ Disallow: /admin/*
         'key'         => 'common.privacy_policy',
         'value'       => serialize(
             array(
-                'nl' => 'tekst',
-                'en' => 'text',
+                'nl' => '## Verantwoordelijke voor de verwerking
+
+VTK vzw hecht zeer veel belang aan de bescherming van de persoonsgegevens van de bezoekers van de website en in het bijzonder van haar studenten. We doen dan ook onze uiterste best om deze gegevens te beschermen en in overeenstemming te zijn met de relevante wetgeving.
+
+Voor vragen omtrent de verwerking van uw persoonsgegevens kan u zich altijd wenden tot VTK vzw:
+
+    VTK vzw t.a.v. VTK IT
+    Studentenwijk Arenberg 6/0
+    3001 Heverlee
+    it@vtk.be
+
+Om uw gegevens te bekijken en desgevallend te corrigeren kan u altijd aanmelden op de VTK-website. Mocht u geen login hebben of problemen ondervinden dan kan u altijd contact opnemen met ons op de hierboven vermelde coördinaten.
+
+Indien u dit wenst, kan u steeds uw persoonlijke gegevens laten verwijderen uit onze database. Wanneer u dit doet, worden alle velden in onze database die u kunnen identificeren, leeg gemaakt. Indien u dit niet doet, houdt VTK deze bij totdat de vereniging ontbonden wordt.
+
+## Verwerkte gegevens
+
+### CV-boek
+
+Teneinde aan laatstejaarsstudenten de mogelijkheid te bieden zich kenbaar te maken aan de bedrijfswereld, houdt VTK een online CV-boek bij. Uw gegevens worden enkel met uw uitdrukkelijke toelating opgenomen in dit bestand.
+
+U kan deze gegevens online inkijken en aanpassen. Schrapping uit het online CV-boek is altijd mogelijk. Hiervoor kan u contact opnemen met [br@vtk.be](br@vtk.be).
+
+### Leden-, studenten- en alumnibestand
+
+De volgende gegevens worden verzameld:
+
+* voornaam;
+* naam;
+* geboortedatum;
+* geslacht;
+* telefoonnummer;
+* gebruikersnummer van de KU Leuven;
+* studenten e-mailadres;
+* persoonlijk e-mailadres;
+* thuisadres;
+* kotadres;
+* studierichting.
+
+Deze gegevens worden voor volgende doeleinden gebruikt:
+
+* ledenadministratie;
+* klantenbestand cursusdienst;
+* toegangscontrole website;
+* online diensten (bv. wiki en mailing lists);
+* zendingen van bedrijven (zowel per post als via e-mail).
+
+Bij zendingen door bedrijven worden uw gegevens nooit doorgegeven. VTK vzw doet zelf alle zendingen in opdracht van het bedrijf. U kan altijd uw toestemming intrekken om zendingen van bedrijven te ontvangen ([br@vtk.be](br@vtk.be)).
+
+Deze gegevens worden verkregen via volgende wegen:
+
+* registratie op de website;
+* aanvraag lidmaatschap bij de secretaris ([secretaris@vtk.be](secretaris@vtk.be)).
+
+Deze gegevens worden aan volgende derden doorgegeven:
+
+* VTK Ondersteuning vzw (Studentenwijk Arenberg 6/1, 3001 Heverlee).
+
+U kan deze gegevens op iedere moment kosteloos wijzigen of verwijderen.
+
+## Cookies
+
+VTK gebruikt cookies om het gebruik van de website te analyseren.
+
+Via uw browserinstellingen kunt u de installatie van cookies weigeren. Op de volgende website vindt u voor de meest gebruikte browsers de werkwijze om cookies te weigeren: [www.aboutcookies.org](www.aboutcookies.org).
+
+U kan ook op elk moment de reeds geïnstalleerde cookies verwijderen van uw computer of mobiel apparaat.',
+                'en' => '## Responsible for processing
+
+VTK vzw values the protection of personal information of the visitors of our website and her students in particular. We therefore do our utmost to protect this information and to be in accordance with the relevant legislation.
+
+For question about the processing of your personal information, you may address VTK vzw:
+
+    VTK vzw attn VTK IT
+    Studentenwijk Arenberg 6/0
+    3001 Heverlee
+    it@vtk.be
+
+To view your information, and if necessary to correct it, you can always sign in on the VTK-website. If you don\'t have a login or experience problems, you may address us at the coordinates listed above.
+
+If you wish, you can always have your personal information removed from our database. When you do this, all fields in our database that can identify you, will be emptied. If you do not, VTK keeps these up until the organization is dissolved.
+
+## Processed data
+
+### CV-book
+
+In order to give last year students the opportunity to make themselves known to the business world, VTK keeps an online CV book. Your information will only be included with after your explicit consent.
+
+You can view and edit this information online. Removal from the online CV book is possible anytime. You can contact [br@vtk.be](br@vtk.be) to have this done.
+
+### Member-, student- en alumni file
+
+The following data are collected:
+
+* first name;
+* name;
+* birthday;
+* sex;
+* phone number;
+* username at KU Leuven;
+* student mail address;
+* personal mail address;
+* home address;
+* student housing address;
+* study.
+
+These data are used for the following purposes:
+
+* Member administration;
+* customer base book store;
+* access control website;
+* online services (bv. wiki and mailing lists);
+* consignments of companies (both by mail and e-mail).
+
+Your personal information will never be forwarded in mailings to companies. VTK vzw makes all the mailings commissioned by any company. You can always withdraw your consent to receive company mailings ([br@vtk.be](br@vtk.be)).
+
+This information is obtained from the following sources:
+
+* registration on the website;
+* request for membership at the secretary ([secretaris@vtk.be](secretaris@vtk.be)).
+
+These information will be passed to the following third parties:
+
+* VTK Ondersteuning vzw (Studentenwijk Arenberg 6/1, 3001 Heverlee).
+
+You can modify or remove this information free of charge at any moment.
+
+## Cookies
+
+VTK uses cookies to analyse the usage of the website.
+
+You can refuse the installation of cookies by changing your browser settings. You can find the procedure to refuse cookies for the most common browsers on the following website: [www.aboutcookies.org](www.aboutcookies.org).
+
+You can remove any installed cookies from your computer or mobile device at any moment.',
             )
         ),
         'description' => 'The privacy policy',
