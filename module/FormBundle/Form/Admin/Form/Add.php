31c6107190a3cd2dafca16684659de48d7046d98
MathijsC <mathijs.cuppens@hotmail.com>
2015-08-16 11:17:29 +0200

    make from field required again

--- module/FormBundle/Form/Admin/Form/Add.php
+++ module/FormBundle/Form/Admin/Form/Add.php
@@ -162,6 +162,7 @@ class Add extends \CommonBundle\Component\Form\Admin\Form\Tabbable
             'type'       => 'form_form_mail_add',
             'name'       => 'mail_form',
             'label'      => 'Mail',
+            'required'   => true,
             'attributes' => array(
                 'id' => 'mail_form',
             ),
@@ -182,6 +183,7 @@ class Add extends \CommonBundle\Component\Form\Admin\Form\Tabbable
             'type'       => 'form_form_mail_add',
             'name'       => 'reminder_mail_form',
             'label'      => 'Reminder Mail',
+            'required'   => true,
             'attributes' => array(
                 'id'    => 'reminder_mail_form',
             ),
