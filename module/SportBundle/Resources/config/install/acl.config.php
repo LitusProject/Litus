316ac7600aaad1f351f7890252a880359b622550
Floris Kint <floris.kint@gmail.com>
2015-10-14 19:21:32 +0200

    Add 'next' ACL action for sport

--- module/SportBundle/Resources/config/install/acl.config.php
+++ module/SportBundle/Resources/config/install/acl.config.php
@@ -19,7 +19,7 @@
 return array(
     'sportbundle' => array(
         'sport_admin_run' => array(
-            'edit','editGroup','departments', 'groups', 'identification', 'killSocket', 'laps', 'queue', 'reward', 'runner', 'update',
+            'edit', 'editGroup', 'departments', 'groups', 'identification', 'killSocket', 'laps', 'queue', 'reward', 'runner', 'update', 'next',
         ),
         'sport_run_group' => array(
             'add', 'getName',
