04e314ecfb582e0b93d08dafc870c9aff2094578
MathijsC <mathijs.cuppens@hotmail.com>
2015-11-14 15:23:28 +0100

    remove debug code

--- module/BrBundle/Controller/Corporate/CvController.php
+++ module/BrBundle/Controller/Corporate/CvController.php
@@ -132,7 +132,7 @@ class CvController extends \BrBundle\Component\Controller\CorporateController
         return new ViewModel(
             array(
                 'academicYear' => $academicYear,
-                'entries' => array($entries[0]),
+                'entries' => $entries,
                 'profilePath' => $this->getEntityManager()
                     ->getRepository('CommonBundle\Entity\General\Config')
                     ->getConfigValue('common.profile_path'),
