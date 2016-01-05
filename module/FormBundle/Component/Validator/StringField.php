3c72adf90e5e5a27d9d48504af0c247c9d6386c1
MathijsC <mathijs.cuppens@hotmail.com>
2016-01-05 22:53:40 +0100

    fix formbug

--- module/FormBundle/Component/Validator/StringField.php
+++ module/FormBundle/Component/Validator/StringField.php
@@ -70,8 +70,8 @@ class StringField extends \CommonBundle\Component\Validator\AbstractValidator
         $this->setValue($value);
 
         if ($this->options['multiline']) {
-            if (($this->_isSpecified($this->options['lines']) && !$this->_isSpecified($value)) ||
-                (!$this->_isSpecified($this->options['lines']) && $this->_isSpecified($value))) {
+            if (($this->isSpecified($this->options['lines']) && !$this->isSpecified($value)) ||
+                (!$this->isSpecified($this->options['lines']) && $this->isSpecified($value))) {
                 $this->error(self::ML_BOTH);
 
                 return false;
