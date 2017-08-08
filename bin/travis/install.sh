7755a26f6abff440acab1975843b4deb36700fbe
MathijsC <mathijs.cuppens@hotmail.com>
2017-08-08 10:44:15 +0200

    fix travis script

--- bin/travis/install.sh
+++ bin/travis/install.sh
@@ -16,7 +16,9 @@ sudo ln -s $(which node) /usr/local/bin/node
 
 # install npm
 # I know, right?
-curl -L http://npmjs.org/install.sh | sudo sh
+# curl -L http://npmjs.org/install.sh | sudo sh
+echo "Should install updated npm version, but is not longer compatible with old node.js version" >&2
+echo "The system should realy be updated in the near future!" >&2
 
 # install lessc
 sudo npm install -g less
