f2892cbc3981d3b9d05e3b1d07263cccee39f86c
Hannes Vandecasteele <hannesvdc@gmail.com>
2017-12-07 14:03:55 +0100

    Print result in api

--- module/ApiBundle/Controller/AuthController.php
+++ module/ApiBundle/Controller/AuthController.php
@@ -14,6 +14,7 @@
  * @author Daan Wendelen <daan.wendelen@litus.cc>
  * @author Mathijs Cuppens <mathijs.cuppens@litus.cc>
  * @author Floris Kint <floris.kint@vtk.be>
+ * @author Hannes Vandecasteele <hannes.vandecasteele@vtk.be>
  *
  * @license http://litus.cc/LICENSE
  */
@@ -72,6 +73,7 @@ class AuthController extends \ApiBundle\Component\Controller\ActionController\Ap
                 : "-1";
         }
 
+        print_r($result);
         return new ViewModel(
             array(
                 'result' => (object) $result,
