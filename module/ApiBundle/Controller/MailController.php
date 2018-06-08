627998e47c087967ebfbdeafc6a361e5f0f8d1f1
Matthias <mswiggers@maria.localdomain>
2018-06-08 17:33:40 +0000

    Added controller function for Postfix mailing list

--- module/ApiBundle/Controller/MailController.php
+++ module/ApiBundle/Controller/MailController.php
@@ -58,6 +58,43 @@ class MailController extends \ApiBundle\Component\Controller\ActionController\Ap
         return $this->aliasesAction();
     }
 
+    public function listsAction()
+    {
+        $lists = $this->getEntityManager()
+            ->getRepository('MailBundle\Entity\MailingList')
+            ->findAll();
+
+        $data = [];
+        
+        foreach($lists as $list){
+            $entries = $this->getEntityManager()
+                ->getRepository('MailBundle\Entity\MailingList\Entry')
+                ->findByList($list);
+            
+            $addresses = array_map(function($entry){ return $entry->getEmailAddress(); }, $entries);
+            $addressesString = implode(', ', $addresses);
+            
+            $data[] = array('name' => $list->getName(), 'addresses' => $addressesString);
+        }
+
+        $headers = new Headers();
+        $headers->addHeaders(array(
+            'Content-Type' => 'text/plain',
+        ));
+        $this->getResponse()->setHeaders($headers);
+
+        return new ViewModel(
+            array(
+                'data' => $data,
+            )
+        );
+    }
+
+    public function getListsAction()
+    {
+        return $this->listsAction();
+    }
+
     public function listsArchiveAction()
     {
         $lists = $this->getEntityManager()
