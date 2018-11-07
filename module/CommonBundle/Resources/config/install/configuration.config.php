422f83094a126b60f1f7f9d7faee6a480fed0406
Pieter Maene <pieter.maene@litus.cc>
2018-11-07 23:14:40 +0100

    Forgot to update last_upgrade configuration value

--- module/CommonBundle/Resources/config/install/configuration.config.php
+++ module/CommonBundle/Resources/config/install/configuration.config.php
@@ -457,7 +457,7 @@ You can remove any installed cookies from your computer or mobile device at any 
     ),
     array(
         'key'         => 'last_upgrade',
-        'value'       => '2018112901',
+        'value'       => '2018110700',
         'description' => 'The last Litus schema update performed on this system',
     ),
     array(
