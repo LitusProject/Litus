630412ac0fd2a22283c422cdea0cbe194143af29
Pieter Maene <pieter.maene@litus.cc>
2018-11-08 22:38:48 +0100

    Resolved validator name bug

--- module/MailBundle/Form/Admin/MailingList/Add.php
+++ module/MailBundle/Form/Admin/MailingList/Add.php
@@ -45,7 +45,7 @@ class Add extends \CommonBundle\Component\Form\Admin\Form
                             array('name' => 'StringTrim'),
                         ),
                         'validators' => array(
-                            array('name' => 'NamedList'),
+                            array('name' => 'ListName'),
                         ),
                     ),
                 ),
