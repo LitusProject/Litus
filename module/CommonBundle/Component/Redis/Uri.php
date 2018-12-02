3494427b287aba7ef7e12d27523b7279ea80c309
Pieter Maene <pieter.maene@litus.cc>
2018-12-02 01:01:28 +0100

    Support for more options in our Redis URI builder

--- module/CommonBundle/Component/Redis/Uri.php
+++ module/CommonBundle/Component/Redis/Uri.php
@@ -52,20 +52,29 @@ class Uri
             case 'tcp':
                 $uri = 'tcp://' . $components['host'] . ':' . $components['port'];
 
-                $options = array();
-                if (isset($components['password']) && $components['password'] != '') {
-                    $options[] = 'auth=' . $components['password'];
-                }
-
-                if (isset($components['database']) && $components['password'] != 0) {
-                    $options[] = 'database=' . $components['database'];
+                $parameters = array();
+                if (isset($components['weight']) && $components['weight'] !== null) {
+                    $parameters[] = 'weight=' . $components['weight'];
                 }
 
                 if (isset($components['timeout']) && $components['timeout'] !== null) {
-                    $options[] = 'timeout=' . $components['timeout'];
+                    $parameters[] = 'timeout=' . $components['timeout'];
                 }
 
-                return $uri . (count($options) > 0 ? '?' . implode('&', $options) : '');
+                if (isset($components['persistent_id']) && $components['persistent_id'] != '') {
+                    $parameters[] = 'persistent=1';
+                    $parameters[] = 'persistent_id=' . $components['persistent_id'];
+                }
+
+                if (isset($components['password']) && $components['password'] != '') {
+                    $parameters[] = 'auth=' . $components['password'];
+                }
+
+                if (isset($components['database']) && $components['password'] != 0) {
+                    $parameters[] = 'database=' . $components['database'];
+                }
+
+                return $uri . (count($parameters) > 0 ? '?' . implode('&', $parameters) : '');
 
             default:
                 throw new InvalidArgumentException('Invalid format');
