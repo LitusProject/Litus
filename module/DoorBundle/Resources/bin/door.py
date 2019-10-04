2e50b3d44a0be99806d6a2481f62f6374810a40f
Pieter Maene <pieter.maene@litus.cc>
2019-10-04 21:57:46 +0200

    Refactored door script to use raw keyboard events
    This commit also adds a systemd service to start this script on boot.

--- module/DoorBundle/Resources/bin/door.py
+++ module/DoorBundle/Resources/bin/door.py
@@ -8,6 +8,7 @@
 #
 
 import datetime
+import evdev
 import json
 import os.path as path
 import pickle
@@ -32,6 +33,7 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(GPIO_PORT, GPIO.OUT)
 
 # Globals
+identification = ''
 rules = None
 
 # Functions
@@ -73,46 +75,19 @@ def getRules():
         rules = pickle.load(cacheFile)
 
     cacheFile.close()
-#TODO use build in logger
-def log(message):
-    line = '[' +  datetime.datetime.now().strftime(LOG_TIME_FORMAT) + '] ' + message
-    try:
-        file = open(LOG_FILE, 'a')
-        try:
-            file.write(line + '\n')
-        finally:
-            file.close()
-    except IOError:
-        pass
 
-    print line
-
-def openDoor():
-    GPIO.output(GPIO_PORT, GPIO.HIGH)
-    time.sleep(2)
-    GPIO.output(GPIO_PORT, GPIO.LOW)
-
-# Main
-#TODO get this called each hour
-getRules()
-
-while True:
-    identification = raw_input('[' + datetime.datetime.now().strftime(LOG_TIME_FORMAT) + '] University Identfication: ')
-
+def input(identification):
     try:
         rules[identification]
     except KeyError:
         log('No rule for ' + identification)
-        continue
 
-    if False == isinstance(rules[identification], list) or len(rules[identification]) < 1:
+    if isinstance(rules[identification], list) == False or len(rules[identification]) < 1:
         log('No rule for ' + identification)
-        continue
 
     accessAllowed = False
-
     for rule in rules[identification]:
-        if None == rule['start_date'] and None == rule['end_date']:
+        if rule['start_date'] == None and rule['end_date'] == None:
             accessAllowed = True
             break
         else:
@@ -132,8 +107,48 @@ while True:
                     accessAllowed = True
                     break
 
-    if True == accessAllowed:
+    if accessAllowed == True:
         allowAccess(identification, rule['academic'])
 
     if path.getmtime(CACHE_FILE) < time.time()-CACHE_TTL:
         getRules()
+
+# TODO Use built-in logger
+def log(message):
+    line = '[' +  datetime.datetime.now().strftime(LOG_TIME_FORMAT) + '] ' + message
+    try:
+        file = open(LOG_FILE, 'a')
+        try:
+            file.write(line + '\n')
+        finally:
+            file.close()
+    except IOError:
+        pass
+
+    print line
+
+def openDoor():
+    GPIO.output(GPIO_PORT, GPIO.HIGH)
+    time.sleep(2)
+    GPIO.output(GPIO_PORT, GPIO.LOW)
+
+# Main
+# TODO Call this every hour
+getRules()
+
+device = evdev.InputDevice('/dev/input/event0')
+for event in device.read_loop():
+    if event.type == evdev.ecodes.EV_KEY:
+        if event.value == 1:
+            continue
+
+        if event.code == 42:
+            continue
+
+        if event.code == 28:
+            input(identification)
+            identification = ''
+
+            continue
+
+        identification = identification + (format(evdev.ecodes.KEY[event.code]).decode()[-1:]).lower()
--- module/DoorBundle/Resources/bin/door.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIRECTORY=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-cd "$SCRIPT_DIRECTORY/../../"
-
-python bin/DoorBundle/door.py
