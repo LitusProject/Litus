7e7a383c00a1ddd1ec29c37684a9cd1610cde5ea
Andreas Awouters <andreas.awouters@student.kuleuven.be>
2020-07-27 09:27:41 +0000

    unit email data csv generator

--- module/CommonBundle/Resources/config/install/configuration.config.php
+++ module/CommonBundle/Resources/config/install/configuration.config.php
@@ -100,6 +100,11 @@ Student IT',
         'description' => 'The system mail name',
     ),
     array(
+        'key'         => 'common.mail_domain',
+        'value'       => 'vtk.be',
+        'description' => 'mail domain used by unit csv generator',
+    ),
+    array(
         'key'         => 'organization_short_name',
         'value'       => 'Student IT',
         'description' => 'The short name of this organization',
