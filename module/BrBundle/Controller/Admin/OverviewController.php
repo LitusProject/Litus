395c2ee9490ccfae724145702180a3b653d8cfa5
Robin Wroblowski <robin.wroblowski@telenet.be>
2020-08-06 19:34:24 +0000

    Changed academic year to organization year for Br overview

--- module/BrBundle/Controller/Admin/OverviewController.php
+++ module/BrBundle/Controller/Admin/OverviewController.php
@@ -70,7 +70,7 @@ class OverviewController extends \CommonBundle\Component\Controller\ActionContro
             return new ViewModel();
         }
 
-        $currentYear = $this->getCurrentAcademicYear();
+        $currentYear = $this->getCurrentAcademicYear(true);
 
         $paginator = $this->paginator()->createFromQuery(
             $this->getEntityManager()
@@ -317,7 +317,7 @@ class OverviewController extends \CommonBundle\Component\Controller\ActionContro
         $totalMInvoiced = 0;
         $totalPaid = 0;
 
-        $currentYear = $this->getCurrentAcademicYear();
+        $currentYear = $this->getCurrentAcademicYear(true);
 
         $ids = $this->getEntityManager()
             ->getRepository('BrBundle\Entity\Contract')
