cd35145ed0e0d473697060648f207ad099f5d5fe
Robin Wroblowski <robin.wroblowski@telenet.be>
2020-09-28 18:43:34 +0000

    progression in Logi entities

---
+++ module/LogisticsBundle/Entity/Order.php
@@ -0,0 +1,372 @@
+<?php
+/**
+ * Litus is a project by a group of students from the KU Leuven. The goal is to create
+ * various applications to support the IT needs of student unions.
+ *
+ * @author Niels Avonds <niels.avonds@litus.cc>
+ * @author Karsten Daemen <karsten.daemen@litus.cc>
+ * @author Koen Certyn <koen.certyn@litus.cc>
+ * @author Bram Gotink <bram.gotink@litus.cc>
+ * @author Dario Incalza <dario.incalza@litus.cc>
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ * @author Lars Vierbergen <lars.vierbergen@litus.cc>
+ * @author Daan Wendelen <daan.wendelen@litus.cc>
+ * @author Mathijs Cuppens <mathijs.cuppens@litus.cc>
+ * @author Floris Kint <floris.kint@vtk.be>
+ *
+ * @license http://litus.cc/LICENSE
+ */
+
+namespace LogisticsBundle\Entity;
+
+use CommonBundle\Component\Util\StringUtil;
+use CommonBundle\Entity\General\Location;
+use CommonBundle\Entity\General\Organization\Unit;
+use CommonBundle\Entity\User\Person;
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+use InvalidArgumentException;
+use Parsedown;
+use function Functional\push;
+
+/**
+ * This is the entity for an order.
+ *
+ * @ORM\Entity(repositoryClass="LogisticsBundle\Entity\Order")
+ * @ORM\Table(name="br_companies_jobs")
+ */
+class Order
+{
+    /**
+     * @var string The order's ID
+     *
+     * @ORM\Id
+     * @ORM\Column(type="bigint")
+     * @ORM\GeneratedValue
+     */
+    private $id;
+
+    /**
+     * @var string The order's name
+     *
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @var string The description of the order
+     *
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @var string The mail-address for the contact for this order
+     *
+     * @ORM\Column(name="email", type="text", nullable=true)
+     */
+    private $email;
+
+    /**
+     * @var Location The location (eg. province) where the order is located
+     *
+     * @ORM\Column(type="text")
+     */
+    private $location;
+
+    /**
+     * @var Person The contact for whom this order is meant
+     *
+     * @ORM\Column(type="Person")
+     */
+    private $contact;
+
+    /**
+     * @var Unit The unit of the order
+     *
+     * @ORM\ManyToOne(targetEntity="\CommonBundle\Entity\General\Organization\Unit")
+     * @ORM\JoinColumn(name="unit", referencedColumnName="id")
+     */
+    private $unit;
+
+    /**
+     * @var DateTime The last time this order was updated.
+     *
+     * @ORM\Column(type="date")
+     */
+    private $dateUpdated;
+
+    /**
+     * @var DateTime The start date and time of this order.
+     *
+     * @ORM\Column(name="start_date", type="datetime")
+     */
+    private $startDate;
+
+    /**
+     * @var DateTime The end date and time of this order.
+     *
+     * @ORM\Column(name="end_date", type="datetime")
+     */
+    private $endDate;
+
+    /**
+     * @var boolean If this order has been approved by our Logistics team
+     *
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $approved;
+
+    /**
+     * @var boolean If this order has been removed.
+     *
+     * @ORM\Column(type="boolean", options={"default" = false})
+     */
+    private $removed;
+
+    /**
+     * @var array an array containing the article bookings of the order
+     *
+     * @ORM\OneToMany(targetEntity="\LogisticsBundle\Entity\Booking")
+     * @ORM\JoinColumn(name="booking", referencedColumnName="id")
+     */
+    private $bookings;
+
+    /**
+     * @param Person $contact The order's contact
+     */
+    public function __construct(Person $contact)
+    {
+        $this->contact = $contact;
+        $this->dateUpdated = new DateTime();
+        $this->removed = false;
+    }
+
+    /**
+     * @return array
+     */
+    public function getBookings()
+    {
+        return $this->bookings;
+    }
+
+    /**
+     * @param array $booking
+     * @return self
+     */
+    public function addBooking($booking)
+    {
+        array_push($this->bookings, $booking);
+        return $this;
+
+    }
+
+    /**
+     * @return self
+     */
+    public function approve()
+    {
+        $this->approved = true;
+
+        return $this;
+    }
+
+    /**
+     * @return self
+     */
+    public function pending()
+    {
+        $this->approved = false;
+
+        return $this;
+    }
+
+    /**
+     * @return self
+     */
+    public function remove()
+    {
+        $this->removed = true;
+
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isRemoved()
+    {
+        return $this->removed;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isApproved()
+    {
+        if ($this->approved === null) {
+            return true;
+        }
+
+        return $this->approved;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param  string $name
+     * @return Order
+     */
+    public function setName($name)
+    {
+        if ($name === null || !is_string($name)) {
+            throw new InvalidArgumentException('Invalid name');
+        }
+
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param  string $description
+     * @return Order
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param  string $email
+     * @return Order
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+
+    /**
+     * @return Unit
+     */
+    public function getUnit()
+    {
+        return $this->unit;
+    }
+
+    /**
+     * @return Order
+     */
+    public function updateDate()
+    {
+        $this->dateUpdated = new DateTime();
+
+        return $this;
+    }
+
+    /**
+     * @return DateTime The last time this order was updated.
+     */
+    public function getLastUpdateDate()
+    {
+        return $this->dateUpdated;
+    }
+
+    /**
+     * @param  DateTime $startDate
+     * @return Order
+     */
+    public function setStartDate($startDate)
+    {
+        $this->startDate = $startDate;
+
+        return $this;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getStartDate()
+    {
+        return $this->startDate;
+    }
+
+    /**
+     * @param  DateTime $endDate
+     * @return Order
+     */
+    public function setEndDate($endDate)
+    {
+        $this->endDate = $endDate;
+
+        return $this;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getEndDate()
+    {
+        return $this->endDate;
+    }
+
+    /**
+     * @return Location
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    /**
+     * @param Location $location
+     */
+    public function setLocation($location)
+    {
+        $this->location = $location;
+    }
+
+    /**
+     * @return Person
+     */
+    public function getContact()
+    {
+        return $this->contact;
+    }
+}
---
+++ module/LogisticsBundle/Entity/Request.php
@@ -0,0 +1,271 @@
+<?php
+/**
+ * Litus is a project by a group of students from the KU Leuven. The goal is to create
+ * various applications to support the IT needs of student unions.
+ *
+ * @author Niels Avonds <niels.avonds@litus.cc>
+ * @author Karsten Daemen <karsten.daemen@litus.cc>
+ * @author Koen Certyn <koen.certyn@litus.cc>
+ * @author Bram Gotink <bram.gotink@litus.cc>
+ * @author Dario Incalza <dario.incalza@litus.cc>
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ * @author Lars Vierbergen <lars.vierbergen@litus.cc>
+ * @author Daan Wendelen <daan.wendelen@litus.cc>
+ * @author Mathijs Cuppens <mathijs.cuppens@litus.cc>
+ * @author Floris Kint <floris.kint@vtk.be>
+ *
+ * @license http://litus.cc/LICENSE
+ */
+
+namespace LogisticsBundle\Entity;
+
+use LogisticsBundle\Entity\Order;
+use CommonBundle\Entity\User\Person\Academic;
+use LogisticsBundle\Entity\Article;
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+use RuntimeException;
+
+/**
+ * This entity stores a request for an order.
+ *
+ * @ORM\Entity(repositoryClass="LogisticsBundle\Entity\Request")
+ * @ORM\Table(name="logistics_requests")
+ */
+class Request
+{
+    /**
+     * @var request's ID
+     *
+     * @ORM\Id
+     * @ORM\Column(type="bigint")
+     * @ORM\GeneratedValue
+     */
+    private $id;
+
+    /**
+     * @var Academic The contact used in this order
+     *
+     * @ORM\ManyToOne(targetEntity="CommonBundle\Entity\User\Person\Academic")
+     * @ORM\JoinColumn(name="contact", referencedColumnName="id")
+     */
+    private $contact;
+
+    /**
+     * @var DateTime The time of creation of this node
+     *
+     * @ORM\Column(name="creation_time", type="datetime")
+     */
+    private $creationTime;
+
+    /**
+     * @var boolean True if the request has been handled, false if not.
+     *
+     * @ORM\Column(type="boolean")
+     */
+    private $handled;
+
+    /**
+     * @var string The type of the request
+     *
+     * @ORM\Column(type="text")
+     */
+    private $requestType;
+
+    /**
+     * @var Job
+     *
+     * @ORM\ManyToOne(targetEntity="LogisticsBundle\Entity\Order")
+     * @ORM\JoinColumn(name="job", referencedColumnName="id")
+     */
+    private $job;
+
+    /**
+     * @var Order
+     *
+     * @ORM\ManyToOne(targetEntity="LogisticsBundle\Entity\Order")
+     * @ORM\JoinColumn(name="edit_job", referencedColumnName="id", nullable=true)
+     */
+    private $editOrder;
+
+    /**
+     * @var string The type of the request
+     *
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $rejectMessage;
+
+    /**
+     * @static
+     * @var array All the possible requests allowed
+     */
+    public static $possibleRequests = array(
+        'edit'        => 'edit',
+        'edit reject' => 'edit reject',
+        'add'         => 'add',
+        'delete'      => 'delete',
+    );
+
+    /**
+     * @param Academic $contact
+     * @param Order       $order
+     * @param string    $requestType
+     * @param Academic $contact
+     * @param Order|null  $editOrder
+     */
+    public function __construct(Academic $contact, Order $order, $requestType, Academic $contact, Order $editOrder = null)
+    {
+        $this->creationTime = new DateTime();
+        $this->contact = $contact;
+        $this->handled = false;
+        $this->order = $order;
+        $this->setRequestType($requestType);
+        $this->editOrder = $editOrder;
+    }
+
+    /**
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param  \CommonBundle\Entity\User\Person\Academic $contact
+     * @return \LogisticsBundle\Entity\Request
+     */
+    public function setContact($contact)
+    {
+        $this->contact = $contact;
+
+        return $this;
+    }
+
+    /**
+     * @return \CommonBundle\Entity\User\Person\Academic
+     */
+    public function getContact()
+    {
+        return $this->contact;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreationTime()
+    {
+        return $this->creationTime;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function handled()
+    {
+        $this->handled = true;
+
+        return true;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus()
+    {
+        $result = 'pending';
+
+        if ($this->handled) {
+            $result = 'rejected';
+
+            if ($this->getOrder()->isApproved()) {
+                $result = 'approved';
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param string $type
+     */
+    private function setRequestType($type)
+    {
+        if (!in_array($type, self::$possibleRequests)) {
+            throw new RuntimeException('The requested type does not exist for the order');
+        }
+
+        $this->requestType = $type;
+    }
+
+    /**
+     * @return Order
+     */
+    public function getOrder()
+    {
+        return $this->order;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRejectMessage()
+    {
+        return $this->rejectMessage;
+    }
+
+    /**
+     * @return Order
+     */
+    public function getEditJob()
+    {
+        return $this->editOrder;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRequestType()
+    {
+        return $this->requestType;
+    }
+
+    /**
+     * @return null
+     */
+    public function approveRequest()
+    {
+        switch ($this->requestType) {
+            case 'add':
+                $this->getOrder()->approve();
+                break;
+
+            case 'edit':
+                $this->getOrder()->approve();
+                $this->getEditOrder()->remove();
+                break;
+
+            case 'edit reject':
+                $this->getOrder()->approve();
+                if (($editOrder = $this->getEditOrder())!== null)
+                    $editOrder->remove();
+                break;
+
+            case 'delete':
+                $this->getOrder()->remove();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    /**
+     * @return null
+     */
+    public function rejectRequest($message)
+    {
+        $this->rejectMessage = $message;
+    }
+}
