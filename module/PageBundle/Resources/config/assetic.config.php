6375f4dd6b95ad76f286154f298b9e0c5c253a60
Pieter Maene <pieter.maene@litus.cc>
2021-08-27 19:34:24 +0200

    Removed license header from PHP source files
    This project has seen contributions from a lot of different people over
    the past ten years. The current author list was terribly out-of-date and
    listing everyone would have made it too long. It therefore seemed to
    make sense to just get rid of it instead ☺️.

--- module/PageBundle/Resources/config/assetic.config.php
+++ module/PageBundle/Resources/config/assetic.config.php
@@ -1,22 +1,4 @@
 <?php
-/**
- * Litus is a project by a group of students from the KU Leuven. The goal is to create
- * various applications to support the IT needs of student unions.
- *
- * @author Niels Avonds <niels.avonds@litus.cc>
- * @author Karsten Daemen <karsten.daemen@litus.cc>
- * @author Koen Certyn <koen.certyn@litus.cc>
- * @author Bram Gotink <bram.gotink@litus.cc>
- * @author Dario Incalza <dario.incalza@litus.cc>
- * @author Pieter Maene <pieter.maene@litus.cc>
- * @author Kristof Mariën <kristof.marien@litus.cc>
- * @author Lars Vierbergen <lars.vierbergen@litus.cc>
- * @author Daan Wendelen <daan.wendelen@litus.cc>
- * @author Mathijs Cuppens <mathijs.cuppens@litus.cc>
- * @author Floris Kint <floris.kint@vtk.be>
- *
- * @license http://litus.cc/LICENSE
- */
 
 return array(
     'controllers' => array(
@@ -54,7 +36,7 @@ return array(
             '@common_jquery',
             '@bootstrap_css',
             '@site_css',
-            '@page_css', 
+            '@page_css',
             '@bootstrap_js_dropdown',
             '@bootstrap_js_transition',
             '@bootstrap_js_modal',
