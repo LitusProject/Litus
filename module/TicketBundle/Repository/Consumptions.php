f6aae2ab42b7d50d0a9d852ca034383db9705453
stancardinaels <stancardinaels@hotmail.com>
2021-10-13 12:35:02 +0200

    now for real

--- module/TicketBundle/Repository/Consumptions.php
+++ module/TicketBundle/Repository/Consumptions.php
@@ -8,19 +8,6 @@ use CommonBundle\Repository\User\Person;
 
 class Consumptions extends \CommonBundle\Component\Doctrine\ORM\EntityRepository
 {
-//    public function findAllActiveQuery()
-//    {
-//        $query = $this->getEntityManager()->createQueryBuilder();
-//        return $query->select('r')
-//            ->from('TicketBundle\Entity\Consumptions', 'r')
-//            ->where(
-//                $query->expr()->gte('r.getAcademic()', ':start')
-//            )
-//            ->setParameter('start', new Person())
-//            ->orderBy('r.id')
-//            ->getQuery();
-//    }
-
     public function findAllByUserNameQuery($username)
     {
         $query = $this->getEntityManager()->createQueryBuilder();
