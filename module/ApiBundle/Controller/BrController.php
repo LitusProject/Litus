fd65ee4a19a842bef4e4d24a2474bad20353f2cb
stancardinaels <stancardinaels@hotmail.com>
2022-07-04 13:09:43 +0000

    br api start of add cv book

--- module/ApiBundle/Controller/BrController.php
+++ module/ApiBundle/Controller/BrController.php
@@ -2,10 +2,37 @@
 
 namespace ApiBundle\Controller;
 
+use BrBundle\Entity\Company;
+
 /**
  * BrController
  */
 class BrController extends \ApiBundle\Component\Controller\ActionController\ApiController
 {
+    public function newCompanyAction()
+    {
+
+    }
+
+    public function addCvBookAction()
+    {
+        $this->initJson();
+
+        if (!$this->getRequest()->isPost()) {
+            return $this->error(405, 'This endpoint can only be accessed through POST');
+        }
+
+        $input = $this->getRequest()->getContent();
+        $json = json_decode($input);
+
+        $company_name = $json->company;
+
+        $company = $this->getEntityManager()
+            ->getRepository('BrBundle\Entity\Company')
+            ->findallByNameQuery($company_name)
+            ->getResult();
+
+    }
+
 
 }
