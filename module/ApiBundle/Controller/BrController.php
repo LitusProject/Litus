e48da0b481d0c71729640c9e3cd49a74da6c5262
stancardinaels <stancardinaels@hotmail.com>
2022-07-05 11:27:20 +0200

    add company page visible through api

--- module/ApiBundle/Controller/BrController.php
+++ module/ApiBundle/Controller/BrController.php
@@ -18,8 +18,10 @@ class BrController extends \ApiBundle\Component\Controller\ActionController\ApiC
     /**
      * input: json {
      *      "key": "api key",
-     *      "company": "company name"
+     *      "company": "company id"
      * }
+     *
+     * TO DO: navragen of BR ID al heeft in Odoo, anders aanpassen naar naam
      */
     public function addCvBookAction()
     {
@@ -32,14 +34,63 @@ class BrController extends \ApiBundle\Component\Controller\ActionController\ApiC
         $input = $this->getRequest()->getContent();
         $json = json_decode($input);
 
-        $company_name = $json->company;
+//        $company_name = $json->company;
+//
+//        $company = $this->getEntityManager()
+//            ->getRepository('BrBundle\Entity\Company')
+//            ->findallByNameQuery($company_name)
+//            ->getResult()[0];
+
+        $company_id = $json->company;
 
         $company = $this->getEntityManager()
             ->getRepository('BrBundle\Entity\Company')
-            ->findallByNameQuery($company_name)
-            ->getResult()[0];
+            ->findById($company_id)[0];
 
         $company->addCvBookYear($this->getCurrentAcademicYear());
+
+        $this->getEntityManager()->flush();
+
+        return new ViewModel(
+            array(
+                'result' => (object) array(
+                    'status' => 'success',
+                ),
+            )
+        );
+    }
+
+    /**
+     * input: json {
+     *      "key": "api key",
+     *      "company": "company id"
+     * }
+     *
+     * TO DO: navragen of BR ID al heeft in Odoo, anders aanpassen naar naam
+     */
+    public function addPageVisibleAction()
+    {
+        $this->initJson();
+
+        if (!$this->getRequest()->isPost()) {
+            return $this->error(405, 'This endpoint can only be accessed through POST');
+        }
+
+        $input = $this->getRequest()->getContent();
+        $json = json_decode($input);
+
+        $company_id = $json->company;
+
+//        $company = $this->getEntityManager()
+//            ->getRepository('BrBundle\Entity\Company')
+//            ->findallByNameQuery($company_name)
+//            ->getResult()[0];
+
+        $page = $this->getEntityManager()
+            ->getRepository('BrBundle\Entity\Company\Page')
+            ->findById($company_id)[0];
+
+        $page->addYear($this->getCurrentAcademicYear());
 
         $this->getEntityManager()->flush();
 
