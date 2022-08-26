f3504aa6a78155b2f5a8f0c95348417b502b5938
Robbe Serry <robbeserry@gmail.com>
2022-08-26 15:46:27 +0000

    forgot migration

---
+++ migrations/Version20220826131142.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Version 20220826131142
+ */
+class Version20220826131142 extends \Doctrine\Migrations\AbstractMigration
+{
+    /**
+     * @param  \Doctrine\DBAL\Schema\Schema $schema
+     * @return void
+     */
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE publications_videos ADD date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL');
+    }
+
+    /**
+     * @param  \Doctrine\DBAL\Schema\Schema $schema
+     * @return void
+     */
+    public function down(Schema $schema) : void
+    {
+        $this->throwIrreversibleMigrationException();
+    }
+}
