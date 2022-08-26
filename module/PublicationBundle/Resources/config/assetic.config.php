7035aa90df548863b7ac8c7103392ea9f65932d8
Robbe Serry <robbeserry@gmail.com>
2022-08-26 15:42:20 +0000

    sort videos by date

--- module/PublicationBundle/Resources/config/assetic.config.php
+++ module/PublicationBundle/Resources/config/assetic.config.php
@@ -47,6 +47,10 @@ return array(
             '@admin_js',
             '@bootstrap_js_transition',
             '@bootstrap_js_modal',
+            '@common_jqueryui',
+            '@common_jqueryui_datepicker',
+            '@common_jqueryui_css',
+            '@common_jqueryui_datepicker_css',
         ),
         'publication_archive' => array(
             '@common_jquery',
