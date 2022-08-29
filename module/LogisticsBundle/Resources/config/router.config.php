1fd15bc85029dd3781f43cbb0ac56cd5f48f3327
stancardinaels <stancardinaels@hotmail.com>
2022-08-29 17:48:16 +0200

    for some reason some changes were undone

--- module/LogisticsBundle/Resources/config/router.config.php
+++ module/LogisticsBundle/Resources/config/router.config.php
@@ -125,23 +125,21 @@ return array(
                 ),
             ),
         ),
-//        'logistics_admin_consumptions' => array(
-//            'type'    => 'Laminas\Router\Http\Segment',
-//            'options' => array(
-//                'route'       => '/admin/logistics/consumptions[/:action[/:id][/:field/:string][/page/:page]][/]',
-//                'constraints' => array(
-//                    'action'   => '[a-zA-Z][a-zA-Z0-9_-]*',
-//                    'id'       => '[0-9]*',
-//                    'page'   => '[0-9]*',
-//                    'field'  => '[a-zA-Z][a-zA-Z0-9_-]*',
-//                    'string' => '[a-zA-Z][%a-zA-Z0-9:.,_-]*',
-//                ),
-//                'defaults' => array(
-//                    'controller' => 'logistics_admin_consumptions',
-//                    'action'     => 'manage',
-//                ),
-//            ),
-//        ),
+        'logistics_admin_inventory' => array(
+            'type'    => 'Laminas\Router\Http\Segment',
+            'options' => array(
+                'route'       => '/admin/logistics/inventory[/:action[/:id]][/page/:page][/]',
+                'constraints' => array(
+                    'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                    'id'     => '[0-9]*',
+                    'page'   => '[0-9]*',
+                ),
+                'defaults'    => array(
+                    'controller' => 'logistics_admin_inventory',
+                    'action'     => 'manage',
+                ),
+            ),
+        ),
         'logistics_index' => array(
             'type'    => 'Laminas\Router\Http\Segment',
             'options' => array(
@@ -290,7 +288,7 @@ return array(
         'logistics_admin_van_reservation'   => 'LogisticsBundle\Controller\Admin\VanReservationController',
         'logistics_admin_piano_reservation' => 'LogisticsBundle\Controller\Admin\PianoReservationController',
         'logistics_admin_lease'             => 'LogisticsBundle\Controller\Admin\LeaseController',
-//        'logistics_admin_consumptions'      => 'LogisticsBundle\Controller\Admin\ConsumptionsController',
+        'logistics_admin_inventory'         => 'LogisticsBundle\Controller\Admin\InventoryController',
 
         'logistics_index'                   => 'LogisticsBundle\Controller\IndexController',
         'logistics_auth'                    => 'LogisticsBundle\Controller\AuthController',
