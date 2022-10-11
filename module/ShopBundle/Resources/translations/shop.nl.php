61bbff7d5b9fdc18c23a30a878061afbf093b5f0
RobbeS3 <robbeserry@gmail.com>
2022-10-11 19:35:21 +0000

    theo 0 broodjes beschikbaar verandert naar uitverkocht

--- module/ShopBundle/Resources/translations/shop.nl.php
+++ module/ShopBundle/Resources/translations/shop.nl.php
@@ -27,5 +27,6 @@ return array(
     'An error occurred while trying to cancel your reservation'                    => 'Er trad een fout op tijdens het annuleren van uw reservatie.',
     'However, there are no future sales sessions open for reservations yet.'       => 'Er zijn echter nog geen verkoopsessies beschikbaar om reservaties te plaatsen.',
     'available'                                                                    => 'beschikbaar',
+    'soldout'                                                                      => 'Dit broodje is jammer genoeg uitverkocht, probeer volgende keer iets vroeger :)',
     'My Sandwiches'                                                                => 'Mijn Broodjes',
 );
