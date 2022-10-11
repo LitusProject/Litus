3f1d8655880942b94784851c181746c738c5de34
RobbeS3 <robbeserry@gmail.com>
2022-10-11 15:48:49 +0000

    changed shift mails to 24h date format

--- module/ShiftBundle/Controller/RegistrationShiftController.php
+++ module/ShiftBundle/Controller/RegistrationShiftController.php
@@ -222,7 +222,7 @@ class RegistrationShiftController extends \CommonBundle\Component\Controller\Act
                     $message = $mailData[$language->getAbbrev()]['content'];
                     $subject = $mailData[$language->getAbbrev()]['subject'];
 
-                    $shiftString = $shift->getName() . ' from ' . $shift->getStartDate()->format('d/m/Y h:i') . ' to ' . $shift->getEndDate()->format('d/m/Y h:i');
+                    $shiftString = $shift->getName() . ' from ' . $shift->getStartDate()->format('d/m/Y H:i') . ' to ' . $shift->getEndDate()->format('d/m/Y H:i');
 
                     $mail = new Message();
                     $mail->setEncoding('UTF-8')
