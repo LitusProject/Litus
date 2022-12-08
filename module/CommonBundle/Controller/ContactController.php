abd6cbb52bb4ace22a7d6831f5dcefe7fdbc590c
Dries Vanspauwen <dries.vanspauwen@gmail.com>
2022-12-08 11:53:28 +0000

    Add contact page translations and code comments

--- module/CommonBundle/Controller/ContactController.php
+++ module/CommonBundle/Controller/ContactController.php
@@ -6,20 +6,14 @@ use Laminas\View\Model\ViewModel;
 
 /**
  * ContactController
+ *
+ * At this moment this controller is not being used but, the code is still here so it can be used in the future.
  */
 class ContactController extends \CommonBundle\Component\Controller\ActionController\SiteController
 {
     public function indexAction() {
 
-        $contactInfo = array(
-            'activiteiten'      => 'activiteiten@vtk.be',
-            'bedrijvenrelaties' => 'bedrijvenrelaties@vtk.be',
-        );
-
         return new ViewModel(
-            array(
-                'contactInfo'  => $contactInfo
-            )
         );
     }
 }
