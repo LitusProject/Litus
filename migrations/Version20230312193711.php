3c5c970d651864498ffe424bdbd4f594149072aa
stancardinaels <stancardinaels@hotmail.com>
2023-03-12 20:45:48 +0100

    start off limit per option per person

---
+++ migrations/Version20230312193711.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Version 20230312193711
+ */
+class Version20230312193711 extends \Doctrine\Migrations\AbstractMigration
+{
+    /**
+     * @param  \Doctrine\DBAL\Schema\Schema $schema
+     * @return void
+     */
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE ticket_events_options ADD limit_per_person_option INT DEFAULT NULL');
+    }
+
+    /**
+     * @param  \Doctrine\DBAL\Schema\Schema $schema
+     * @return void
+     */
+    public function down(Schema $schema) : void
+    {
+        $this->throwIrreversibleMigrationException();
+    }
+}
