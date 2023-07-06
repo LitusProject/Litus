2a2a5414ac0060a70434f77c85989bcac06d4be1
Stan Cardinaels <72024517+stancardinaels@users.noreply.github.com>
2023-07-06 17:07:37 +0200

    Update BrController.php

--- module/ApiBundle/Controller/BrController.php
+++ module/ApiBundle/Controller/BrController.php
@@ -30,12 +30,12 @@ class BrController extends \ApiBundle\Component\Controller\ActionController\ApiC
 
         $company = new Company();
         $company->setName($company_name);
-
-        $id = $company->getId();
         
         $this->getEntityManager()->persist($company);
         $this->getEntityManager()->flush();
 
+        $id = $company->getId();
+        
         return new ViewModel(
             array(
                 'result' => (object) array(
