023596457a641a6ed8427b1c2e7cea4487e12586
Robbe Serry <robbeserry@gmail.com>
2023-10-11 12:17:49 +0000

    add order number to category

---
+++ migrations/Version20231011121343.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Version 20231011121343
+ */
+class Version20231011121343 extends \Doctrine\Migrations\AbstractMigration
+{
+    /**
+     * @param  \Doctrine\DBAL\Schema\Schema $schema
+     * @return void
+     */
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE nodes_pages_categories ADD order_number INT DEFAULT NULL');
+    }
+
+    /**
+     * @param  \Doctrine\DBAL\Schema\Schema $schema
+     * @return void
+     */
+    public function down(Schema $schema) : void
+    {
+        $this->throwIrreversibleMigrationException();
+    }
+}
