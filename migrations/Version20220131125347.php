c991c9e66374d95be10cabb5130c4ba6a489b722
Pieter Maene <pieter.maene@litus.cc>
2023-10-16 07:07:26 +0200

    Resolved PHP_CodeSniffer issues ✨

--- migrations/Version20220131125347.php
+++ migrations/Version20220131125347.php
@@ -52,7 +52,7 @@ class Version20220131125347 extends \Doctrine\Migrations\AbstractMigration
         $this->addSql('ALTER TABLE br_match ADD CONSTRAINT FK_CDBA31974FBF094F FOREIGN KEY (company) REFERENCES br_match_matchee_map_company (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE br_match ADD CONSTRAINT FK_CDBA3197B723AF33 FOREIGN KEY (student) REFERENCES br_match_matchee_map_student (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE br_match ADD CONSTRAINT FK_CDBA3197DA04AD89 FOREIGN KEY (wave) REFERENCES br_match_companywave_match_map (id) ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
- }
+    }
 
     /**
      * @param  \Doctrine\DBAL\Schema\Schema $schema
--- migrations/Version20230825112313.php
+++ migrations/Version20230825112313.php
@@ -19,7 +19,6 @@ class Version20230825112313 extends \Doctrine\Migrations\AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('ALTER TABLE nodes_events ADD is_career BOOLEAN DEFAULT \'false\'');
-
     }
 
     /**
