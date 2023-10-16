472cf255d625cbd7344c38d8b4b816fc009b5921
pedrod <pedro.devogelaere@vtk.be>
2023-10-16 21:31:07 +0000

    automatic shifts add for waaiberg cantus

--- module/CalendarBundle/Controller/Admin/CalendarController.php
+++ module/CalendarBundle/Controller/Admin/CalendarController.php
@@ -11,6 +11,7 @@ use Laminas\View\Model\ViewModel;
  * Handles system admin for calendar.
  *
  * @author Kristof Mariën <kristof.marien@litus.cc>
+ * @author Pedro Devogelaere <pedro.devogelaere@vtk.be>
  */
 class CalendarController extends \CommonBundle\Component\Controller\ActionController\AdminController
 {
@@ -65,6 +66,187 @@ class CalendarController extends \CommonBundle\Component\Controller\ActionContro
                 $this->flashMessenger()->success(
                     'Success',
                     'The event was successfully added!'
+                );
+
+                $this->redirect()->toRoute(
+                    'calendar_admin_calendar',
+                    array(
+                        'action' => 'manage',
+                    )
+                );
+
+                return new ViewModel();
+            }
+        }
+
+        return new ViewModel(
+            array(
+                'form' => $form,
+            )
+        );
+    }
+
+    public function templateAction()
+    {
+        $form = $this->getForm('calendar_event_add');
+        $shiftForm = $this->getForm('shift_shift_add');
+
+        if ($this->getRequest()->isPost()) {
+            $formData = $this->getRequest()->getPost();
+            $form->setData($formData);
+
+            if ($form->isValid()) {
+                $event = $form->hydrateObject();
+
+                $this->getEntityManager()->persist($event);
+                $this->getEntityManager()->flush();
+
+                $data = array(
+                    'manager'               => false,
+                    'unit'                  => 3,
+                    'event'                 => $event->getId(),
+                    'location'              => 2,
+                    'nb_responsibles'       => 0,
+                    'handled_on_event'      => false,
+                    'ticket_needed'         => false,
+                    'points'                => 0,
+                );
+
+                $startDate = $event->getStartDate()->format('d/m/Y');
+                $endDate = $event->getEndDate()->format('d/m/Y');
+
+                $data['name'] = 'Pispolitie';
+                $data['description'] = 'Mensen die naar het toilet willen een strafje geven';
+                $data['nb_volunteers'] = 2;
+                $data['nb_volunteers_min'] = 2;
+                $data['reward'] = 2;
+                $data['start_date'] = $startDate . ' 20:30';
+                $data['end_date'] = $startDate . ' 22:00';
+
+                $this->getEntityManager()->persist(
+                    $shiftForm->getHydrator()->hydrate($data)
+                );
+
+                $data['start_date'] = $startDate . ' 22:15';
+                $data['end_date'] = $endDate . ' 00:00';
+
+                $this->getEntityManager()->persist(
+                    $shiftForm->getHydrator()->hydrate($data)
+                );
+
+                $data['name'] = 'Controleren QR-code + bandjes uitdelen';
+                $data['description'] = 'Controleren QR-code + bandjes uitdelen';
+                $data['reward'] = 1;
+                $data['start_date'] = $startDate . ' 20:00';
+                $data['end_date'] = $startDate . ' 20:30';
+
+                $this->getEntityManager()->persist(
+                    $shiftForm->getHydrator()->hydrate($data)
+                );
+
+                $data['name'] = 'Stewarden';
+                $data['description'] = 'Zorgen dat er niemand luid is buiten en fiksen dat er geen drank buiten geraakt';
+                $data['start_date'] = $startDate . ' 22:00';
+                $data['end_date'] = $startDate . ' 22:15';
+
+                $this->getEntityManager()->persist(
+                    $shiftForm->getHydrator()->hydrate($data)
+                );
+
+                $data['start_date'] = $endDate . ' 00:00';
+                $data['end_date'] = $endDate . ' 00:15';
+
+                $this->getEntityManager()->persist(
+                    $shiftForm->getHydrator()->hydrate($data)
+                );
+
+                $data['name'] = 'Bijrijden';
+                $data['description'] = 'Chille in de kar. Locatie van de loods is: Tervuursevest 238';
+                $data['nb_volunteers_min'] = 1;
+                $data['start_date'] = $startDate . ' 18:00';
+                $data['end_date'] = $startDate . ' 19:00';
+
+                $this->getEntityManager()->persist(
+                    $shiftForm->getHydrator()->hydrate($data)
+                );
+
+                $data['name'] = 'Corona stilhouden';
+                $data['description'] = 'Je mag ze ook buiten gooien als ze niet stil zijn xxx';
+                $data['start_date'] = $endDate . ' 00:15';
+                $data['end_date'] = $endDate . ' 01:00';
+
+                $this->getEntityManager()->persist(
+                    $shiftForm->getHydrator()->hydrate($data)
+                );
+
+                $data['name'] = 'Bandjes controleren';
+                $data['description'] = 'Bandjes controleren tijdens de tempus';
+                $data['nb_volunteers'] = 1;
+                $data['start_date'] = $startDate . ' 22:00';
+                $data['end_date'] = $startDate . ' 22:15';
+
+                $this->getEntityManager()->persist(
+                    $shiftForm->getHydrator()->hydrate($data)
+                );
+
+                $data['start_date'] = $endDate . ' 00:00';
+                $data['end_date'] = $endDate . ' 00:15';
+
+                $this->getEntityManager()->persist(
+                    $shiftForm->getHydrator()->hydrate($data)
+                );
+
+                $data['name'] = 'Tappen & Rondbrengen';
+                $data['description'] = 'Tappen & Rondbrengen';
+                $data['nb_volunteers'] = 4;
+                $data['nb_volunteers_min'] = 3;
+                $data['reward'] = 2;
+                $data['start_date'] = $startDate . ' 20:30';
+                $data['end_date'] = $startDate . ' 22:00';
+
+                $this->getEntityManager()->persist(
+                    $shiftForm->getHydrator()->hydrate($data)
+                );
+
+                $data['start_date'] = $startDate . ' 22:15';
+                $data['end_date'] = $endDate . ' 00:00';
+
+                $this->getEntityManager()->persist(
+                    $shiftForm->getHydrator()->hydrate($data)
+                );
+
+                $data['start_date'] = $endDate . ' 00:15';
+                $data['end_date'] = $endDate . ' 01:00';
+                $data['reward'] = 1;
+
+                $this->getEntityManager()->persist(
+                    $shiftForm->getHydrator()->hydrate($data)
+                );
+
+                $data['name'] = 'Opbouwen';
+                $data['description'] = 'Wat tafels en stoelen klaar zetten voor de cantus';
+                $data['start_date'] = $startDate . ' 19:00';
+                $data['end_date'] = $startDate . ' 20:00';
+
+                $this->getEntityManager()->persist(
+                    $shiftForm->getHydrator()->hydrate($data)
+                );
+
+                $data['name'] = 'Afbraak';
+                $data['description'] = 'Please help ons mee en zorg dat we na een half uurtje klaar kunnen zijn :))';
+                $data['nb_volunteers'] = 5;
+                $data['start_date'] = $endDate . ' 01:00';
+                $data['end_date'] = $endDate . ' 02:00';
+
+                $this->getEntityManager()->persist(
+                    $shiftForm->getHydrator()->hydrate($data)
+                );
+
+                $this->getEntityManager()->flush();
+
+                $this->flashMessenger()->success(
+                    'Success',
+                    'The event and shifts were successfully added!'
                 );
 
                 $this->redirect()->toRoute(
--- module/CalendarBundle/Resources/config/install/acl.config.php
+++ module/CalendarBundle/Resources/config/install/acl.config.php
@@ -3,7 +3,7 @@
 return array(
     'calendarbundle' => array(
         'calendar_admin_calendar' => array(
-            'add', 'delete', 'edit', 'editPoster', 'manage', 'old', 'poster', 'upload',
+            'add', 'delete', 'edit', 'editPoster', 'manage', 'old', 'poster', 'upload', 'template',
         ),
         'calendar_admin_calendar_registration' => array(
             'export', 'manage',
