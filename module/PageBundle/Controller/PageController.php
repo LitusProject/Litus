0dad6be4375596c81223a41ab55c7c6f3e91fd02
Robbe Serry <robbeserry@gmail.com>
2023-12-19 10:36:21 +0000

    fix error with pdf loading

--- module/PageBundle/Controller/PageController.php
+++ module/PageBundle/Controller/PageController.php
@@ -58,7 +58,6 @@ class PageController extends \CommonBundle\Component\Controller\ActionController
             array(
                 'Content-Disposition' => 'inline; filename="' . $this->getParam('name') . '"',
                 'Content-Type'        => mime_content_type($filePath),
-                'Content-Length'      => filesize($filePath),
             )
         );
         $this->getResponse()->setHeaders($headers);
