fd0a06ec6ba214d249c0b52d76ebb84dd30d0a9c
pedrod <pedro.devogelaere@vtk.be>
2024-04-08 19:57:55 +0000

    dev-uitleendienst-abstract-articles: made getters and setters for the entities

--- module/LogisticsBundle/Entity/InventoryArticle.php
+++ module/LogisticsBundle/Entity/InventoryArticle.php
@@ -2,6 +2,7 @@
 
 namespace LogisticsBundle\Entity;
 
+use CommonBundle\Entity\General\Organization\Unit;
 use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -65,7 +66,7 @@ class InventoryArticle extends AbstractArticle
      * @var InventoryCategory The category of this article
      *
      * @ORM\ManyToOne(targetEntity="LogisticsBundle\Entity\InventoryCategory")
-     * @ORM\JoinColumn(name="category", referencedColumnName="id")
+     * @ORM\JoinColumn(name="category", referencedColumnName="id", nullable=true)
      */
     private InventoryCategory $category;
 
@@ -98,21 +99,140 @@ class InventoryArticle extends AbstractArticle
     /**
      * @var DateTime The warranty of this article
      *
-     * @ORM\Column(name="warranty_date", type="datetime")
+     * @ORM\Column(name="warranty_date", type="datetime", nullable=true)
      */
     private DateTime $warrantyDate;
 
     /**
      * @var integer The amount of deposit that has to be paid for this article when rent by an external
      *
-     * @ORM\Column(name="deposit", type="integer")
+     * @ORM\Column(name="deposit", type="integer", default=0)
      */
     private int $deposit;
 
     /**
      * @var integer The amount of rent that has to be paid for this article when rent by an external
      *
-     * @ORM\Column(name="rent", type="integer")
+     * @ORM\Column(name="rent", type="integer", default=0)
      */
     private int $rent;
+
+    public function getLocation(): string
+    {
+        return $this->location;
+    }
+
+    public function setLocation(string $location): self
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    public function getSpot(): string
+    {
+        return $this->spot;
+    }
+
+    public function setSpot(string $spot): self
+    {
+        $this->spot = $spot;
+
+        return $this;
+    }
+
+    public function getCategory(): InventoryCategory
+    {
+        return $this->category;
+    }
+
+    public function setCategory(InventoryCategory $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getUnits(): Collection
+    {
+        return $this->units;
+    }
+
+    public function addUnit(Unit $unit): self
+    {
+        if (!$this->units->contains($unit)) {
+            $this->units->add($unit);
+        }
+
+        return $this;
+    }
+
+    public function removeUnit(Unit $unit): self
+    {
+        if ($this->units->removeElement($unit)) {
+            $this->units->removeElement($unit);
+        }
+
+        return $this;
+    }
+
+    public function getVisibility(): string
+    {
+        return $this->visibility;
+    }
+
+    public function setVisibility(string $visibility): self
+    {
+        $this->visibility = $visibility;
+
+        return $this;
+    }
+
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getWarrantyDate(): DateTime
+    {
+        return $this->warrantyDate;
+    }
+
+    public function setWarrantyDate(DateTime $warrantyDate): self
+    {
+        $this->warrantyDate = $warrantyDate;
+
+        return $this;
+    }
+
+    public function getDeposit(): int
+    {
+        return $this->deposit;
+    }
+
+    public function setDeposit(int $deposit): self
+    {
+        $this->deposit = $deposit;
+
+        return $this;
+    }
+
+    public function getRent(): int
+    {
+        return $this->rent;
+    }
+
+    public function setRent(int $rent): self
+    {
+        $this->rent = $rent;
+
+        return $this;
+    }
 }
