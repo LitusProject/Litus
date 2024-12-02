fecc05135df7af725b4850014bb30cbdc1f1ff46
Tiddons <tiddo.nees@gmail.com>
2024-12-02 15:40:20 +0100

    Improved werkgroepDatabase size and added cudi development

---
+++ cudiDevelopmentScript.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Get the current username
+CURRENT_USER=$(whoami)
+
+# Create the service file with the specified contents
+sudo bash -c "cat > /etc/systemd/system/litus-sockets.service <<EOL
+[Unit]
+Description=Litus Sockets
+After=network-online.target
+Wants=network-online.target
+
+[Service]
+User=www-data
+Group=www-data
+WorkingDirectory=/home/$CURRENT_USER/Projects/LitusProject/Litus
+Environment=APPLICATION_ENV=development
+ExecStartPre=/bin/mkdir -p /var/log/litus
+ExecStartPre=/bin/chown -R www-data:www-data /var/log/litus
+ExecStartPre=/bin/mkdir -p /var/run/litus
+ExecStartPre=/bin/chown -R www-data:www-data /var/run/litus
+ExecStart=/home/$CURRENT_USER/Projects/LitusProject/Litus/bin/sockets.sh
+Restart=on-failure
+
+PermissionsStartOnly=true
+
+[Install]
+WantedBy=multi-user.target
+EOL"
+
+# Reload systemd to recognize the new service
+sudo systemctl daemon-reload
+
+# Enable the service to start on boot
+sudo systemctl enable litus-sockets.service
+
+# Start the service
+sudo systemctl start litus-sockets.service
