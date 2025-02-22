9a0b98f9889be6b7bf2d85a55110558973ccbd2f
d1ff1cult0 <jarne.plessers@student.kuleuven.be>
2025-02-22 14:43:14 +0100

    Removed people that have checked no mails on profile from export

--- module/SyllabusBundle/Component/Document/Generator/Group.php
+++ module/SyllabusBundle/Component/Document/Generator/Group.php
@@ -52,6 +52,9 @@ class Group extends \CommonBundle\Component\Document\Generator\Csv
 
             foreach ($enrollments as $enrollment) {
                 $ac = $enrollment->getAcademic();
+                if ($ac->getNoMail()) {
+                    continue;
+                }
 
                 $primaryAddress = $ac->getPrimaryAddress();
                 $secondaryAddress = $ac->getSecondaryAddress();
